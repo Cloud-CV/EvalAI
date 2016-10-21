ad1dcf8c11859718cc022d66044f6c0f91b4e32e
Deshraj <deshrajdry@gmail.com>
2016-10-21 08:06:22 -0400

    Docs modified

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,4 @@
+import sphinx_rtd_theme
 # -*- coding: utf-8 -*-
 #
 # EvalAI documentation build configuration file, created by
@@ -119,7 +120,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -128,7 +130,7 @@ html_theme = 'alabaster'
 # html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-# html_theme_path = []
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
