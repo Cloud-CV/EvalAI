416e8cedc2f4db686453a47192865b8515f73db5
Deshraj <deshrajdry@gmail.com>
2016-11-01 18:20:22 -0400

    Apps added for different features

---
+++ evalai/accounts/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.urls import reverse
+
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from rest_auth.registration.views import VerifyEmailView
+from rest_framework.response import Response
+from rest_framework import status
+
+import requests
+
+# Create your views here.
+class ConfirmEmailView(APIView):
+
+    permission_classes = (AllowAny,)
+    allowed_methods = ('GET', 'OPTIONS', 'HEAD')
+
+    def get(self, request, *args, **kwargs):
+        post_data = {
+            'key': kwargs['key'],
+        }
+
+        requests.post(request.build_absolute_uri(reverse("rest_verify_email")), data=post_data)
+        return Response({'message': "Email verified successfully"}, status=status.HTTP_200_OK)
--- evalai/urls.py
+++ evalai/urls.py
@@ -15,26 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
-
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'is_staff')
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+from evalai.accounts.views import ConfirmEmailView
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
     url(r'^admin/', admin.site.urls),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api/auth/', include('rest_auth.urls')),
+    url(r'^api/auth/registration/account-confirm-email/(?P<key>[-:\w]+)/$', ConfirmEmailView.as_view(), name='account_confirm_email'),
+    url(r'^api/auth/registration/', include('rest_auth.registration.urls')),
+    # this url is used to generate email content
+    url(r'^api/admin-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
