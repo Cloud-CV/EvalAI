18c384d45fd27be61e97265c25f6872fdbb82be0
Deshraj <deshrajdry@gmail.com>
2016-11-01 20:19:39 -0400

    Passwords reset confirmation url pattern added

--- evalai/urls.py
+++ evalai/urls.py
@@ -16,12 +16,15 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from evalai.accounts.views import ConfirmEmailView
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
+    url(r'^', include('django.contrib.auth.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^api/auth/', include('rest_auth.urls')),
     url(r'^api/auth/registration/account-confirm-email/(?P<key>[-:\w]+)/$', ConfirmEmailView.as_view(), name='account_confirm_email'),
     url(r'^api/auth/registration/', include('rest_auth.registration.urls')),
-    # this url is used to generate email content
+    url(r'^api/password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', TemplateView.as_view(template_name="password_reset_confirm.html"),
+        name='password_reset_confirm'),
     url(r'^api/admin-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
