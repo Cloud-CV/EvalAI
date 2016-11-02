517e2f9cb6c2f7da53e96f071d1f19c159db5b1e
Deshraj <deshrajdry@gmail.com>
2016-11-01 21:53:30 -0400

    Dummy test added

---
+++ tests/test_dummy.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
