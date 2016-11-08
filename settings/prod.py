9066b4475b60267c824f60f3b611340f4b8a2a82
Deshraj <deshrajdry@gmail.com>
2016-11-08 05:30:38 -0500

    DB setting modified to handle exception

--- settings/prod.py
+++ settings/prod.py
@@ -13,11 +13,11 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['RDS_DB_NAME'],
-        'USER': os.environ['RDS_USERNAME'],
-        'PASSWORD': os.environ['RDS_PASSWORD'],
-        'HOST': os.environ['RDS_HOSTNAME'],
-        'PORT': os.environ['RDS_PORT'],
+        'NAME': os.environ.get('RDS_DB_NAME', ""),
+        'USER': os.environ.get('RDS_USERNAME', ""),
+        'PASSWORD': os.environ.get('RDS_PASSWORD', ""),
+        'HOST': os.environ.get('RDS_HOSTNAME', ""),
+        'PORT': os.environ.get('RDS_PORT', ""),
     }
 }
 
