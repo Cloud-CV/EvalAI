e6a2e13e372c1e79bfae497385a55e2a215e004b
Deshraj <deshrajdry@gmail.com>
2016-11-07 21:54:00 -0500

    ConfirmEmailView shifted to accounts app

--- apps/participants/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from .views import (
-    ConfirmEmailView,
-)
-
-urlpatterns = [
-    url(r'^api/auth/registration/account-confirm-email/(?P<key>[-:\w]+)/$',
-        ConfirmEmailView.as_view(), name='account_confirm_email'),
-]
