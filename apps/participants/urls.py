d4dd3d6b8e9b84a100a82088026bda14736f3c0e
Deshraj <deshrajdry@gmail.com>
2016-11-07 21:14:56 -0500

    ConfirmEmail view added to participants app

---
+++ apps/participants/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from .views import (
+    ConfirmEmailView,
+)
+
+urlpatterns = [
+    url(r'^api/auth/registration/account-confirm-email/(?P<key>[-:\w]+)/$', ConfirmEmailView.as_view(), name='account_confirm_email'),
+]
