20d1746f5dfd079df506a3846f782e2ccbcd69e9
Deshraj <deshrajdry@gmail.com>
2016-11-09 05:18:53 -0500

    UserStatus model added in Accounts app

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -3,3 +3,24 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class UserStatus(models.Model):
+    """
+    Model representing the status of a user when invited for hosting/participating a competition
+    .. note::
+        There are four different status:
+            - Unknown.
+            - Denied.
+            - Accepted
+            - Pending.
+    """
+    UNKNOWN = 'unknown'
+    DENIED = 'denied'
+    ACCEPTED = 'accepted'
+    PENDING = 'pending'
+
+    name = models.CharField(max_length=30)
+    status = models.CharField(max_length=30,unique=True)
+
+    def __unicode__(self):
+        return self.name
