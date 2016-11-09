48ca0ddfd1f439bdb640a7e0b4ff6f137425318a
Deshraj <deshrajdry@gmail.com>
2016-11-09 06:08:49 -0500

    Challenge model added

--- apps/challenges/models.py
+++ apps/challenges/models.py
@@ -1,5 +1,24 @@
 from __future__ import unicode_literals
 
+from accounts.models import (TimeStampedModel)
 from django.db import models
 
-# Create your models here.
+
+class Challenge(TimeStampedModel):
+    """ 
+    Model representing a hosted Challenge
+    """
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(
+        upload_to='logos', null=True, blank=True, verbose_name="Logo")
+    start_date = models.DateTimeField(
+        null=True, blank=True, verbose_name="Start Date (UTC)")
+    end_date = models.DateTimeField(
+        null=True, blank=True, verbose_name="End Date (UTC)")
+    creator = models.ForeignKey(
+        ChallengeOrganizerTeams, related_name='challenge_creator')
+    published = models.BooleanField(
+        default=False, verbose_name="Publicly Available")
+    enable_forum = models.BooleanField(default=True)
+    anonymous_leaderboard = models.BooleanField(default=False)
