334a3e38be19fe3b1992951baaf3b9b04b5f431a
Deshraj <deshrajdry@gmail.com>
2016-11-09 05:39:31 -0500

    Team models added

--- apps/teams/models.py
+++ apps/teams/models.py
@@ -1,5 +1,23 @@
 from __future__ import unicode_literals
 
+from accounts.models import (TimeStampedModel, UserStatus)
+from challenges.models import (Challenge,)
+from participants.models import(Participant,)
+
 from django.db import models
 
-# Create your models here.
+class Team(TimeStampedModel):
+    """
+    Model representing the Teams associated with different challenges
+    """
+    challenge = models.ForeignKey(Challenge)
+    team_name = models.CharField(max_length=64, null=False, blank=False)
+    
+
+class TeamMember(TimeStampedModel):
+    """
+    Model representing members associated with a particular team
+    """
+    participant = models.ForeignKey(Participant)
+    team = models.ForeignKey(Team)
+    status = models.ForeignKey(UserStatus)
