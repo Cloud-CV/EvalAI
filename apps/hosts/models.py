75e5b711623c643f7912a18771d4daeb1ec52610
Deshraj Yadav <deshraj@vt.edu>
2016-11-11 13:35:11 -0800

    Hosts related models added (#22)

--- apps/hosts/models.py
+++ apps/hosts/models.py
@@ -1,5 +1,42 @@
 from __future__ import unicode_literals
 
-from django.db import models
+from accounts.models import (TimeStampedModel, UserStatus)
+from challenges.models import (Challenge)
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class HostPermission(TimeStampedModel):
+    """
+    Model representing the status of a competition's participant
+    .. note::
+        There are four different status:
+            - Admin.
+            - Write.
+            - Read
+            - Restricted (Default).
+    """
+    ADMIN = 'Admin'
+    WRITE = 'Write'
+    READ = 'Read'
+    RESTRICTED = 'Restricted'
+    name = models.Chaield(max_length=30)
+    status = models.CharField(max_length=30, unique=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class ChallengeHostTeams(TimeStampedModel):
+    challenge = models.ForeignKey(Challenge)
+    team_name = models.CharField(max_length=64, null=False, blank=False)
+
+
+class ChallengeHost(TimeStampedModel):
+    user = models.ForeignKey(User)
+    affiliation = models.CharField(max_length=255, null=False, blank=False)
+    team_name = models.ForeignKey(ChallengeHostTeams)
+    status = models.ForeignKey(UserStatus)
+    permissions = models.ForeignKey(HostPermission)
+    # TODO: More field names to be added
