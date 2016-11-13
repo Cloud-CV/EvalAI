0f61b0544a9d71f6595a85716631df8b20519def
Deshraj Yadav <deshraj@vt.edu>
2016-11-13 12:13:10 -0800

    Email configurations added for production server (#29)

--- settings/prod.py
+++ settings/prod.py
@@ -38,5 +38,14 @@ STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
 
 # Media files configuration on S3
 MEDIAFILES_LOCATION = 'media'
-MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
-DEFAULT_FILE_STORAGE = 'settings.custom_storages.MediaStorage'
+MEDIA_URL = 'http://%s.s3.amazonaws.com/%s/' % (AWS_STORAGE_BUCKET_NAME, MEDIAFILES_LOCATION)
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+
+# Setup Email Bacend related settings
+DEFAULT_FROM_EMAIL = "noreply@cloudcv.org"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.environ.get("EMAIL_HOST", "")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
+EMAIL_PORT = os.environ.get("EMAIL_PORT", "")
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "")
