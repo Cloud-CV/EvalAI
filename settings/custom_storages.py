f3f046aadb235789487b0a7cd6ee531300cdc85e
Deshraj <deshrajdry@gmail.com>
2016-11-14 17:05:42 -0500

    Import fixes

--- settings/custom_storages.py
+++ settings/custom_storages.py
@@ -1,13 +1,13 @@
-from django.conf import settings
 from storages.backends.s3boto import S3BotoStorage
 
+from settings.prod import STATICFILES_LOCATION, MEDIAFILES_LOCATION
 # In order to make sure that both static and media files are stored in
 # different directories
 
 
 class StaticStorage(S3BotoStorage):
-    location = settings.STATICFILES_LOCATION
+    location = STATICFILES_LOCATION
 
 
 class MediaStorage(S3BotoStorage):
-    location = settings.MEDIAFILES_LOCATION
+    location = MEDIAFILES_LOCATION
