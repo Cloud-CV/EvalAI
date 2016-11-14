3f5eb107330bf5ec69b319674d2913978b19dd3b
Deshraj Yadav <deshraj@vt.edu>
2016-11-14 14:50:14 -0800

    Import fixes for S3 configuration for production server (#58)
    * Challenges models modified to fix circular import
    
    * Hosts model modified to fix circulr import
    
    * Sample urlpattern added to all apps
    
    * Migrations added to gitignore
    
    * Storage path fixed for storing media and static files on production server
    
    * Import fixes
    
    * PEP8 fixes
    
    * Unnecessary imports removed

--- settings/custom_storages.py
+++ settings/custom_storages.py
@@ -1,6 +1,6 @@
 from storages.backends.s3boto import S3BotoStorage
 
-from settings.prod import STATICFILES_LOCATION, MEDIAFILES_LOCATION
+from .prod import STATICFILES_LOCATION, MEDIAFILES_LOCATION
 
 # In order to make sure that both static and media files are stored in
 # different directories
