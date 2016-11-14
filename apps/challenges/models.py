0d092db30291ca80ec05e3bc67da9ddf443137ba
Deshraj <deshrajdry@gmail.com>
2016-11-14 09:14:16 -0500

    Challenge model modified and Phase added in Challenge

--- apps/challenges/models.py
+++ apps/challenges/models.py
@@ -1,8 +1,9 @@
 from __future__ import unicode_literals
 
 from accounts.models import (TimeStampedModel)
-from hosts.models import (ChallengeOrganizerTeams)
 from django.db import models
+
+from hosts.models import (ChallengeHostTeams)
 
 
 class Challenge(TimeStampedModel):
@@ -11,6 +12,9 @@ class Challenge(TimeStampedModel):
     """
     title = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
+    terms_and_conditions = models.TextField(null=True, blank=True)
+    submission_guidelines = models.TextField(null=True, blank=True)
+    evaluation_details = models.TextField(null=True, blank=True)
     image = models.ImageField(
         upload_to='logos', null=True, blank=True, verbose_name="Logo")
     start_date = models.DateTimeField(
@@ -18,8 +22,23 @@ class Challenge(TimeStampedModel):
     end_date = models.DateTimeField(
         null=True, blank=True, verbose_name="End Date (UTC)")
     creator = models.ForeignKey(
-        ChallengeOrganizerTeams, related_name='challenge_creator')
+        ChallengeHostTeams, related_name='challenge_creator')
     published = models.BooleanField(
         default=False, verbose_name="Publicly Available")
     enable_forum = models.BooleanField(default=True)
     anonymous_leaderboard = models.BooleanField(default=False)
+
+    class Meta:
+        app_label = 'challenges'
+        db_table = 'challenge'
+
+
+class Phase(TimeStampedModel):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    leaderboard_public = models.BooleanField(default=False)
+    challenge = models.ForeignKey(Challenge)
+
+    class Meta:
+        app_label = 'challenges'
+        db_table = 'challenge_phase'
