f043a5ce21d257177f483c6cb0ad41862ecec0ba
Taranjeet Singh <reachtotj@gmail.com>
2016-11-17 00:50:41 +0530

    team: Add api to create team using POST.

--- apps/teams/serializers.py
+++ apps/teams/serializers.py
@@ -4,6 +4,19 @@ from challenges.serializers import ChallengeSerializer
 from .models import Team
 
 
+class TeamSerializer(serializers.ModelSerializer):
+
+    def __init__(self, *args, **kwargs):
+        super(TeamSerializer, self).__init__(*args, **kwargs)
+        context = kwargs.get('context')
+        challenge = context.get('challenge')
+        kwargs['data']['challenge'] = challenge.id
+
+    class Meta:
+        model = Team
+        fields = ('id', 'team_name', 'challenge',)
+
+
 class TeamChallengeSerializer(serializers.ModelSerializer):
 
     challenge = ChallengeSerializer()
