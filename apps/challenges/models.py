0fcd4b451d6c85d314fe50b2a9177be14ecdf9a4
Taranjeet Singh <reachtotj@gmail.com>
2016-11-19 14:34:30 +0530

    Rename ChallengeHostTeams to ChallengeHostTeam. (#77)

--- apps/challenges/models.py
+++ apps/challenges/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 from accounts.models import (TimeStampedModel, )
-# from hosts.models import (ChallengeHostTeams, )
+from hosts.models import (ChallengeHostTeam, )
 
 
 class Challenge(TimeStampedModel):
@@ -22,7 +22,7 @@ class Challenge(TimeStampedModel):
     end_date = models.DateTimeField(
         null=True, blank=True, verbose_name="End Date (UTC)")
     creator = models.ForeignKey(
-        'hosts.ChallengeHostTeams', related_name='challenge_creator')
+        'hosts.ChallengeHostTeam', related_name='challenge_creator')
     published = models.BooleanField(
         default=False, verbose_name="Publicly Available")
     enable_forum = models.BooleanField(default=True)
