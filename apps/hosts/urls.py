bba88e5ca4a20536322aa905a7774af44ad17a3e
Taranjeet Singh <reachtotj@gmail.com>
2016-11-21 01:06:45 +0530

    Hosts: Add API to get all challenge host for a particular challenge host team. (#110)

--- apps/hosts/urls.py
+++ apps/hosts/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     url(r'challenge_host_team/$', views.challenge_host_team_list, name='get_challenge_host_team_list'),
     url(r'challenge_host_team/(?P<pk>[0-9]+)$', views.challenge_host_team_detail,
         name='get_challenge_host_team_details'),
+    url(r'challenge_host_team/(?P<challenge_host_team_pk>[0-9]+)/challenge_host$', views.challenge_host_list,
+        name='get_challenge_host_list'),
 
 ]
