555f5ea1a4088fa9a065aef53483544c14fc20b1
Taranjeet Singh <reachtotj@gmail.com>
2016-11-20 14:10:42 +0530

    Hosts: Add create a new challenge host team api.

--- apps/hosts/serializers.py
+++ apps/hosts/serializers.py
@@ -5,6 +5,13 @@ from .models import ChallengeHostTeam
 
 class ChallengeHostTeamSerializer(serializers.ModelSerializer):
 
+    def __init__(self, *args, **kwargs):
+        super(ChallengeHostTeamSerializer, self).__init__(*args, **kwargs)
+        context = kwargs.get('context')
+        if context:
+            request = context.get('request')
+            kwargs['data']['created_by'] = request.user.pk
+
     class Meta:
         model = ChallengeHostTeam
         fields = ('id', 'team_name', 'created_by',)
