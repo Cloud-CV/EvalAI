e782651d0a5a61eadcc5770a020eae27e2a2f237
Taranjeet Singh <reachtotj@gmail.com>
2016-11-22 07:25:24 +0530

    Challenges: Add API to get all challenges. (#122)

--- apps/challenges/urls.py
+++ apps/challenges/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
-# from .views import ()
 
+from . import views
 
 urlpatterns = [
+
+    url(r'challenge_host_team/(?P<challenge_host_team_pk>[0-9]+)/challenge$', views.challenge_list,
+        name='get_challenge_list'),
+
 ]
--- apps/challenges/views.py
+++ apps/challenges/views.py
@@ -1,3 +1,34 @@
+from django.conf import settings
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import permissions, status
+from rest_framework.authentication import (TokenAuthentication,)
+from rest_framework.decorators import (api_view,
+                                       authentication_classes,
+                                       permission_classes,)
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
+from hosts.models import ChallengeHostTeam
+
+from .models import Challenge
+from .serializers import ChallengeSerializer
+
+
+@api_view(['GET'])
+@permission_classes((permissions.IsAuthenticated,))
+@authentication_classes((TokenAuthentication,))
+def challenge_list(request, challenge_host_team_pk):
+    try:
+        challenge_host_team = ChallengeHostTeam.objects.get(pk=challenge_host_team_pk)
+    except ChallengeHostTeam.DoesNotExist:
+        response_data = {'error': 'ChallengeHostTeam does not exist'}
+        return Response(response_data, status=status.HTTP_406_NOT_ACCEPTABLE)
+
+    challenge = Challenge.objects.filter(creator=challenge_host_team)
+    paginator = PageNumberPagination()
+    paginator.page_size = settings.REST_FRAMEWORK['PAGE_SIZE']
+    result_page = paginator.paginate_queryset(challenge, request)
+    serializer = ChallengeSerializer(result_page, many=True)
+    response_data = serializer.data
+    return paginator.get_paginated_response(response_data)
