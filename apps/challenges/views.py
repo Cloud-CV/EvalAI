6f4ccfefce3eaaafe803578bd48e68bee4a01601
Taranjeet Singh <reachtotj@gmail.com>
2016-11-22 07:37:34 +0530

    Challenges: Add API to create a new challenge. (#123)

--- apps/challenges/views.py
+++ apps/challenges/views.py
@@ -15,7 +15,7 @@ from .models import Challenge
 from .serializers import ChallengeSerializer
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 @permission_classes((permissions.IsAuthenticated,))
 @authentication_classes((TokenAuthentication,))
 def challenge_list(request, challenge_host_team_pk):
@@ -25,10 +25,20 @@ def challenge_list(request, challenge_host_team_pk):
         response_data = {'error': 'ChallengeHostTeam does not exist'}
         return Response(response_data, status=status.HTTP_406_NOT_ACCEPTABLE)
 
-    challenge = Challenge.objects.filter(creator=challenge_host_team)
-    paginator = PageNumberPagination()
-    paginator.page_size = settings.REST_FRAMEWORK['PAGE_SIZE']
-    result_page = paginator.paginate_queryset(challenge, request)
-    serializer = ChallengeSerializer(result_page, many=True)
-    response_data = serializer.data
-    return paginator.get_paginated_response(response_data)
+    if request.method == 'GET':
+        challenge = Challenge.objects.filter(creator=challenge_host_team)
+        paginator = PageNumberPagination()
+        paginator.page_size = settings.REST_FRAMEWORK['PAGE_SIZE']
+        result_page = paginator.paginate_queryset(challenge, request)
+        serializer = ChallengeSerializer(result_page, many=True)
+        response_data = serializer.data
+        return paginator.get_paginated_response(response_data)
+
+    elif request.method == 'POST':
+        serializer = ChallengeSerializer(data=request.data,
+                                         context={'challenge_host_team': challenge_host_team})
+        if serializer.is_valid():
+            serializer.save()
+            response_data = serializer.data
+            return Response(response_data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
