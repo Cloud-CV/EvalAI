49cd229234c09e26afa9fe2abc44d417967361cc
gauthamzz <thabeatsz@gmail.com>
2016-11-24 02:30:05 +0530

    Fixing flake8 errors

--- apps/challenges/admin.py
+++ apps/challenges/admin.py
@@ -1,21 +1,58 @@
 from django.contrib import admin
 
 
-from .models import Challenge,Phase
+from .models import Challenge, Phase
 from base.admin import TimeStampedAdmin
+
 
 @admin.register(Challenge)
 class ChallengeModelAdmin(TimeStampedAdmin):
-    list_display = ("title","description","terms_and_conditions","submission_guidelines","evaluation_details","image","start_date","end_date","creator","published","enable_forum","anonymous_leaderboard")
-    list_filter = ("title","description","terms_and_conditions","submission_guidelines","evaluation_details","image","start_date","end_date","creator","published","enable_forum","anonymous_leaderboard")
-    search_fields = ("title","description","terms_and_conditions","submission_guidelines","evaluation_details","image","start_date","end_date","creator","published","enable_forum","anonymous_leaderboard")
+    list_display = ("title",
+                    "description",
+                    "terms_and_conditions",
+                    "submission_guidelines",
+                    "evaluation_details",
+                    "image",
+                    "start_date",
+                    "end_date",
+                    "creator",
+                    "published",
+                    "enable_forum",
+                    "anonymous_leaderboard")
+    list_filter = ("title",
+                   "description",
+                   "terms_and_conditions",
+                   "submission_guidelines",
+                   "evaluation_details",
+                   "image",
+                   "start_date",
+                   "end_date",
+                   "creator",
+                   "published",
+                   "enable_forum",
+                   "anonymous_leaderboard")
+    search_fields = ("title",
+                     "description",
+                     "terms_and_conditions",
+                     "submission_guidelines",
+                     "evaluation_details",
+                     "image",
+                     "start_date",
+                     "end_date",
+                     "creator",
+                     "published",
+                     "enable_forum",
+                     "anonymous_leaderboard")
+
     class Meta:
         model = Challenge
 
+
 @admin.register(Phase)
 class PhaseModelAdmin(TimeStampedAdmin):
-    list_display = ("name","description","leaderboard_public","challenge")
-    list_filter = ("name","description","leaderboard_public","challenge")
-    search_fields = ("name","description","leaderboard_public","challenge")
+    list_display = ("name", "description", "leaderboard_public", "challenge")
+    list_filter = ("name", "description", "leaderboard_public", "challenge")
+    search_fields = ("name", "description", "leaderboard_public", "challenge")
+
     class Meta:
         model = Phase
--- apps/hosts/admin.py
+++ apps/hosts/admin.py
@@ -1,21 +1,25 @@
 from django.contrib import admin
 
 
-from .models import ChallengeHostTeam,ChallengeHost
+from .models import ChallengeHostTeam, ChallengeHost
 from base.admin import TimeStampedAdmin
+
 
 @admin.register(ChallengeHostTeam)
 class HostTeamModelAdmin(TimeStampedAdmin):
-    list_display = ("team_name","created_by")
-    list_filter = ("team_name","created_by")
-    search_fields = ("team_name","created_by")
+    list_display = ("team_name", "created_by")
+    list_filter = ("team_name", "created_by")
+    search_fields = ("team_name", "created_by")
+
     class Meta:
         model = ChallengeHostTeam
 
+
 @admin.register(ChallengeHost)
 class HostModelAdmin(TimeStampedAdmin):
-    list_display = ("user","team_name","status")
-    list_filter = ("user","team_name","status")
-    search_fields = ("user","team_name","status")
+    list_display = ("user", "team_name", "status")
+    list_filter = ("user", "team_name", "status")
+    search_fields = ("user", "team_name", "status")
+
     class Meta:
         model = ChallengeHost
