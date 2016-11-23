b1bf2fb17f4c3596c684ba6c043a7c58852d609b
gauthamzz <thabeatsz@gmail.com>
2016-11-24 01:13:06 +0530

    Setup ModelAdmin class for each model in Host app

--- apps/hosts/admin.py
+++ apps/hosts/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import ChallengeHostTeam,ChallengeHost
+from base.admin import TimeStampedAdmin
+
+@admin.register(ChallengeHostTeam)
+class HostTeamModelAdmin(TimeStampedAdmin):
+    list_display = ("team_name","created_by")
+    list_filter = ("team_name","created_by")
+    search_fields = ("team_name","created_by")
+    class Meta:
+        model = ChallengeHostTeam
+
+@admin.register(ChallengeHost)
+class HostModelAdmin(TimeStampedAdmin):
+    list_display = ("user","team_name","status")
+    list_filter = ("user","team_name","status")
+    search_fields = ("user","team_name","status")
+    class Meta:
+        model = ChallengeHost
