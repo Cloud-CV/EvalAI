d6096725e913911a1cf6843a523c278879568b47
gauthamzz <thabeatsz@gmail.com>
2016-11-27 15:02:18 +0530

    Setup ModelAdmin class for each model in Hosts app

--- apps/challenges/admin.py
+++ apps/challenges/admin.py
@@ -1,28 +1,3 @@
 from django.contrib import admin
 
-from base.admin import TimeStampedAdmin
-
-from .models import Challenge, Phase
-
-
-@admin.register(Challenge)
-class ChallengeModelAdmin(TimeStampedAdmin):
-    list_display = ("title",
-                    "start_date",
-                    "end_date",
-                    "creator",
-                    "published",
-                    "enable_forum",
-                    "anonymous_leaderboard")
-    list_filter = ("creator",
-                   "published",
-                   "enable_forum",
-                   "anonymous_leaderboard")
-    search_fields = ("title", "creator")
-
-
-@admin.register(Phase)
-class PhaseModelAdmin(TimeStampedAdmin):
-    list_display = ("name", "leaderboard_public", "challenge")
-    list_filter = ("leaderboard_public", "challenge")
-    search_fields = ("name")
+# Register your models here.
