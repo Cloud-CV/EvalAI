38471f138644a3b5124ede9078ac628d128849e9
Akash Jain <akashjain993@gmail.com>
2016-11-27 07:07:09 +0530

    Profile and Dashboard View added (#143)
    * Added dashboard setup and improved auth
    
    * Solved mobile auth issues
    
    * Added Dashboard view and Profile view
    
    * Resolved merge conflicts

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,3 +1,6 @@
+// Gulp Tasks
+'use strict';
+
 var gulp = require('gulp'),
     merge = require('merge-stream'),
     sass = require('gulp-ruby-sass'),
@@ -16,19 +19,22 @@ var gulp = require('gulp'),
     htmlmin = require('gulp-html-minifier'),
     fs = require('fs'),
     connectModRewrite = require('connect-modrewrite'),
+    ngConfig = require('gulp-ng-config'),
+    // path = require('gulp-path'),
+    // conf = require('./conf')(gulp),
     _ = require('lodash');
- 
+
 
 var scripts = require('./app.scripts.json');
 var styles = require('./app.styles.json');
 
 
 //include all bower scripts files
-gulp.task('vendorjs', function(){
+gulp.task('vendorjs', function() {
 
-    _.forIn(scripts.chunks, function(chunkScripts, chunkName){
+    _.forIn(scripts.chunks, function(chunkScripts, chunkName) {
         var paths = [];
-        chunkScripts.forEach(function(script){
+        chunkScripts.forEach(function(script) {
             var scriptFileName = scripts.paths[script];
 
             if (!fs.existsSync(__dirname + '/' + scriptFileName)) {
@@ -47,11 +53,11 @@ gulp.task('vendorjs', function(){
 
 
 //include all bower  styles files
-gulp.task('vendorcss', function(){
+gulp.task('vendorcss', function() {
 
-    _.forIn(styles.chunks, function(chunkStyles, chunkName){
+    _.forIn(styles.chunks, function(chunkStyles, chunkName) {
         var paths = [];
-        chunkStyles.forEach(function(style){
+        chunkStyles.forEach(function(style) {
             var styleFileName = styles.paths[style];
 
             if (!fs.existsSync(__dirname + '/' + styleFileName)) {
@@ -68,62 +74,78 @@ gulp.task('vendorcss', function(){
 
 });
 
+// config for dev server
+gulp.task('configDev', function() {
+    gulp.src('src/js/config.json')
+        .pipe(ngConfig('evalai-config', {
+            environment: 'local'
+        }))
+        .pipe(gulp.dest('./dist/js'))
+});
+
+// config for prod server
+gulp.task('configProd', function() {
+    gulp.src('src/js/config.json')
+        .pipe(ngConfig('evalai-config', {
+            environment: 'production'
+        }))
+        .pipe(gulp.dest('./dist/js'))
+});
 
 // minify and compress CSS files
-gulp.task('css', function(){
-     return sass('src/css/main.scss', { style: 'expanded' })
+gulp.task('css', function() {
+    return sass('src/css/main.scss', { style: 'expanded' })
         .pipe(autoprefixer('last 2 version'))
         .pipe(gulp.dest('./dist/css'))
-        .pipe(rename({suffix: '.min'}))
+        .pipe(rename({ suffix: '.min' }))
         .pipe(cssnano())
         .pipe(gulp.dest('./dist/css'));
 })
 
-
 // minify angular scripts
 gulp.task('js', function() {
 
-  var app = gulp.src('src/js/app.js')
-            .pipe(jshint.reporter('default'))
-            .pipe(concat('app.js'))
-            .pipe(gulp.dest('./dist/js'))
-            .pipe(rename({ suffix: '.min' }))
-            .pipe(uglify())
-            .pipe(gulp.dest('./dist/js'));
+    var app = gulp.src('src/js/app.js')
+        .pipe(jshint.reporter('default'))
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('./dist/js'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'));
 
-  var configs = gulp.src('src/js/route-config/*.js')
-                .pipe(jshint.reporter('default'))
-                .pipe(concat('route-config.js'))
-                .pipe(gulp.dest('./dist/js'))
-                .pipe(rename({ suffix: '.min' }))
-                .pipe(uglify())
-                .pipe(gulp.dest('./dist/js'));
+    var configs = gulp.src('src/js/route-config/*.js')
+        .pipe(jshint.reporter('default'))
+        .pipe(concat('route-config.js'))
+        .pipe(gulp.dest('./dist/js'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'));
 
-  var controllers = gulp.src('src/js/controllers/*.js')
-                    .pipe(jshint.reporter('default'))
-                    .pipe(concat('controllers.js'))
-                    .pipe(gulp.dest('./dist/js'))
-                    .pipe(rename({ suffix: '.min' }))
-                    .pipe(uglify())
-                    .pipe(gulp.dest('./dist/js'));
+    var controllers = gulp.src('src/js/controllers/*.js')
+        .pipe(jshint.reporter('default'))
+        .pipe(concat('controllers.js'))
+        .pipe(gulp.dest('./dist/js'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'));
 
-  var directives = gulp.src('src/js/directives/*.js')
-                    .pipe(jshint.reporter('default'))
-                    .pipe(concat('directives.js'))
-                    .pipe(gulp.dest('./dist/js'))
-                    .pipe(rename({ suffix: '.min' }))
-                    .pipe(uglify())                  
-                    .pipe(gulp.dest('./dist/js'));
+    var directives = gulp.src('src/js/directives/*.js')
+        .pipe(jshint.reporter('default'))
+        .pipe(concat('directives.js'))
+        .pipe(gulp.dest('./dist/js'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'));
 
-  var services = gulp.src('src/js/services/*.js')
-                .pipe(jshint.reporter('default'))
-                .pipe(concat('services.js'))
-                .pipe(gulp.dest('./dist/js'))
-                .pipe(rename({ suffix: '.min' }))
-                .pipe(uglify())             
-                .pipe(gulp.dest('./dist/js'));    
+    var services = gulp.src('src/js/services/*.js')
+        .pipe(jshint.reporter('default'))
+        .pipe(concat('services.js'))
+        .pipe(gulp.dest('./dist/js'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'));
 
-    return merge(app, configs, controllers, directives, services)              
+    return merge(app, configs, controllers, directives, services)
 });
 
 
@@ -131,11 +153,11 @@ gulp.task('js', function() {
 gulp.task('html', function() {
 
     var webViews = gulp.src('src/views/web/*.html')
-        .pipe(htmlmin({collapseWhitespace: true }))
+        .pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(gulp.dest('./dist/views/web'));
 
     var webPartials = gulp.src('src/views/web/partials/*.html')
-        .pipe(htmlmin({collapseWhitespace: true }))
+        .pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(gulp.dest('./dist/views/web/partials'));
 
     return merge(webViews, webPartials);
@@ -144,21 +166,23 @@ gulp.task('html', function() {
 
 // for image compression
 gulp.task('images', function() {
-  return gulp.src('src/images/**/*')
-    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
-    .pipe(gulp.dest('./dist/images'));
+    return gulp.src('src/images/**/*')
+        .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+        .pipe(gulp.dest('./dist/images'));
 });
 
 
 // Fonts
 gulp.task('fonts', function() {
     var font = gulp.src([
-                    'bower_components/font-awesome/fonts/fontawesome-webfont.*', 'bower_components/materialize/fonts/**/*'])
-            .pipe(gulp.dest('dist/fonts/'));
+            'bower_components/font-awesome/fonts/fontawesome-webfont.*', 'bower_components/materialize/fonts/**/*'
+        ])
+        .pipe(gulp.dest('dist/fonts/'));
 
     var fontCss = gulp.src([
-                    'bower_components/font-awesome/css/font-awesome.css'])
-            .pipe(gulp.dest('dist/css/'));
+            'bower_components/font-awesome/css/font-awesome.css'
+        ])
+        .pipe(gulp.dest('dist/css/'));
 
     return merge(font, fontCss);
 });
@@ -166,7 +190,7 @@ gulp.task('fonts', function() {
 
 // cleaning build process- run clean before deploy and rebuild files again
 gulp.task('clean', function() {
-    return del(['./dist/css', './dist/js', './dist/images', '../dist/vendors',  './dist/views'],{force: true});
+    return del(['./dist/css', './dist/js', './dist/images', '../dist/vendors', './dist/views'], { force: true });
 });
 
 
@@ -195,21 +219,26 @@ gulp.task('watch', function() {
 
 
 // connect to server
-gulp.task('connect', function(){
+gulp.task('connect', function() {
     connect.server({
         root: './',
         port: 8888,
-        middleware: function (connect) {
-        return [
-          connectModRewrite([
-            '!\\.html|\\.js|\\.css|\\.ico|\\.png|\\.gif|\\.jpg|\\.woff|.\\.ttf|.\\otf|\\.jpeg|\\.swf.*$ /index.html [NC,L]'
-          ])
-        ];
-      }
+        middleware: function(connect) {
+            return [
+                connectModRewrite([
+                    '!\\.html|\\.js|\\.css|\\.ico|\\.png|\\.gif|\\.jpg|\\.woff|.\\.ttf|.\\otf|\\.jpeg|\\.swf.*$ /index.html [NC,L]'
+                ])
+            ];
+        }
     });
 })
 
-// run by default
+// development task
 gulp.task('dev', ['clean'], function() {
-    gulp.start('css', 'js', 'html', 'images', 'vendorjs', 'vendorcss', 'fonts');
+    gulp.start('css', 'js', 'html', 'images', 'vendorjs', 'vendorcss', 'fonts', 'configDev');
+});
+
+// production task
+gulp.task('prod', ['clean'], function() {
+    gulp.start('css', 'js', 'html', 'images', 'vendorjs', 'vendorcss', 'fonts', 'configProd');
 });
--- frontend/src/js/controllers/webCtrl.js
+++ frontend/src/js/controllers/webCtrl.js
@@ -1,19 +1,58 @@
 // Invoking IIFE for dashboard
+(function() {
 
-(function(){
+    'use strict';
 
-	'use strict';
+    angular
+        .module('evalai')
+        .controller('WebCtrl', WebCtrl);
 
-	angular
-	    .module('evalai')
-	    .controller('WebCtrl', WebCtrl);
+    WebCtrl.$inject = ['utilities', '$state', '$stateParams'];
 
-	WebCtrl.$inject = ['utilities', '$state'];
+    function WebCtrl(utilities, $state, $stateParams) {
+        var vm = this;
 
-    function WebCtrl(utilities, $state){
-    	var vm = this;
-    	
-    	// vm.name = "check"				
+        vm.user = {};
+        vm.countLeft = 0;
+        vm.compPerc = 0;
+        var count = 0;
+
+        // added sidebar box-shadow on scroll
+        angular.element().find(".side-intro").addClass("z-depth-3");
+
+        // get token
+        var userKey = utilities.getData('userKey');
+
+        var parameters = {};
+        parameters.url = 'auth/user/';
+        parameters.method = 'GET';
+        parameters.token = userKey;
+        parameters.callback = {
+            onSuccess: function(response, status) {
+                if (status == 200) {
+                    vm.name = response.username;
+
+                    for (var i in response) {
+                        if (response[i] == "" || response[i] == undefined || response[i] == null) {
+                            response[i] = "-";
+                            vm.countLeft = vm.countLeft + 1;
+                        }
+                        count = count + 1;
+                    }
+                    vm.compPerc = parseInt((vm.countLeft / count) * 100);
+
+                    vm.user = response;
+                    vm.user.complete = 100 - vm.compPerc;
+
+                }
+            },
+            onError: function() {
+
+            }
+        };
+
+        utilities.sendRequest(parameters);
+	    
     }
 
 })();
