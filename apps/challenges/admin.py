2de204bd1eab660ebf11606b52a031c1c144b9fe
gauthamzz <thabeatsz@gmail.com>
2016-11-27 14:59:27 +0530

    Setup ModelAdmin class for each model in Hosts app

--- apps/challenges/admin.py
+++ apps/challenges/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from base.admin import TimeStampedAdmin
+
+from .models import Challenge, Phase
+
+
+@admin.register(Challenge)
+class ChallengeModelAdmin(TimeStampedAdmin):
+    list_display = ("title",
+                    "start_date",
+                    "end_date",
+                    "creator",
+                    "published",
+                    "enable_forum",
+                    "anonymous_leaderboard")
+    list_filter = ("creator",
+                   "published",
+                   "enable_forum",
+                   "anonymous_leaderboard")
+    search_fields = ("title", "creator")
+
+
+@admin.register(Phase)
+class PhaseModelAdmin(TimeStampedAdmin):
+    list_display = ("name", "leaderboard_public", "challenge")
+    list_filter = ("leaderboard_public", "challenge")
+    search_fields = ("name")
--- apps/hosts/admin.py
+++ apps/hosts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from base.admin import TimeStampedAdmin
+
+from .models import ChallengeHostTeam, ChallengeHost
+
+
+@admin.register(ChallengeHostTeam)
+class HostTeamModelAdmin(TimeStampedAdmin):
+    list_display = ("team_name", "created_by")
+    list_filter = ("team_name", "created_by")
+    search_fields = ("team_name", "created_by")
+
+
+@admin.register(ChallengeHost)
+class HostModelAdmin(TimeStampedAdmin):
+    list_display = ("user", "team_name", "status")
+    list_filter = ("user", "team_name", "status")
+    search_fields = ("user", "team_name", "status")
