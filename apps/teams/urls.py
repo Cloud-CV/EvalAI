59bf1c816fad2c71b872a6c265f1ff92b91a1c85
Deshraj <deshrajdry@gmail.com>
2016-11-28 12:14:53 -0500

    swagger urlconf. removed from teams app

--- apps/teams/urls.py
+++ apps/teams/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
     url(r'^$', views.team_list, name='get_team_list'),
     url(r'(?P<pk>[0-9]+)$', views.team_detail, name='get_team_details'),
-    url(r'^documents/$', views.schema_view, name='schema_view'),
 ]
