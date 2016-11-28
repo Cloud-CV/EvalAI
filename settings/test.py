30836014569b60321a39214646369016754796d5
Taranjeet Singh <reachtotj@gmail.com>
2016-11-28 09:50:04 +0530

    Test setup added for hosts app (#160)
    * Settings: Add test settings.
    
    It also updates corresponding references to config files
    for pytest.
    
    * Requirements: Add pytest-django as dependency.
    Pytest: Update pytest config to exclude env folder.
    
    * Hosts: Add test case for get challenge host team.
    
    It also update settings to add TEST_REQUEST_DEFAULT_FORMAT
    key in REST_FRAMEWORK variable.

---
+++ settings/test.py
@@ -0,0 +1,17 @@
+from .common import *  # noqa
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'evalai',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+# E-Mail Settings
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
