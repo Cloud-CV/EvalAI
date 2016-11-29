f15ef7b7c854b1018d162e5b9910297b22143fd7
gauthamzz <thabeatsz@gmail.com>
2016-11-29 16:40:04 +0530

    Challenges admin update

--- apps/challenges/admin.py
+++ apps/challenges/admin.py
@@ -6,14 +6,14 @@ from .models import Challenge, Phase
 
 
 @admin.register(Challenge)
-class ChallengeModelAdmin(TimeStampedAdmin):
+class ChallengeAdmin(TimeStampedAdmin):
     list_display = ("title", "start_date", "end_date", "creator", "published", "enable_forum", "anonymous_leaderboard")
     list_filter = ("creator", "published", "enable_forum", "anonymous_leaderboard")
     search_fields = ("title", "creator")
 
 
 @admin.register(Phase)
-class PhaseModelAdmin(TimeStampedAdmin):
+class PhaseAdmin(TimeStampedAdmin):
     list_display = ("name", "leaderboard_public", "challenge")
     list_filter = ("leaderboard_public", "challenge")
     search_fields = ("name",)
