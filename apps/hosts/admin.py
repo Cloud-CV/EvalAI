270704aa6ff02c273ce635219c31594ee41e045f
gauthamzz <thabeatsz@gmail.com>
2016-11-29 16:18:45 +0530

    Hosts admin

--- apps/hosts/admin.py
+++ apps/hosts/admin.py
@@ -2,19 +2,19 @@ from django.contrib import admin
 
 from base.admin import TimeStampedAdmin
 
-from .models import (ChallengeHostAdmin,
-                     ChallengeHostTeamAdmin)
+from .models import (ChallengeHost,
+                     ChallengeHostTeam)
 
 
-@admin.register(ChallengeHostTeamAdmin)
+@admin.register(ChallengeHostTeam)
 class HostTeamModelAdmin(TimeStampedAdmin):
     list_display = ("team_name", "created_by")
-    list_filter = ("team_name", "created_by")
+    list_filter = ("created_by", )
     search_fields = ("team_name", "created_by")
 
 
-@admin.register(ChallengeHostAdmin)
+@admin.register(ChallengeHost)
 class HostModelAdmin(TimeStampedAdmin):
-    list_display = ("user", "team_name", "status")
-    list_filter = ("user", "team_name", "status")
-    search_fields = ("user", "team_name", "status")
+    list_display = ("user", "team_name", "status", "permissions")
+    list_filter = ("status", "permissions")
+    search_fields = ("user", "team_name")
