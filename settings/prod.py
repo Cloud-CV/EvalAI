c0294fb4cb560f44ee6356a3f3846c918e06509e
Deshraj Yadav <deshraj@vt.edu>
2016-11-29 06:09:40 -0500

    DRFDocs disabled in production environment (#173)

--- settings/prod.py
+++ settings/prod.py
@@ -48,3 +48,8 @@ EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
 EMAIL_PORT = os.environ.get("EMAIL_PORT", "")
 EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "")
+
+# Hide API Docs on production environment
+REST_FRAMEWORK_DOCS = {
+    'HIDE_DOCS': True
+}
