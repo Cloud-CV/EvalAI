a6f272071f930ee3cdd7c38d36a63d6ef139dc7b
rishabhjain <rishabhjain2018@gmail.com>
2016-12-01 19:02:44 +0530

    ModelAdmin Class created for Participants app. (#170)

--- apps/participants/admin.py
+++ apps/participants/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from base.admin import TimeStampedAdmin
+from .models import (
+    ParticipantStatus,
+    Participant,
+    ParticipantTeam,
+    ParticipantTeamMember,
+)
+
+
+@admin.register(ParticipantStatus)
+class ParticipantStatusAdmin(TimeStampedAdmin):
+    list_display = ('status',)
+
+
+@admin.register(Participant)
+class ParticipantAdmin(TimeStampedAdmin):
+    list_display = ('user', 'challenge', 'status',)
+    search_fields = ('user', 'challenge', 'status',)
+    list_filter = ('user', 'challenge', 'status',)
+
+
+@admin.register(ParticipantTeam)
+class ParticipantTeamAdmin(TimeStampedAdmin):
+    list_display = ('challenge', 'team_name',)
+    search_fields = ('challenge',)
+    list_filter = ('team_name', 'challenge',)
+
+
+@admin.register(ParticipantTeamMember)
+class ParticipantTeamMemberAdmin(TimeStampedAdmin):
+    list_display = ('participant', 'team',)
+    search_fields = ('participant',)
+    list_filter = ('participant', 'team',)
