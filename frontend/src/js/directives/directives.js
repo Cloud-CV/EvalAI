1d2b10a62bd449869b4cd69aefc5ee526327feea
itaditya <adityaa803@gmail.com>
2016-12-03 02:22:30 +0530

    fixed #175

--- frontend/src/js/directives/directives.js
+++ frontend/src/js/directives/directives.js
@@ -46,6 +46,77 @@
 })();
 
 
+//Dashboard header
+(function() {
+
+    'use strict'
+
+    // dynamic header directive
+    angular
+        .module('evalai')
+        .directive('simHeader', simHeader);
+
+    function simHeader() {
+        var directive = {
+            link: link,
+            templateUrl: 'dist/views/web/partials/sim-header.html',
+            transclude: true,
+            restrict: 'EA'
+        };
+        return directive;
+
+        function link(scope, element, attrs) {
+
+            function headerComp() {
+                var self = this;
+                this.init();
+            };
+
+            headerComp.prototype = {
+                init: function() {
+                    var self = this;
+
+                    // initialized mobile sidebar
+                    angular.element(".button-collapse").sideNav({
+                        menuWidth: 200,
+                        closeOnClick: true,
+                        draggable: true
+                    });
+
+                }
+            };
+
+            var headerObj = new headerComp();
+        }
+    }
+    
+})();
+
+//Sidebar in Dashboard
+
+(function() {
+
+    'use strict'
+
+    // dynamic header directive
+    angular
+        .module('evalai')
+        .directive('sideBar', sideBar);
+
+    function sideBar() {
+        var directive = {
+            link:function(){
+                //do nothing
+            },
+            templateUrl: 'dist/views/web/partials/sidebar.html',
+            transclude: true,
+            restrict: 'EA'
+        };
+        return directive;
+    }
+    
+})();
+
 // scroll directive
 
 (function() {
