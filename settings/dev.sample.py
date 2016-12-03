4939b5edc5e753aa21b29727acf1983245ad9192
gauthamzz <thabeatsz@gmail.com>
2016-12-03 22:57:32 +0530

    Add team in ParticipantAdmin class in Participant app

---
+++ settings/dev.sample.py
@@ -0,0 +1,32 @@
+from .common import *  # noqa: ignore=F405
+
+# Database
+# https://docs.djangoproject.com/en/1.10.2/ref/settings/#databases
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'evalai',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+# E-Mail Settings
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 1025
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+ACCOUNT_DEFAULT_HTTP_PROTOCOL = 'http'
+
+# DJANGO-SPAGHETTI-AND-MEATBALLS SETTINGS
+INSTALLED_APPS += ['django_spaghetti', ] # noqa: ignore=F405
+
+SPAGHETTI_SAUCE = {
+    'apps': ['auth', 'accounts', 'analytics', 'base', 'challenges', 'hosts', 'jobs', 'participants', 'web'],
+    'show_fields': True,
+}
