26d3255537fb611d326ec1d96015f52a36170478
Deshraj Yadav <deshraj@vt.edu>
2016-12-04 16:25:59 -0500

    Django-autofixture integrated for using  dummy data in development environment (#240)

--- settings/dev.sample.py
+++ settings/dev.sample.py
@@ -24,7 +24,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 ACCOUNT_DEFAULT_HTTP_PROTOCOL = 'http'
 
 # DJANGO-SPAGHETTI-AND-MEATBALLS SETTINGS
-INSTALLED_APPS += ['django_spaghetti', ] # noqa: ignore=F405
+INSTALLED_APPS += [ # noqa: ignore=F405
+    'django_spaghetti',
+    'autofixture'
+]
 
 SPAGHETTI_SAUCE = {
     'apps': ['auth', 'accounts', 'analytics', 'base', 'challenges', 'hosts', 'jobs', 'participants', 'web'],
