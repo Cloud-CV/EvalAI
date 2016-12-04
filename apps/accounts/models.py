251ade2a12c724ef4944ea4e050f82f04fc41211
Taranjeet Singh <reachtotj@gmail.com>
2016-12-04 11:36:09 +0530

    Accounts: Add User Profile model.

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,7 +1,9 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from django.dispatch import receiver
 
 from base.models import (TimeStampedModel,)
 
@@ -52,3 +54,27 @@ class UserAffliation(TimeStampedModel):
     class Meta:
         app_label = 'accounts'
         db_table = 'user_affiliation'
+
+
+class Profile(TimeStampedModel):
+    """
+    Model to store profile of a user
+    """
+    user = models.OneToOneField(User)
+    contact_number = models.CharField(max_length=10, blank=False, null=True)
+    affiliation = models.CharField(max_length=512)
+    receive_participated_challenge_updates = models.BooleanField(default=False)
+    recieve_newsletter = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return '{}'.format(self.user)
+
+    class Meta:
+        app_label = 'accounts'
+        db_table = 'user_profile'
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
