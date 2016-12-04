c46a583105bba2a2b0ec676097eb25115491b720
Deshraj <deshrajdry@gmail.com>
2016-12-03 19:11:58 -0500

    PEP8 fixes in urls

--- apps/participants/urls.py
+++ apps/participants/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'participant_team/(?P<pk>[0-9]+)$', views.participant_team_detail, name='get_participant_team_details'),
     url(r'participant_team/(?P<pk>[0-9]+)/invite$', views.invite_participant_to_team,
         name='invite_participant_to_team'),
-    url(r'participant_team/(?P<participant_team_pk>[0-9]+)/participant/(?P<participant_pk>[0-9]+)$', views.delete_participant_from_team, name='delete_participant_from_team'),
+    url(r'participant_team/(?P<participant_team_pk>[0-9]+)/participant/(?P<participant_pk>[0-9]+)$',
+        views.delete_participant_from_team, name='delete_participant_from_team'),
 ]
