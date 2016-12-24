3b4a52650d24bd649bcaa6f5bacf6c8c77f71e10
anweshknayak <anweshknayak@gmail.com>
2016-12-24 23:53:50 +0530

    minor fix

--- evalai/views.py
+++ evalai/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-# HTTP Error 404
-
 
 def page_not_found(request):
     response = render_to_response('src/views/web/error/404.html',
@@ -10,8 +8,6 @@ def page_not_found(request):
                                   )
     response.status_code = 404
     return response
-
-# HTTP Error 500
 
 
 def server_error(request):
