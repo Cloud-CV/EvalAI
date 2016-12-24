68164e1d931f45f6385b2b3f07c35370425bdc90
Deshraj <deshrajdry@gmail.com>
2016-12-24 06:10:21 -0500

    Migrations updated

--- apps/challenges/migrations/0006_changed_path_to_upload.py
+++ apps/challenges/migrations/0006_changed_path_to_upload.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('challenges', '0006_upload_to_attrbute_added'),
+        ('challenges', '0005_changed_phase_model'),
     ]
 
     operations = [
