7d6fc02fab84dadca990d399852fb1a223a8ed59
Deshraj <deshrajdry@gmail.com>
2016-12-28 18:00:35 -0500

    Nginx added as a separate docker container

--- docker/django/container-start.sh
+++ docker/django/container-start.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 cd /code && python manage.py migrate --noinput && python manage.py collectstatic --noinput
 supervisord -n -c /etc/supervisor/supervisord.conf
-
-# Next line commented since we are not going to use websockets for V1
-# cd /code && daphne -b 0.0.0.0 -p 8001 evalai.asgi:channel_layer
--- docker/nodejs/nginx.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Wait on UWSGI initializtion
-sleep 5
-
-/usr/bin/supervisord -c /etc/supervisor/conf.d/app.conf
