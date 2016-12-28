39a18e8b027ed75a9be7c6ce5bfca098d9614541
RishabhJain2018 <rishabhjain2018@gmail.com>
2016-12-28 20:58:36 +0530

    Setup Throttling in drf backend.

--- settings/common.py
+++ settings/common.py
@@ -232,3 +232,14 @@ LOGGING = {
         }
     }
 }
+
+REST_FRAMEWORK = {
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/minute',
+        'user': '100/minute'
+    }
+}
