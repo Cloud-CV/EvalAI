659e52485dd9dd6b09161127e3465ec5a8475c9a
anweshknayak <anweshknayak@gmail.com>
2016-12-29 15:56:02 +0530

    flakefix

--- evalai/urls.py
+++ evalai/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 
-from django.conf.urls import url, include, handler404, handler500
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic.base import TemplateView
 from django.conf import settings
