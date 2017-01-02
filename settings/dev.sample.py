c17334ea74cb76a4c332f28dd8f8f503bac5b2ff
Deshraj <deshrajdry@gmail.com>
2017-01-01 20:24:37 -0500

    Database credentials changed for development environment in docker container

--- settings/dev.sample.py
+++ settings/dev.sample.py
@@ -7,12 +7,11 @@ DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'evalai',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
