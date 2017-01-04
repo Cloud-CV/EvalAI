f3a943572d0358c8547c6b922040cbaa9b61eb4f
RishabhJain2018 <rishabhjain2018@gmail.com>
2017-01-04 23:03:48 +0530

    flake8 fixes

--- evalai/views.py
+++ evalai/views.py
@@ -2,16 +2,14 @@ from django.shortcuts import render
 
 
 def page_not_found(request):
-    response = render(request,
-                    'src/views/web/error-pages/error-404.html',
+    response = render(request, 'src/views/web/error-pages/error-404.html',
                       )
     response.status_code = 404
     return response
 
 
 def internal_server_error(request):
-    response = render(request,
-                    'src/views/web/error-pages/error-500.html',
+    response = render(request, 'src/views/web/error-pages/error-500.html',
                       )
     response.status_code = 500
     return response
