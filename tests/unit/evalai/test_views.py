8d5578def7394604595d0fb41f8a94e3e0b55540
RishabhJain2018 <rishabhjain2018@gmail.com>
2017-01-05 12:03:30 +0530

    Test for 404 handler added.

--- tests/unit/evalai/test_views.py
+++ tests/unit/evalai/test_views.py
@@ -1,24 +1,17 @@
-from django.test.client import RequestFactory
+from django.test import Client
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
 from evalai.views import page_not_found, internal_server_error
-from evalai import urls
+from evalai.urls import handler404, handler500
 
 
 class TestErrorPages(TestCase):
 
     def test_error_handlers_404(self):
-        self.assertTrue(urls.handler404.endswith('.page_not_found'))
-        factory = RequestFactory()
-        request = factory.get('/')
+        self.assertTrue(handler404.endswith('.page_not_found'))
+        c = Client()
+        request = c.get("/abc")
         response = page_not_found(request)
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(request.status_code, response.status_code)
         self.assertIn('Error 404', unicode(response))
-
-    def test_error_handlers_500(self):
-        self.assertTrue(urls.handler500.endswith('.internal_server_error'))
-        factory = RequestFactory()
-        request = factory.get('/')
-        response = internal_server_error(request)
-        self.assertEqual(response.status_code, 500)
-        self.assertIn('500 Internal Server Error', unicode(response))
