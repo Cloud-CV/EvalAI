c0662a6567d89bcde6daee5545231c220e07207a
anweshknayak <anweshknayak@gmail.com>
2017-01-09 22:51:53 +0530

    indentation fix

--- tests/unit/participants/test_urls.py
+++ tests/unit/participants/test_urls.py
@@ -44,29 +44,26 @@ class TestStringMethods(BaseAPITestClass):
         url = reverse_lazy('participants:get_participant_team_list')
         self.assertEqual(unicode(url), '/api/participants/participant_team')
 
-        url = reverse_lazy('participants:get_participant_team_details', kwargs={
-                           'pk': self.participant_team.pk})
-        self.assertEqual(
-            unicode(url), '/api/participants/participant_team/' + str(self.participant_team.pk))
+        url = reverse_lazy('participants:get_participant_team_details',
+                           kwargs={'pk': self.participant_team.pk})
+        self.assertEqual(unicode(
+            url), '/api/participants/participant_team/' + str(self.participant_team.pk))
 
-        url = reverse_lazy('participants:invite_participant_to_team', kwargs={
-                           'pk': self.participant_team.pk})
+        url = reverse_lazy('participants:invite_participant_to_team',
+                           kwargs={'pk': self.participant_team.pk})
         self.assertEqual(unicode(url), '/api/participants/participant_team/' +
                          str(self.participant_team.pk) + '/invite')
 
         url = reverse_lazy('participants:delete_participant_from_team',
                            kwargs={'participant_team_pk': self.participant_team.pk,
                                    'participant_pk': self.invite_user.pk})
-        self.assertEqual(unicode(url), '/api/participants/participant_team/' + str(
-            self.participant_team.pk) + '/participant/' + str(self.invite_user.pk))
+        self.assertEqual(unicode(url), '/api/participants/participant_team/' + str(self.participant_team.pk) +
+                         '/participant/' + str(self.invite_user.pk))
 
-        url = reverse_lazy(
-            'participants:get_teams_and_corresponding_challenges_for_a_participant')
-        self.assertEqual(
-            unicode(url), '/api/participants/participant_teams/challenges/user')
+        url = reverse_lazy('participants:get_teams_and_corresponding_challenges_for_a_participant')
+        self.assertEqual(unicode(url), '/api/participants/participant_teams/challenges/user')
 
         url = reverse_lazy('participants:remove_self_from_participant_team',
-                           kwargs={'participant_team_pk': self.participant_team.pk
-                                   })
-        self.assertEqual(
-            unicode(url), '/api/participants/remove_self_from_participant_team/' + str(self.participant_team.pk))
+                           kwargs={'participant_team_pk': self.participant_team.pk})
+        self.assertEqual(unicode(url), '/api/participants/remove_self_from_participant_team/' +
+                         str(self.participant_team.pk))
