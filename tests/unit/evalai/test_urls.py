0bf9ef7c004692c4354fc2b8a8316b3b0b4ac8e7
parismita <parismita@iitg.ernet.in>
2017-01-12 18:33:14 +0530

    Update test_urls.py

--- tests/unit/evalai/test_urls.py
+++ tests/unit/evalai/test_urls.py
@@ -1,9 +1,9 @@
+from django.contrib.auth.models import User
+
 from django.contrib.auth.tokens import default_token_generator
 from django.urls import reverse_lazy
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
-
-from django.contrib.auth.models import User
 
 from rest_framework.test import APITestCase, APIClient
 
