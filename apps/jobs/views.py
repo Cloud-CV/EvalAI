dafcf0308b7cfd02e6c284ca6eeadb4ccc588414
Deshraj Yadav <deshraj@vt.edu>
2017-01-13 00:44:28 -0500

    Jobs: Added API endpoint for creating submission to a Challenge Phase (#396)
    * Challenges: property methods added to Challenge and ChallengePhase Model
    
    * Docstring added to each method
    
    * Tests added for Challenge model
    
    * More test cases added
    
    * Jobs: Added API endpoint for making submission to a Challenge Phase
    
    * Minor fixes incorporating the changes suggested
    
    * API endpoint for submission ready
    
    * Unused imports removed
    
    * Minor additions in the business logic
    
    * Tests added for Challenge Submission API
    
    * Unused imports removed from jobs app
    
    * Removed unused modules from tests
    
    * Removed unused modules from views
    
    * Logging statments added for jobs models
    
    * Tests modified for Jobs app

--- apps/jobs/views.py
+++ apps/jobs/views.py
@@ -1,1 +1,73 @@
-# Create your views here.
+from rest_framework import permissions, status
+from rest_framework.decorators import (api_view,
+                                       authentication_classes,
+                                       permission_classes,
+                                       throttle_classes,)
+
+from rest_framework_expiring_authtoken.authentication import (
+    ExpiringTokenAuthentication,)
+from rest_framework.response import Response
+from rest_framework.throttling import UserRateThrottle
+
+from accounts.permissions import HasVerifiedEmail
+from challenges.models import (
+    ChallengePhase,
+    Challenge,)
+from participants.models import (ParticipantTeam,)
+from participants.utils import (
+    get_participant_team_id_of_user_for_a_challenge,)
+
+from .serializers import SubmissionSerializer
+
+
+@throttle_classes([UserRateThrottle])
+@api_view(['POST'])
+@permission_classes((permissions.IsAuthenticated, HasVerifiedEmail))
+@authentication_classes((ExpiringTokenAuthentication,))
+def challenge_submission(request, challenge_id, challenge_phase_id):
+    """API Endpoint for making a submission to a challenge"""
+
+    # check if the challenge exists or not
+    try:
+        challenge = Challenge.objects.get(pk=challenge_id)
+    except Challenge.DoesNotExist:
+        response_data = {'error': 'Challenge does not exist'}
+        return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
+
+    # check if the challenge is active or not
+    if not challenge.is_active:
+        response_data = {'error': 'Challenge is not active'}
+        return Response(response_data, status=status.HTTP_406_NOT_ACCEPTABLE)
+
+    # check if the challenge phase exists or not
+    try:
+        challenge_phase = ChallengePhase.objects.get(
+            pk=challenge_phase_id, challenge=challenge)
+    except ChallengePhase.DoesNotExist:
+        response_data = {'error': 'Challenge Phase does not exist'}
+        return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
+
+    # check if challenge phase is public and accepting solutions
+    if not challenge_phase.is_public:
+        response_data = {
+            'error': 'Sorry, cannot accept submissions since challenge phase is not public'}
+        return Response(response_data, status=status.HTTP_406_NOT_ACCEPTABLE)
+
+    participant_team_id = get_participant_team_id_of_user_for_a_challenge(
+        request.user, challenge_id)
+    try:
+        participant_team = ParticipantTeam.objects.get(pk=participant_team_id)
+    except ParticipantTeam.DoesNotExist:
+        response_data = {'error': 'You haven\'t participated in the challenge'}
+        return Response(response_data, status=status.HTTP_403_FORBIDDEN)
+
+    serializer = SubmissionSerializer(data=request.data,
+                                      context={'participant_team': participant_team,
+                                               'challenge_phase': challenge_phase,
+                                               'request': request
+                                               })
+    if serializer.is_valid():
+        serializer.save()
+        response_data = serializer.data
+        return Response(response_data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
