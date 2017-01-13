a2e0b40702fdcb7a508fc2d8c8ba40d9fe291cc0
RishabhJain2018 <rishabhjain2018@gmail.com>
2017-01-13 18:36:07 +0530

    flake8 fixes

--- apps/jobs/urls.py
+++ apps/jobs/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
         r'challenge_phase/(?P<challenge_phase_id>[0-9]+)/submission/',
         views.challenge_submission, name='challenge_submission'),
     url(r'get_challenge/(?P<challenge_id>[0-9]+)/'
-    	r'get_challenge_phase/(?P<challenge_phase_id>[0-9]+)/submission/',
-    	views.get_challenge_submission, name='get_challenge_submission'),
+        r'get_challenge_phase/(?P<challenge_phase_id>[0-9]+)/submission/',
+        views.get_challenge_submission, name='get_challenge_submission'),
 ]
