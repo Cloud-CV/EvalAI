f26c7287ff708eb7307bd960f9e1854907a1d257
akashjain993 <akash.jain@iaugmentor.com>
2017-01-16 04:07:59 +0530

    Added first flow for participation

--- frontend/src/js/controllers/challengeCtrl.js
+++ frontend/src/js/controllers/challengeCtrl.js
@@ -10,10 +10,11 @@
 
     function ChallengeCtrl(utilities, $scope, $state, $stateParams, $rootScope) {
         var vm = this;
-        $scope.challengeId = $stateParams.challengeId;
+        vm.challengeId = $stateParams.challengeId;
         vm.wrnMsg = {};
         vm.page = {};
-
+        vm.isParticipated = false;
+        var flag = 0;
         vm.phases = {};
         vm.isValid = {};
         var userKey = utilities.getData('userKey');
@@ -22,7 +23,7 @@
 
         // get details of the particular challenge
         var parameters = {};
-        parameters.url = 'challenges/challenge/' + $scope.challengeId + '/';
+        parameters.url = 'challenges/challenge/' + vm.challengeId + '/';
         parameters.method = 'GET';
         parameters.data = {}
         parameters.token = userKey;
@@ -47,7 +48,7 @@
 
         // get details of the particular challenge
         var parameters = {};
-        parameters.url = 'challenges/challenge/' + $scope.challengeId + '/challenge_phase';
+        parameters.url = 'challenges/challenge/' + vm.challengeId + '/challenge_phase';
         parameters.method = 'GET';
         parameters.data = {}
         parameters.token = userKey;
@@ -71,6 +72,47 @@
 
         utilities.sendRequest(parameters);
 
+
+        // get details of the particular challenge
+        var parameters = {};
+        parameters.url = 'participants/participant_teams/challenges/user';
+        parameters.method = 'GET';
+        parameters.data = {}
+        parameters.token = userKey;
+        parameters.callback = {
+            onSuccess: function(response) {
+                var status = response.status;
+                var response = response.data;
+                console.log(response);
+
+                for (var i in response.challenge_participant_team_list) {
+                    console.log
+                    if (response.challenge_participant_team_list[i].challenge != null && response.challenge_participant_team_list[i].challenge.id == vm.challengeId) {
+                        flag = 1;
+                        break;
+                    }
+
+                }
+
+                if (flag == 1) {
+                    vm.isParticipated = false
+                } else {
+                    vm.isParticipated = true;
+                }
+                // vm.phases = response;
+                // navigate to challenge page
+                // $state.go('web.challenge-page.overview');
+                utilities.hideLoader();
+            },
+            onError: function(response) {
+                var status = response.status;
+                var error = response.data;
+                console.log(error);
+                utilities.hideLoader();
+            }
+        };
+
+        utilities.sendRequest(parameters);
 
     }
 })();
