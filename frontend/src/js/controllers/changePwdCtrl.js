443b2296c71c3cb70e4952d9356a8eccfafa7acb
Shiv Baran Singh <spyshiv@users.noreply.github.com>
2017-01-17 02:31:55 +0530

    Frontend(service): Add validation service, Add password check feature in it (#487)

--- frontend/src/js/controllers/changePwdCtrl.js
+++ frontend/src/js/controllers/changePwdCtrl.js
@@ -7,9 +7,9 @@
         .module('evalai')
         .controller('ChangePwdCtrl', ChangePwdCtrl);
 
-    ChangePwdCtrl.$inject = ['utilities', '$state', '$http', '$rootScope'];
+    ChangePwdCtrl.$inject = ['utilities', 'validationSvc', '$state', '$http', '$rootScope'];
 
-    function ChangePwdCtrl(utilities, $state, $http, $rootScope) {
+    function ChangePwdCtrl(utilities, validationSvc, $state, $http, $rootScope) {
         var vm = this;
         var userKey = utilities.getData('userKey');
         vm.wrnMsg = {};
@@ -28,42 +28,47 @@
                 "new_password2": vm.user.new_password2,
             }
             parameters.token = userKey;
-            parameters.callback = {
-                onSuccess: function(response) {
-                    var status = response.status;
-                    var response = response.data;
-                    vm.user.error = false;
-                    console.log("PASSWORD CHANGED SUCCESSFULLY");
-                    console.log(response);
-                    // navigate to challenge page
-                    // $state.go('web.challenge-page.overview');
-                },
-                onError: function(response) {
-                    var status = response.status;
-                    var error = response.data;
-                    vm.user.error = "Failed";
-                    if (status == 400) {
-                        console.log("ERROR Occured");
-                        console.log(error);
-                        angular.forEach(error, function(value, key) {
-                            if (key == 'old_password') {
-                                vm.isValid.old_password = true;
-                                vm.wrnMsg.old_password = value[0];
-                            }
-                            if (key == 'new_password1') {
-                                vm.isValid.new_password1 = true;
-                                vm.wrnMsg.new_password1 = value[0];
-                            }
-                            if (key == 'new_password2' || key == 'non_field_errors') {
-                                vm.isValid.new_password2 = true;
-                                vm.wrnMsg.new_password2 = value[0];
-                            }
-                        })
+            var passwordCheck = validationSvc.password_check(parameters.data.new_password1, parameters.data.new_password2);
+            if (passwordCheck.status) {
+                parameters.callback = {
+                    onSuccess: function(response) {
+                        var status = response.status;
+                        var response = response.data;
+                        vm.user.error = false;
+                        console.log("PASSWORD CHANGED SUCCESSFULLY");
+                        console.log(response);
+                        // navigate to challenge page
+                        // $state.go('web.challenge-page.overview');
+                    },
+                    onError: function(response) {
+                        var status = response.status;
+                        var error = response.data;
+                        vm.user.error = "Failed";
+                        if (status == 400) {
+                            console.log("ERROR Occured");
+                            console.log(error);
+                            angular.forEach(error, function(value, key) {
+                                if (key == 'old_password') {
+                                    vm.isValid.old_password = true;
+                                    vm.wrnMsg.old_password = value[0];
+                                }
+                                if (key == 'new_password1') {
+                                    vm.isValid.new_password1 = true;
+                                    vm.wrnMsg.new_password1 = value[0];
+                                }
+                                if (key == 'new_password2' || key == 'non_field_errors') {
+                                    vm.isValid.new_password2 = true;
+                                    vm.wrnMsg.new_password2 = value[0];
+                                }
+                            })
+                        }
                     }
-                }
-            };
+                };
 
-            utilities.sendRequest(parameters);
+                utilities.sendRequest(parameters);
+            } else {
+                //Assign variable for the view to show errors
+            }
         }
     }
 
