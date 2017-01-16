f7a581557795680875e4e155de043ad9588bddc2
Deshraj <deshrajdry@gmail.com>
2017-01-16 11:19:08 -0500

    Minor changes in frontend

--- frontend/src/js/controllers/challengeCtrl.js
+++ frontend/src/js/controllers/challengeCtrl.js
@@ -14,6 +14,7 @@
         vm.wrnMsg = {};
         vm.page = {};
         vm.isParticipated = false;
+        vm.isActive = false;
         var flag = 0;
         vm.phases = {};
         vm.isValid = {};
@@ -31,7 +32,6 @@
             onSuccess: function(response) {
                 var status = response.status;
                 var response = response.data;
-                console.log(response);
                 vm.page = response;
                 // navigate to challenge page
                 // $state.go('web.challenge-page.overview');
@@ -49,7 +49,7 @@
 
         utilities.sendRequest(parameters);
 
-        // get details of the particular challenge
+        // get details of the particular challenge phase
         var parameters = {};
         parameters.url = 'challenges/challenge/' + vm.challengeId + '/challenge_phase';
         parameters.method = 'GET';
@@ -76,9 +76,9 @@
         utilities.sendRequest(parameters);
 
 
-        // get details of the particular challenge
+        // Check if the challenge is active or not
         var parameters = {};
-        parameters.url = 'participants/participant_teams/challenges/user';
+        parameters.url = 'challenges/challenge/present';
         parameters.method = 'GET';
         parameters.data = {}
         parameters.token = userKey;
@@ -86,29 +86,52 @@
             onSuccess: function(response) {
                 var status = response.status;
                 var response = response.data;
-
-                for (var i in response.challenge_participant_team_list) {
-                    console.log
-                    if (response.challenge_participant_team_list[i].challenge != null && response.challenge_participant_team_list[i].challenge.id == vm.challengeId) {
-                        flag = 1;
+                for (var i in response.results) {
+                    if (response.results[i].id == vm.challengeId) {
+                        vm.isActive = true;
                         break;
                     }
-
                 }
 
-                if (flag == 1) {
-                    vm.isParticipated = false
-                } else {
-                    vm.isParticipated = true;
+                if (vm.isActive == true){
+                    // get details of challenges corresponding to participant teams of that user
+                    var parameters = {};
+                    parameters.url = 'participants/participant_teams/challenges/user';
+                    parameters.method = 'GET';
+                    parameters.data = {}
+                    parameters.token = userKey;
+                    parameters.callback = {
+                        onSuccess: function(response) {
+                            var status = response.status;
+                            var response = response.data;
+
+                            for (var i in response.challenge_participant_team_list) {
+                                if (response.challenge_participant_team_list[i].challenge != null && response.challenge_participant_team_list[i].challenge.id == vm.challengeId) {
+                                    vm.isParticipated = true;
+                                    break;
+                                }
+                            }
+
+                            // vm.phases = response;
+                            // navigate to challenge page
+                            // $state.go('web.challenge-page.overview');
+                            utilities.hideLoader();
+                        },
+                        onError: function(response) {
+                            var status = response.status;
+                            var error = response.data;
+                            utilities.hideLoader();
+                        }
+                    };
+
+                    utilities.sendRequest(parameters);
                 }
-                // vm.phases = response;
-                // navigate to challenge page
-                // $state.go('web.challenge-page.overview');
-                utilities.hideLoader();
             },
             onError: function(response) {
                 var status = response.status;
                 var error = response.data;
+                utilities.storeData('emailError', error.detail);
+                $state.go('web.permission-denied');
                 utilities.hideLoader();
             }
         };
