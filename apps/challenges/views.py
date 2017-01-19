Cloud-CV/EvalAI

commit 5112ed45c6c387cb3a3d282cd90e800225b11adf

Challenges: Remove is_active check since it is added as a property (#532)

apps/challenges/views.py
@@ -202,14 +202,8 @@ def get_challenge_by_pk(request, pk):
     Returns a particular challenge by id
     """
     try:
-        challenge = Challenge.objects.get(pk=pk)
-        if challenge.is_active:
-            serializer = ChallengeSerializer(challenge)
-            response_data = serializer.data
-            return Response(response_data, status=status.HTTP_200_OK)
-        else:
-            response_data = {'error': 'Challenge is not active!'}
-            return Response(response_data, status=status.HTTP_406_NOT_ACCEPTABLE)
+        response_data = Challenge.objects.get(pk=pk)
+        return Response(response_data, status=status.HTTP_200_OK)
     except:
         response_data = {'error': 'Challenge does not exist!'}
         return Response(response_data, status=status.HTTP_406_NOT_ACCEPTABLE)

tests/unit/challenges/test_views.py
@@ -681,66 +681,6 @@ class GetChallengeByPk(BaseAPITestClass):
             end_date=timezone.now() + timedelta(days=1),
         )
 
-    def test_get_challenge_by_pk_when_challenge_exists_and_active(self):
-        self.url = reverse_lazy('challenges:get_challenge_by_pk',
-                                kwargs={'pk': self.challenge3.pk})
-
-        expected = {
-            "id": self.challenge3.pk,
-            "title": self.challenge3.title,
-            "description": self.challenge3.description,
-            "terms_and_conditions": self.challenge3.terms_and_conditions,
-            "submission_guidelines": self.challenge3.submission_guidelines,
-            "evaluation_details": self.challenge3.evaluation_details,
-            "image": None,
-            "start_date": "{0}{1}".format(self.challenge3.start_date.isoformat(), 'Z').replace("+00:00", ""),
-            "end_date": "{0}{1}".format(self.challenge3.end_date.isoformat(), 'Z').replace("+00:00", ""),
-            "creator": {
-                "id": self.challenge3.creator.pk,
-                "team_name": self.challenge3.creator.team_name,
-                "created_by": self.challenge3.creator.created_by.username,
-            },
-            "published": self.challenge3.published,
-            "enable_forum": self.challenge3.enable_forum,
-            "anonymous_leaderboard": self.challenge3.anonymous_leaderboard,
-            "is_active": True,
-        }
-
-        response = self.client.get(self.url, {})
-        self.assertEqual(response.data, expected)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_get_challenge_by_pk_when_challenge_exists_in_past_and_not_active(self):
-        self.url = reverse_lazy('challenges:get_challenge_by_pk',
-                                kwargs={'pk': self.challenge3.pk})
-
-        self.challenge3.end_date = timezone.now() - timedelta(days=1)
-        self.challenge3.save()
-
-        expected = {
-            'error': 'Challenge is not active!'
-        }
-
-        response = self.client.get(self.url, {})
-        self.assertEqual(response.data, expected)
-        self.assertEqual(response.status_code, status.HTTP_406_NOT_ACCEPTABLE)
-
-    def test_get_challenge_by_pk_when_challenge_will_exist_in_future_and_not_active(self):
-        self.url = reverse_lazy('challenges:get_challenge_by_pk',
-                                kwargs={'pk': self.challenge3.pk})
-
-        self.challenge3.start_date = timezone.now() + timedelta(days=1)
-        self.challenge3.end_date = timezone.now() + timedelta(days=3)
-        self.challenge3.save()
-
-        expected = {
-            'error': 'Challenge is not active!'
-        }
-
-        response = self.client.get(self.url, {})
-        self.assertEqual(response.data, expected)
-        self.assertEqual(response.status_code, status.HTTP_406_NOT_ACCEPTABLE)
-
     def test_get_challenge_by_pk_when_challenge_does_not_exists(self):
         self.url = reverse_lazy('challenges:get_challenge_by_pk',
                                 kwargs={'pk': self.challenge3.pk + 10})

commit c65dd07081d6e157b52c2edb03d220f9ebbad88e

Challenges: Minor fix to resolve the bug introduced in #532

apps/challenges/views.py
@@ -202,7 +202,9 @@ def get_challenge_by_pk(request, pk):
     Returns a particular challenge by id
     """
     try:
-        response_data = Challenge.objects.get(pk=pk)
+        challenge = Challenge.objects.get(pk=pk)
+        serializer = ChallengeSerializer(challenge)
+        response_data = serializer.data
         return Response(response_data, status=status.HTTP_200_OK)
     except:
         response_data = {'error': 'Challenge does not exist!'}