9d7241b45a6b25769e4a2c32b06b01951dcc7edb
Deshraj Yadav <deshraj@vt.edu>
2017-01-20 18:38:16 -0500

    Participants: Participant Team Name made unique (#543)

--- apps/participants/models.py
+++ apps/participants/models.py
@@ -38,7 +38,7 @@ class ParticipantTeam(TimeStampedModel):
     """
     Model representing the Teams associated with different challenges
     """
-    team_name = models.CharField(max_length=100)
+    team_name = models.CharField(max_length=100, unique=True)
     created_by = models.ForeignKey(User, null=True)
 
     def __str__(self):
