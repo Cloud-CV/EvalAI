3130ecc946c6a0cc971a2d1167262de47c96868e
rishabhjain <rishabhjain2018@gmail.com>
2017-01-23 03:29:41 +0530

    Contact Message: API Endpoint for Submitting a Contact Message. (#580)
    * Contact Message: API Endpoint for Submitting a Contact Message.
    
    * Support of name & email for logged in users is added.
    
    * review changes fixed.

--- apps/web/urls.py
+++ apps/web/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url
-from .views import (
-    home,
-)
+
+from . import views
 
 urlpatterns = [
-    url(r'^$', home, name="home"),
+    url(r'contact/$', views.contact_us, name='contact_us'),
 ]
