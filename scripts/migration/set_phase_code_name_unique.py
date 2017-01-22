9fdfeb0fb1f1a2eb459c5e0cc5c2e01dc5cd6135
Deshraj Yadav <deshraj@vt.edu>
2017-01-21 21:15:53 -0500

    Script added to make challenge phase code name unique (#556)
    * Challenges: Added code_name field in ChallengePhase model
    
    * Challenge Phase Code Name and Challenge Id made unique together
    
    * Script added to make challenge phase code name unique

---
+++ scripts/migration/set_phase_code_name_unique.py
@@ -0,0 +1,21 @@
+# Command to run: python manage.py shell < scripts/migration/set_phase_code_name_unique.py
+
+from challenges.models import ChallengePhase
+
+
+def set_phase_code_name_unique():
+
+    challenge_phases = ChallengePhase.objects.all()
+    try:
+        for challenge_phase in challenge_phases:
+            new_phase_code_name = "{0}_{1}".format(
+                challenge_phase.code_name, challenge_phase.id)
+            print ("Modifying Challenge Phase Code Name: `%s` --> `%s` ") % (
+                challenge_phase.code_name, new_phase_code_name)
+            challenge_phase.code_name = new_phase_code_name
+            challenge_phase.save()
+            print ("Successfully modified Challenge Phase Code Name")
+    except Exception, e:
+        print str(e)
+
+set_phase_code_name_unique()
