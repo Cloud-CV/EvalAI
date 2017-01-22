6b95eb63aa4623bea99e67d2653c17481376e6ac
rishabhjain <rishabhjain2018@gmail.com>
2017-01-23 03:29:41 +0530

    Contact Message: API Endpoint for Submitting a Contact Message. (#580)
    * Contact Message: API Endpoint for Submitting a Contact Message.
    
    * Support of name & email for logged in users is added.
    
    * review changes fixed.

--- apps/web/views.py
+++ apps/web/views.py
@@ -1,4 +1,14 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+
+from rest_framework import permissions, status
+from rest_framework.decorators import (api_view,
+                                       permission_classes,
+                                       throttle_classes,)
+from rest_framework.response import Response
+from rest_framework.throttling import AnonRateThrottle
+
+from .serializers import ContactSerializer
 
 
 def home(request, template_name="index.html"):
@@ -6,3 +16,28 @@ def home(request, template_name="index.html"):
     Home Page View
     """
     return render(request, template_name)
+
+
+@throttle_classes([AnonRateThrottle, ])
+@api_view(['POST', ])
+@permission_classes((permissions.AllowAny,))
+def contact_us(request):
+    try:
+        user = User.objects.get(username=request.user)
+        name = user.username
+        email = user.email
+        request_data = {"name": name, "email": email}
+        request_data['message'] = request.data['message']
+        serializer = ContactSerializer(data=request_data)
+        if serializer.is_valid():
+            serializer.save()
+            response_data = {'message': 'Your message has been successfully recorded. We will contact you shortly.'}
+            return Response(response_data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    except:
+        serializer = ContactSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            response_data = {'message': 'Your message has been successfully recorded. We will contact you shortly.'}
+            return Response(response_data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
