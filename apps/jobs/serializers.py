27a07b09a8925d024e10c6ab27d76b6ce5b31036
Deshraj <deshrajdry@gmail.com>
2017-01-24 02:31:02 -0500

    Created an initial version of leaderboard

--- apps/jobs/serializers.py
+++ apps/jobs/serializers.py
@@ -41,9 +41,7 @@ class SubmissionSerializer(serializers.ModelSerializer):
 class LeaderboardSerializer(serializers.ModelSerializer):
 
     participant_team_name = serializers.SerializerMethodField()
-    created_by = serializers.SerializerMethodField()
     challenge_phase_name = serializers.SerializerMethodField()
-    execution_time = serializers.SerializerMethodField()
     final_result = serializers.SerializerMethodField()
 
     def __init__(self, *args, **kwargs):
@@ -51,20 +49,13 @@ class LeaderboardSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Submission
-        fields = ('participant_team', 'participant_team_name', 'challenge_phase', 'challenge_phase_name',
-            'created_by', 'status', 'output', 'id', 'execution_time', 'final_result')
+        fields = ('participant_team_name', 'challenge_phase_name', 'final_result')
 
     def get_participant_team_name(self, obj):
         return obj.participant_team.team_name
 
-    def get_created_by(self, obj):
-        return obj.created_by.username
-
     def get_challenge_phase_name(self, obj):
         return obj.challenge_phase.name
-
-    def get_execution_time(self, obj):
-        return obj.execution_time
 
     def get_final_result(self, obj):
         try:
