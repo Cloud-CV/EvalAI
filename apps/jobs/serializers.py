feb442db6f6d5cb7b4e5b0c3558e6589b43d2465
Deshraj Yadav <deshraj@vt.edu>
2017-01-23 20:26:41 -0500

    Submissions: Leaderboard Serializer added (#590)
    * Requirements: Added PyYaml as dependency (#585)
    
    * Frontend: Timeout removed from requests (#586)
    
    * Jobs: Add execution time property in Submission model
    
    * Leaderboard View added
    
    * Leaderboard serializer added

--- apps/jobs/serializers.py
+++ apps/jobs/serializers.py
@@ -1,4 +1,8 @@
+import yaml
+
 from rest_framework import serializers
+
+from participants.serializers import ParticipantTeamSerializer
 
 from .models import Submission
 
@@ -22,3 +26,38 @@ class SubmissionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Submission
         fields = ('participant_team', 'challenge_phase', 'created_by', 'status', 'input_file')
+
+
+class LeaderboardSerializer(serializers.ModelSerializer):
+
+    participant_team_name = serializers.SerializerMethodField()
+    created_by = serializers.SerializerMethodField()
+    challenge_phase_name = serializers.SerializerMethodField()
+    execution_time = serializers.SerializerMethodField()
+    final_result = serializers.SerializerMethodField()
+
+    def __init__(self, *args, **kwargs):
+        super(LeaderboardSerializer, self).__init__(*args, **kwargs)
+
+    class Meta:
+        model = Submission
+        fields = ('participant_team', 'participant_team_name', 'challenge_phase', 'challenge_phase_name',
+            'created_by', 'status', 'output', 'id', 'execution_time', 'final_result')
+
+    def get_participant_team_name(self, obj):
+        return obj.participant_team.team_name
+
+    def get_created_by(self, obj):
+        return obj.created_by.username
+
+    def get_challenge_phase_name(self, obj):
+        return obj.challenge_phase.name
+
+    def get_execution_time(self, obj):
+        return obj.execution_time
+
+    def get_final_result(self, obj):
+        try:
+            return yaml.safe_load(obj.output)
+        except:
+            return None
