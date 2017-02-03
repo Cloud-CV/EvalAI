b92d09df564d20ce8dc9b72925787a58945d101c
Deshraj Yadav <deshraj@vt.edu>
2017-02-03 09:33:23 -0500

    Challenges: DatasetSplit added as many to many field in Challenge Phase (#620)
    * Challenges: DatasetSplit model added
    
    * Challenges: DatasetSplit added as many to many field in Challenge Phase

--- apps/challenges/models.py
+++ apps/challenges/models.py
@@ -98,6 +98,7 @@ class ChallengePhase(TimeStampedModel):
     max_submissions_per_day = models.PositiveIntegerField(default=100000)
     max_submissions = models.PositiveIntegerField(default=100000)
     codename = models.CharField(max_length=100, default="Phase Code Name")
+    dataset_split = models.ManyToManyField(DatasetSplit, blank=True)
 
     class Meta:
         app_label = 'challenges'
