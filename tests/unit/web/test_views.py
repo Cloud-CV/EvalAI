b28c2033b7cc0089a31548fdcf2f347bf160109a
arun jain <jainarun889@gmail.com>
2017-02-05 06:42:04 -0800

    Web: Added Custom 404 and 500 Error Page (#631)
    * Custom Error Pages
    
    * fixflakes
    
    * flakefix
    
    * added_test_case
    
    * fixflake

--- tests/unit/web/test_views.py
+++ tests/unit/web/test_views.py
@@ -5,6 +5,12 @@ from rest_framework.test import APITestCase, APIClient
 
 from web.models import Contact
 
+from django.test import Client
+from django.test import TestCase
+
+from web.views import page_not_found, internal_server_error
+from evalai.urls import handler404, handler500
+
 
 class BaseAPITestCase(APITestCase):
 
@@ -37,3 +43,18 @@ class CreateContactMessage(BaseAPITestCase):
         del self.data['message']
         response = self.client.post(self.url, self.data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class TestErrorPages(TestCase):
+
+    def test_error_handlers(self):
+        self.assertTrue(handler404.endswith('.page_not_found'))
+        self.assertTrue(handler500.endswith('.internal_server_error'))
+        c = Client()
+        request = c.get("/abc")
+        response = page_not_found(request)
+        self.assertEqual(response.status_code, 404)
+        self.assertIn('Error 404', unicode(response))
+        response = internal_server_error(request)
+        self.assertEqual(response.status_code, 500)
+        self.assertIn('Error 500', unicode(response))
