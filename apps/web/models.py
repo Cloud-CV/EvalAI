19ff193877e488587911c335d57bbe4839195756
Ankit Yadav <connectankityadav@gmail.com>
2017-02-06 16:35:03 +0530

    requested changes

--- apps/web/models.py
+++ apps/web/models.py
@@ -7,9 +7,9 @@ from base.models import (TimeStampedModel, )
 
 class Contact(TimeStampedModel):
     """Model representing details of User submitting queries."""
-    name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=70)
-    message = models.CharField(max_length=500)
+    name = models.CharField(max_length=100,)
+    email = models.EmailField(max_length=70,)
+    message = models.CharField(max_length=500,)
 
     def __unicode__(self):
         return "%s: %s: %s" % (self.name, self.email, self.message)
