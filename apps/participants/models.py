6b135ab14f191064bca6d21b922ab909c85aa461
Sourav Singh <souravsingh@users.noreply.github.com>
2017-02-08 03:50:32 +0530

    Update models.py

--- apps/participants/models.py
+++ apps/participants/models.py
@@ -7,7 +7,9 @@ from base.models import (TimeStampedModel,)
 
 
 class Participant(TimeStampedModel):
-
+    """
+    Model representing the Participant of the competition. 
+    """
     UNKNOWN = 'Unknown'
     SELF = 'Self'
     DENIED = 'Denied'
