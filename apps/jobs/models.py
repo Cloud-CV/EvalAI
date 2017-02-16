01a0b5dab86f511d6589fe61c2cbe63c63eef360
RishabhJain2018 <rishabhjain2018@gmail.com>
2017-02-16 22:47:24 +0530

    Added Migration Files.

--- apps/jobs/models.py
+++ apps/jobs/models.py
@@ -16,6 +16,26 @@ from participants.models import ParticipantTeam
 
 logger = logging.getLogger(__name__)
 
+# Whenever the migrations are being squashed, Please remove these functions.
+# Reason: During migrating django looks for its previous state, 
+#         hence these functions were not removed to prevent this error.
+# "AttributeError: 'module' object has no attribute 'input_file_name'" 
+
+def submission_root(instance):
+    return join('submission_files', 'submission_' + str(instance.pk))
+
+
+def input_file_name(instance, filename='input.txt'):
+    return join(submission_root(instance), filename)
+
+
+def stdout_file_name(instance, filename='stdout.txt'):
+    return join(submission_root(instance), filename)
+
+
+def stderr_file_name(instance, filename='stderr.txt'):
+    return join(submission_root(instance), filename)
+
 
 class Submission(TimeStampedModel):
 
