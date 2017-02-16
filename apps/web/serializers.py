c6673bd4dee893eea1c2d0c84f1e2fa5390987c4
Ankit Yadav <connectankityadav@gmail.com>
2017-02-17 05:10:26 +0530

    flake8 fixes

--- apps/web/serializers.py
+++ apps/web/serializers.py
@@ -14,4 +14,5 @@ class TeamSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Team
-        fields = ('name', 'description', 'headshot', 'background_image', 'github_url', 'linkedin_url', 'personal_website', 'team_type')
+        fields = ('name', 'description', 'headshot', 'background_image', 'github_url', 'linkedin_url',
+                  'personal_website', 'team_type')
