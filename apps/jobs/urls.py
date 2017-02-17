62aa5759a9f5c87a766d0a8f74108ed55dee4f9c
Deshraj Yadav <deshraj@vt.edu>
2017-02-17 04:46:59 -0500

    Challenges: Leaderboard updated according to the new schema (#685)
    * Challenges: Added visibility in ChallengePhaseSplit Serializer
    
    * Challenges: Leaderboard updated according to the new schema
    
    * Minor changes in the filtering logic
    
    * leaderboard fixes added according to the new LeaderboardData model

--- apps/jobs/urls.py
+++ apps/jobs/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     url(r'challenge/(?P<challenge_id>[0-9]+)/'
         r'challenge_phase/(?P<challenge_phase_id>[0-9]+)/submission/',
         views.challenge_submission, name='challenge_submission'),
-    url(r'challenge/(?P<challenge_id>[0-9]+)/'
-        r'challenge_phase/(?P<challenge_phase_id>[0-9]+)/leaderboard/',
+    url(r'challenge_phase_split/(?P<challenge_phase_split_id>[0-9]+)/leaderboard/',
         views.leaderboard, name='leaderboard'),
 ]
