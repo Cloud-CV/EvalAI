0eb13530684848769574ce56c7fa184ec7ab8d38
rishabhjain <rishabhjain2018@gmail.com>
2017-02-21 11:08:26 +0530

    Settings: Setting default renderer to JSONRenderer in DRF backend. (#697)
    *  Settings: Setting default renderer to JSONRenderer in DRF backend.
    
    * flake8 fixes

--- settings/common.py
+++ settings/common.py
@@ -163,7 +163,10 @@ REST_FRAMEWORK = {
     'DEFAULT_THROTTLE_RATES': {
         'anon': '100/minute',
         'user': '100/minute'
-    }
+    },
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
 }
 
 # ALLAUTH SETTINGS
