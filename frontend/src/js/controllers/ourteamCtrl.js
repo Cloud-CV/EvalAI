4fbfe951bef0e1a525ebd2c5d82cd7e9f31107d7
RishabhJain2018 <rishabhjain2018@gmail.com>
2017-03-03 17:40:30 +0530

    Review Errors Fixed.

--- frontend/src/js/controllers/ourteamCtrl.js
+++ frontend/src/js/controllers/ourteamCtrl.js
@@ -21,25 +21,29 @@
             onSuccess: function(response) {
                 var status = response.status;
                 var results = response.data;
-                if ((status == 200) && results.length) {
-                    var coreTeamList = [];
-                    var contributingTeamList = [];
-                    for (var i = 0; i < results.length; i++) {
-                        if (results[i].team_type === "Core Team") {
-                            vm.coreTeamType = results[i].team_type;
-                            vm.coreTeamList = coreTeamList.push(results[i]);
+                if (status == 200) {
+                    if (results.length !== 0) {
+                        var coreTeamList = [];
+                        var contributingTeamList = [];
+                        for (var i = 0; i < results.length; i++) {
+                            if (results[i].team_type === "Core Team") {
+                                vm.coreTeamType = results[i].team_type;
+                                vm.coreTeamList = coreTeamList.push(results[i]);
 
-                        } else if (results[i].team_type === "Contributor") {
-                            vm.contributingTeamType = results[i].team_type;
-                            vm.contributingTeamList = contributingTeamList.push(results[i]);
+                            } else if (results[i].team_type === "Contributor") {
+                                vm.contributingTeamType = results[i].team_type;
+                                vm.contributingTeamList = contributingTeamList.push(results[i]);
+                            }
+                            vm.coreTeamDetails = coreTeamList;
+                            vm.contributingTeamDetails = contributingTeamList;
                         }
-                        vm.coreTeamDetails = coreTeamList;
-                        vm.contributingTeamDetails = contributingTeamList;
+                    } else {
+                        vm.noTeamDisplay = "Team will be updated very soon !";
                     }
                 }
-                else {
-                    vm.noTeamDisplay = "Team will be updated very soon !"
-                }
+            },
+            onError: function(response) {
+                vm.noTeamDisplay = "Team will be updated very soon !";
             }
         };
 
