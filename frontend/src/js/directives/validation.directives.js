67c08d96f55bde11ca335e0e21f0ed951b154a57
Shivam <shivam@ziploan.in>
2017-03-04 14:49:13 +0530

    Added validation for new password

--- frontend/src/js/directives/validation.directives.js
+++ frontend/src/js/directives/validation.directives.js
@@ -16,7 +16,26 @@ var compareTo = function() {
         }
     };
 };
+var match = function() {
+    return {
+        require: "ngModel",
+        scope: {
+            otherModelValue: "=match"
+        },
+        link: function(scope, element, attributes, ngModel) {
+
+            ngModel.$validators.match = function(modelValue) {
+                return modelValue != scope.otherModelValue;
+            };
+
+            scope.$watch("otherModelValue", function() {
+                ngModel.$validate();
+            });
+        }
+    };
+};
 
 angular
     .module('evalai')
-    .directive("compareTo", compareTo);
+    .directive("compareTo", compareTo)
+    .directive("match", match);
