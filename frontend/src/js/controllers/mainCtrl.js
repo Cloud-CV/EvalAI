5b7247b24f6b25cdd3b4feca49c68de5275369fd
Akash Jain <akashjain993@gmail.com>
2017-03-27 02:48:48 +0530

    Frontend: Add Challenge Details to home page (#873)
    * Added Challenge list to home page
    
    * Fixed alignemnt issue

--- frontend/src/js/controllers/mainCtrl.js
+++ frontend/src/js/controllers/mainCtrl.js
@@ -14,12 +14,45 @@
         var vm = this;
 
         vm.user = {};
+        vm.challengeList = {};
+        vm.challengeCount = 0;
+        vm.isMore = false;
 
         // get token
         var userKey = utilities.getData('userKey');
 
+        // getting challenge (unauthorized)
+        var parameters = {};
+
+        parameters.url = 'challenges/challenge/present';
+        parameters.method = 'GET';
+
+        parameters.token = null;
+        parameters.callback = {
+            onSuccess: function(response) {
+                var details = response.data;
+                vm.challengeList = details;
+                if (vm.challengeList.count > 10) {
+                    vm.challengeCount = '10+';
+                } else {
+                    vm.challengeCount = vm.challengeList.count;
+                }
+
+                if (vm.challengeList.results[0].description.length > 120) {
+                    vm.isMore = true;
+                } else {
+                    vm.isMore = false;
+                }
+            },
+            onError: function() {
+            }
+        };
+
+        utilities.sendRequest(parameters);
+
+        // check for authenticated user
         if (userKey) {
-            var parameters = {};
+            parameters = {};
             parameters.url = 'auth/user/';
             parameters.method = 'GET';
             parameters.token = userKey;
@@ -35,7 +68,6 @@
 
                     var status = response.status;
                     if (status == 401) {
-                        alert("");
                         utilities.resetStorage();
                         $state.go("auth.login");
                         $rootScope.isAuth = false;
@@ -44,6 +76,8 @@
             };
 
             utilities.sendRequest(parameters);
+
+
         }
         vm.hostChallenge = function() {
 
