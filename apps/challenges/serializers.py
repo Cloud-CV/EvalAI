7ccab103fbebf6796b3d9e8c2985c76c69e4242c
Deshraj <deshrajdry@gmail.com>
2017-03-26 21:35:58 -0400

    Challenge: Add short_description in Challenge Serializer

--- apps/challenges/serializers.py
+++ apps/challenges/serializers.py
@@ -24,7 +24,7 @@ class ChallengeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Challenge
-        fields = ('id', 'title', 'description', 'terms_and_conditions',
+        fields = ('id', 'title', 'short_description', 'description', 'terms_and_conditions',
                   'submission_guidelines', 'evaluation_details',
                   'image', 'start_date', 'end_date', 'creator',
                   'published', 'enable_forum', 'anonymous_leaderboard', 'is_active',)
