c941533a3b0113a7776c303ca99ad5b234760033
Deshraj Yadav <deshraj@vt.edu>
2017-04-03 09:57:58 -0400

    Jobs: Add API endpoint to change the visibility of submissions (#904)

--- apps/jobs/urls.py
+++ apps/jobs/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     url(r'challenge/(?P<challenge_id>[0-9]+)/'
+        r'challenge_phase/(?P<challenge_phase_id>[0-9]+)/submission/(?P<submission_id>[0-9]+)',
+        views.change_submission_visibility, name='change_submission_visibility'),
+    url(r'challenge/(?P<challenge_id>[0-9]+)/'
         r'challenge_phase/(?P<challenge_phase_id>[0-9]+)/submission/',
         views.challenge_submission, name='challenge_submission'),
     url(r'challenge_phase_split/(?P<challenge_phase_split_id>[0-9]+)/leaderboard/',
