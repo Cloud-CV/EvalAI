0b077c7f1eff52f1892ed6445642d8a117dd4620
Deshraj <deshrajdry@gmail.com>
2017-04-04 23:01:00 -0400

    Jobs: Remove submission_metadata from Submission serializer

--- apps/jobs/serializers.py
+++ apps/jobs/serializers.py
@@ -29,7 +29,7 @@ class SubmissionSerializer(serializers.ModelSerializer):
         fields = ('id', 'participant_team', 'participant_team_name', 'execution_time', 'challenge_phase',
                   'created_by', 'status', 'input_file', 'stdout_file', 'stderr_file', 'submitted_at',
                   'method_name', 'method_description', 'project_url', 'publication_url', 'is_public',
-                  'submission_result_file', 'submission_metadata_file')
+                  'submission_result_file',)
 
     def get_participant_team_name(self, obj):
         return obj.participant_team.team_name
