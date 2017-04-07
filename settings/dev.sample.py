f6e6abac30924dda7cf8e46f319d894ae03af880
Varun Agrawal <varagrawal@gmail.com>
2017-04-07 12:44:40 -0400

    Docker: ignore flake8 F405 errors in os.environ lines in dev.sample.py

--- settings/dev.sample.py
+++ settings/dev.sample.py
@@ -8,11 +8,11 @@ DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get("POSTGRES_NAME", 'evalai'),
-        'USER': os.environ.get("POSTGRES_USER", 'postgres'),
-        'PASSWORD': os.environ.get("POSTGRES_PASSWORD", 'postgres'),
-        'HOST': os.environ.get("POSTGRES_HOST", 'localhost'),
-        'PORT': os.environ.get("POSTGRES_PORT", 5432),
+        'NAME': os.environ.get("POSTGRES_NAME", 'evalai'),  # noqa: ignore=F405
+        'USER': os.environ.get("POSTGRES_USER", 'postgres'),  # noqa: ignore=F405
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD", 'postgres'),  # noqa: ignore=F405
+        'HOST': os.environ.get("POSTGRES_HOST", 'localhost'),  # noqa: ignore=F405
+        'PORT': os.environ.get("POSTGRES_PORT", 5432),  # noqa: ignore=F405
     }
 }
 
