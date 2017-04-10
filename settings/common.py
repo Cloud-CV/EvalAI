af4966a937b14d480b7d85b28ad33888c4cbfd66
Mayank Agrawal <agarwalm214@gmail.com>
2017-04-10 16:17:19 +0530

    Accounts: Create view for successful email verification after Signup (#860)

--- settings/common.py
+++ settings/common.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = "/media/"
 
