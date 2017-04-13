b1eb43a68bb3853715b33058282af5335ffa7056
Ankit Yadav <connectankityadav@gmail.com>
2017-04-13 14:11:59 +0530

    alter help text

--- apps/base/management/commands/seed.py
+++ apps/base/management/commands/seed.py
@@ -3,7 +3,7 @@ from django.core.management import BaseCommand, call_command
 
 class Command(BaseCommand):
 
-    help = "Seeds the database for you"
+    help = "Seeds the database with random but sensible values."
 
     def handle(self, *args, **options):
         self.stdout.write(self.style.SUCCESS('Starting the database seeder. Hang on...'))
