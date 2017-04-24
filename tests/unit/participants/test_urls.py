91845a025f4ae601e8f5ad520036ab851d8e94d4
Deshraj Yadav <deshraj@vt.edu>
2017-04-24 05:39:52 -0400

    Participants: Add URL tests for Participants app and reorder urlpatterns	 (#963)

--- tests/unit/participants/test_urls.py
+++ tests/unit/participants/test_urls.py
@@ -1,9 +1,14 @@
-from django.urls import reverse_lazy
+from datetime import timedelta
+
+from django.urls import reverse_lazy, resolve
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 from allauth.account.models import EmailAddress
 from rest_framework.test import APITestCase, APIClient
 
+from challenges.models import Challenge
+from hosts.models import ChallengeHost, ChallengeHostTeam
 from participants.models import ParticipantTeam
 
 
@@ -32,35 +37,89 @@ class BaseAPITestClass(APITestCase):
             team_name='Participant Team',
             created_by=self.user)
 
+        # user who create a challenge host team
+        self.user2 = User.objects.create(
+            username='someuser2',
+            password='some_secret_password')
+
+        self.challenge_host_team = ChallengeHostTeam.objects.create(
+            team_name='Some Test Challenge Host Team',
+            created_by=self.user2)
+
+        self.challenge_host2 = ChallengeHost.objects.create(
+            user=self.user2,
+            team_name=self.challenge_host_team,
+            status=ChallengeHost.ACCEPTED,
+            permissions=ChallengeHost.ADMIN)
+
+        self.challenge = Challenge.objects.create(
+            title='Some Test Challenge',
+            short_description='Short description for some test challenge',
+            description='Description for some test challenge',
+            terms_and_conditions='Terms and conditions for some test challenge',
+            submission_guidelines='Submission guidelines for some test challenge',
+            creator=self.challenge_host_team,
+            published=False,
+            enable_forum=True,
+            anonymous_leaderboard=False,
+            start_date=timezone.now() - timedelta(days=2),
+            end_date=timezone.now() + timedelta(days=1),
+        )
         self.client.force_authenticate(user=self.user)
 
 
 class TestStringMethods(BaseAPITestClass):
 
-    def test_participants_urls(self):
-        url = reverse_lazy('participants:get_participant_team_list')
-        self.assertEqual(unicode(url), '/api/participants/participant_team')
+    def test_participant_team_list_url(self):
+        self.url = reverse_lazy('participants:get_participant_team_list')
+        self.assertEqual(unicode(self.url),
+                         '/api/participants/participant_team')
+        resolver = resolve(self.url)
+        self.assertEqual(resolver.view_name,
+                         'participants:get_participant_team_list')
 
-        url = reverse_lazy('participants:get_participant_team_details',
-                           kwargs={'pk': self.participant_team.pk})
+    def test_participant_team_detail_url(self):
+        self.url = reverse_lazy('participants:get_participant_team_details',
+                                kwargs={'pk': self.participant_team.pk})
         self.assertEqual(unicode(
-            url), '/api/participants/participant_team/' + str(self.participant_team.pk))
+            self.url), '/api/participants/participant_team/%s' % (self.participant_team.pk))
+        resolver = resolve(self.url)
+        self.assertEqual(resolver.view_name,
+                         'participants:get_participant_team_details')
 
-        url = reverse_lazy('participants:invite_participant_to_team',
-                           kwargs={'pk': self.participant_team.pk})
-        self.assertEqual(unicode(url), '/api/participants/participant_team/' +
-                         str(self.participant_team.pk) + '/invite')
+    def test_invite_participant_to_team_url(self):
+        self.url = reverse_lazy('participants:invite_participant_to_team',
+                                kwargs={'pk': self.participant_team.pk})
+        self.assertEqual(unicode(
+            self.url), '/api/participants/participant_team/%s/invite' % (self.participant_team.pk))
+        resolver = resolve(self.url)
+        self.assertEqual(resolver.view_name,
+                         'participants:invite_participant_to_team')
 
-        url = reverse_lazy('participants:delete_participant_from_team',
-                           kwargs={'participant_team_pk': self.participant_team.pk,
-                                   'participant_pk': self.invite_user.pk})
-        self.assertEqual(unicode(url), '/api/participants/participant_team/' + str(self.participant_team.pk) +
-                         '/participant/' + str(self.invite_user.pk))
+    def test_delete_participant_from_team_url(self):
+        self.url = reverse_lazy('participants:delete_participant_from_team',
+                                kwargs={'participant_team_pk': self.participant_team.pk,
+                                        'participant_pk': self.invite_user.pk})
+        self.assertEqual(unicode(self.url), '/api/participants/participant_team/%s/participant/%s' %
+                         (self.participant_team.pk, self.invite_user.pk))
+        resolver = resolve(self.url)
+        self.assertEqual(resolver.view_name,
+                         'participants:delete_participant_from_team')
 
-        url = reverse_lazy('participants:get_teams_and_corresponding_challenges_for_a_participant')
-        self.assertEqual(unicode(url), '/api/participants/participant_teams/challenges/user')
+    def test_get_teams_and_corresponding_challenges_for_a_participant_url(self):
+        self.url = reverse_lazy(
+            'participants:get_teams_and_corresponding_challenges_for_a_participant')
+        self.assertEqual(
+            unicode(self.url), '/api/participants/participant_teams/challenges/user')
+        resolver = resolve(self.url)
+        self.assertEqual(
+            resolver.view_name, 'participants:get_teams_and_corresponding_challenges_for_a_participant')
 
-        url = reverse_lazy('participants:remove_self_from_participant_team',
-                           kwargs={'participant_team_pk': self.participant_team.pk})
-        self.assertEqual(unicode(url), '/api/participants/remove_self_from_participant_team/' +
-                         str(self.participant_team.pk))
+    def test_remove_self_from_participant_team_url(self):
+        self.url = reverse_lazy('participants:remove_self_from_participant_team',
+                                kwargs={'participant_team_pk': self.participant_team.pk})
+        self.assertEqual(unicode(
+            self.url), '/api/participants/remove_self_from_participant_team/%s' % (self.participant_team.pk))
+        resolver = resolve(self.url)
+        self.assertEqual(resolver.view_name,
+                         'participants:remove_self_from_participant_team')
