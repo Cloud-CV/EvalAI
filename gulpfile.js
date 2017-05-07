5400b6aead9601e98d09a9615a248e6cdf897773
spyshiv <spyshiv@gmail.com>
2017-05-07 07:49:30 +0530

    Frontend: Integrate purifycss to remove unused css 	** Added this feature as a gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var gulp = require('gulp'),
     sass = require('gulp-ruby-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     cssnano = require('gulp-cssnano'),
+    purifycss = require('gulp-purifycss'),
     eslint = require('gulp-eslint'),
     angularPlugin = require('eslint-plugin-angular'),
     gulp_if = require('gulp-if'),
@@ -94,13 +95,22 @@ gulp.task('vendorcss', function() {
 });
 
 // minify and compress CSS files
-gulp.task('css', function() {
+gulp.task('css-build', function() {
     return sass('frontend/src/css/main.scss', { style: 'expanded' })
         .pipe(prettyError())
         .pipe(autoprefixer('last 2 version'))
         .pipe(gulp_if(flags.production, rename({ suffix: '.min' })))
-        .pipe(gulp_if(flags.production, cssnano()))
         .pipe(gulp.dest('frontend/dist/css'));
+})
+
+gulp.task('css', ['css-build'], function() {
+    return gulp.src('frontend/dist/css/**/*.css')
+        .pipe(purifycss([
+            'frontend/src/js/**/*.js',
+            'frontend/src/views/**/*.html',
+        ]))
+        .pipe(gulp_if(flags.production, cssnano()))
+        .pipe(gulp.dest('frontend/dist/css/'));
 })
 
 // minify angular scripts
@@ -241,7 +251,7 @@ gulp.task('lint', [], function() {
     return gulp.src(lint_path.js)
         .pipe(eslint({}))
         .pipe(eslint.format())
-        .pipe(eslint.results(function(results) { 
+        .pipe(eslint.results(function(results) {
             connect.serverClose(); // Close the server initially.
             var countError = results.errorCount; // Get the count of lint errors.
             var countWarning = results.warningCount; // Get the count of lint warnings.
