58ebcea1ed38ff30639174cdf1fb1857b991b5ee
RishabhJain2018 <rishabhjain2018@gmail.com>
2017-05-08 01:01:41 +0530

    Review changes fixed.

--- apps/web/views.py
+++ apps/web/views.py
@@ -35,7 +35,7 @@ def internal_server_error(request):
 
 
 @throttle_classes([AnonRateThrottle, ])
-@api_view(['POST', 'GET'])
+@api_view(['GET', 'POST'])
 @permission_classes((permissions.AllowAny,))
 def contact_us(request):
     try:
@@ -46,7 +46,7 @@ def contact_us(request):
         serializer = ContactSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            response_data = {'message': 'Your message has been successfully recorded. We will contact you shortly.'}
+            response_data = {'message': 'We have received your request and will contact you shortly.'}
             return Response(response_data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -56,7 +56,7 @@ def contact_us(request):
         serializer = ContactSerializer(data=request_data)
         if serializer.is_valid():
             serializer.save()
-            response_data = {'message': 'Your message has been successfully recorded. We will contact you shortly.'}
+            response_data = {'message': 'We have received your request and will contact you shortly.'}
             return Response(response_data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
