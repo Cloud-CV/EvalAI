166d5e0a586320bd9b23b99034a1215323ce67fe
RishabhJain2018 <rishabhjain2018@gmail.com>
2017-06-12 01:41:32 +0530

    Fix review changes

--- apps/jobs/serializers.py
+++ apps/jobs/serializers.py
@@ -72,7 +72,8 @@ class ViewSubmissionSerializer(serializers.ModelSerializer):
         model = Submission
         fields = ('id', 'participant_team', 'challenge_phase', 'created_by', 'status', 'is_public',
                   'submission_number', 'submitted_at', 'execution_time', 'input_file', 'stdout_file',
-                  'stderr_file', 'submission_result_file', 'submission_metadata_file', 'participant_team_members_email_id',)
+                  'stderr_file', 'submission_result_file', 'submission_metadata_file',
+                  'participant_team_members_email_id',)
 
     def get_participant_team(self, obj):
         return obj.participant_team.team_name
@@ -83,7 +84,7 @@ class ViewSubmissionSerializer(serializers.ModelSerializer):
     def get_created_by(self, obj):
         return obj.created_by.username
 
-    def get_participant_team_members_email_id(self, obj):
+    def get_participant_team_members_email_ids(self, obj):
         try:
             participant_team = ParticipantTeam.objects.get(team_name=obj.participant_team.team_name)
         except ParticipantTeam.DoesNotExist:
