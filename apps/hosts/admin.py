058ae91c7585a92d2d4406b8b776e39e4806ed0d
rishabhjain <rishabhjain2018@gmail.com>
2017-06-22 17:59:53 +0530

    Fix #1053: Integrate django-import-export (#1054)

--- apps/hosts/admin.py
+++ apps/hosts/admin.py
@@ -5,16 +5,18 @@ from base.admin import TimeStampedAdmin
 from .models import (ChallengeHost,
                      ChallengeHostTeam)
 
+from import_export.admin import ImportExportModelAdmin
+
 
 @admin.register(ChallengeHostTeam)
-class ChallengeHostTeamModelAdmin(TimeStampedAdmin):
+class ChallengeHostTeamModelAdmin(TimeStampedAdmin, ImportExportModelAdmin):
     list_display = ("team_name", "created_by")
     list_filter = ("created_by", )
     search_fields = ("team_name", "created_by")
 
 
 @admin.register(ChallengeHost)
-class ChallengeHostAdmin(TimeStampedAdmin):
+class ChallengeHostAdmin(TimeStampedAdmin, ImportExportModelAdmin):
     list_display = ("user", "team_name", "status", "permissions")
     list_filter = ("status", "permissions")
     search_fields = ("user", "team_name")
