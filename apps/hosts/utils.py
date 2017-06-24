3957b05f6ad5ff4b4e0a36ee5dfd2c6da38585cb
RishabhJain2018 <rishabhjain2018@gmail.com>
2017-06-25 01:06:32 +0530

    Add a ChallengeHostTeam Model getting function in hosts utils.

--- apps/hosts/utils.py
+++ apps/hosts/utils.py
@@ -1,6 +1,10 @@
-from .models import ChallengeHost
+from base.utils import get_model_object
+
+from .models import ChallengeHost, ChallengeHostTeam
 
 
 def get_challenge_host_teams_for_user(user):
     """Returns challenge host team ids for a particular user"""
     return ChallengeHost.objects.filter(user=user).values_list('team_name', flat=True)
+
+get_challenge_host_team_model = get_model_object(ChallengeHostTeam)
