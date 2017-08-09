47611dfc8128184ec732a605852f890417d29a66
spyshiv <spyshiv@gmail.com>
2017-08-09 15:46:09 +0530

    Frontend: Fix error in landing page due to no featured challenge 	* Rewrite function for authenticated user 	* Remove unused code

--- frontend/src/js/controllers/mainCtrl.js
+++ frontend/src/js/controllers/mainCtrl.js
@@ -12,79 +12,75 @@
     function MainCtrl(utilities, $rootScope, $state) {
 
         var vm = this;
-
         vm.user = {};
-        vm.challengeList = {};
-        vm.challengeCount = 0;
+        vm.challengeList = [];
+        vm.isChallenge = true;
         vm.isMore = false;
-
         // store the next redirect value
         vm.redirectUrl = {};
 
-        // get token
-        var userKey = utilities.getData('userKey');
 
-        // getting challenge (unauthorized)
-        var parameters = {};
+        vm.getChallenge = function() {
+            // get featured challenge (unauthorized)
+            var parameters = {};
+            parameters.url = 'challenges/challenge/present';
+            parameters.method = 'GET';
+            parameters.token = null;
+            parameters.callback = {
+                onSuccess: function(response) {
+                    vm.challengeList = response.data;
+                    var challengeCount = vm.challengeList.count;
+                    if (challengeCount === 0) {
+                        vm.isChallenge = false;
+                    } else {
+                        vm.isChallenge = true;
+                        vm.featuredChallenge = vm.challengeList.results[0];
+                        if (vm.featuredChallenge.description.length > 120) {
+                            vm.isMore = true;
+                        } else {
+                            vm.isMore = false;
+                        }
+                    }
+                },
+                onError: function() {}
+            };
+            utilities.sendRequest(parameters);
+        };
 
-        parameters.url = 'challenges/challenge/present';
-        parameters.method = 'GET';
 
-        parameters.token = null;
-        parameters.callback = {
-            onSuccess: function(response) {
-                var details = response.data;
-                vm.challengeList = details;
-                if (vm.challengeList.count > 10) {
-                    vm.challengeCount = '10+';
-                } else {
-                    vm.challengeCount = vm.challengeList.count;
-                }
-
-                if (vm.challengeList.results[0].description.length > 120) {
-                    vm.isMore = true;
-                } else {
-                    vm.isMore = false;
-                }
-            },
-            onError: function() {
+        vm.init = function() {
+            // get if token is available in localstorage
+            var userKey = utilities.getData('userKey');
+            // check for authenticated user
+            if (userKey) {
+                var parameters = {};
+                parameters.url = 'auth/user/';
+                parameters.method = 'GET';
+                parameters.token = userKey;
+                parameters.callback = {
+                    onSuccess: function(response) {
+                        var status = response.status;
+                        var details = response.data;
+                        if (status == 200) {
+                            vm.user.name = details.username;
+                        }
+                    },
+                    onError: function(response) {
+                        var status = response.status;
+                        if (status == 401) {
+                            utilities.resetStorage();
+                            $state.go("auth.login");
+                            $rootScope.isAuth = false;
+                        }
+                    }
+                };
+                utilities.sendRequest(parameters);
             }
         };
 
-        utilities.sendRequest(parameters);
 
-        // check for authenticated user
-        if (userKey) {
-            parameters = {};
-            parameters.url = 'auth/user/';
-            parameters.method = 'GET';
-            parameters.token = userKey;
-            parameters.callback = {
-                onSuccess: function(response) {
-                    var status = response.status;
-                    var details = response.data;
-                    if (status == 200) {
-                        vm.user.name = details.username;
-                    }
-                },
-                onError: function(response) {
-
-                    var status = response.status;
-                    if (status == 401) {
-                        utilities.resetStorage();
-                        $state.go("auth.login");
-                        $rootScope.isAuth = false;
-                    }
-                }
-            };
-
-            utilities.sendRequest(parameters);
-
-
-        }
         vm.hostChallenge = function() {
-            if ($rootScope.isAuth === true)
-            {
+            if ($rootScope.isAuth === true) {
                 $state.go('web.challenge-host-teams');
             } else {
                 $state.go('auth.login');
@@ -92,11 +88,14 @@
             }
         };
 
+
         vm.profileDropdown = function() {
             angular.element(".dropdown-button").dropdown();
-
         };
+
+
+        vm.init();
+        vm.getChallenge();
+
     }
-
-
 })();
