71c3b201846ed7ef4783d5f17900792bf4d6d5aa
RishabhJain2018 <rishabhjain2018@gmail.com>
2017-08-21 16:43:52 +0530

    flake8 fixes

--- apps/analytics/serializers.py
+++ apps/analytics/serializers.py
@@ -5,7 +5,8 @@ from jobs.models import Submission
 
 class ChallengePhaseSubmissionAnalysisSerializer(serializers.ModelSerializer):
 
-	class Meta:
-		model = Submission
-		fields = ('submissions_count_for_challenge_phase', 'participated_teams_count_for_challenge_phase',
-				  'total_submissions_by_participant_team_in_challenge_phase')
+    class Meta:
+        model = Submission
+        fields = ('submissions_count_for_challenge_phase',
+                  'participated_teams_count_for_challenge_phase',
+                  'total_submissions_by_participant_team_in_challenge_phase',)
