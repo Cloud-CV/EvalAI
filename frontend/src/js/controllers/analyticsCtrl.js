665732c4f7e1d86631f99e1372f07dfb56e2559c
RishabhJain2018 <rishabhjain2018@gmail.com>
2017-08-26 04:44:30 +0530

    Removed Comments

--- frontend/src/js/controllers/analyticsCtrl.js
+++ frontend/src/js/controllers/analyticsCtrl.js
@@ -25,17 +25,7 @@
         vm.totalSubmission = {};
         vm.totalParticipatedTeams = {};
         vm.lastSubmissionTime = {};
-        // vm.challengeCount = 0;
-        // vm.hostTeamCount = 0;
-        // vm.hostTeamExist = false;
-        // vm.participatedTeamCount = 0;
-        // // get token
 
-        // if (vm.teamId == null) {
-        //     vm.isTeamSelected = false;
-        // } else {
-        //     vm.isTeamSelected = true;
-        // }
         var userKey = utilities.getData('userKey');
 
         var parameters = {};
@@ -223,123 +213,10 @@
                         vm.currentChallengeDetails = vm.challengeList[i];
                     }
                 }
-                console.log(vm.currentChallengeDetails);
             } else {
                 vm.isTeamSelected = false;
             }
         };
-
-        // // get all ongoing challenges.
-        // parameters.url = 'challenges/challenge/present';
-        // parameters.method = 'GET';
-        // parameters.token = userKey;
-        // parameters.callback = {
-        //     onSuccess: function(response) {
-        //         var status = response.status;
-        //         var details = response.data;
-        //         if (status == 200) {
-        //             vm.challengeCount = details.count;
-        //             if(vm.hostTeamCount == 0){
-        //                 vm.hostTeamExist = false;
-        //             }
-        //             else{
-        //                 vm.hostTeamExist = true;
-        //             }
-        //         }
-        //     },
-        //     onError: function(response) {
-        //         var status = response.status;
-        //         var error = response.data;
-        //         if (status == 403) {
-        //             vm.error = error;
-
-        //             // navigate to permissions denied page
-        //             $state.go('web.permission-denied');
-        //         } else if (status == 401) {
-        //             alert("Timeout, Please login again to continue!");
-        //             utilities.resetStorage();
-        //             $state.go("auth.login");
-        //             $rootScope.isAuth = false;
-
-        //         }
-        //     }
-        // };
-
-        // utilities.sendRequest(parameters);
-
-        //  //check for host teams.
-        // parameters.url = 'hosts/challenge_host_team';
-        // parameters.method = 'GET';
-        // parameters.token = userKey;
-        // parameters.callback = {
-        //     onSuccess: function(response) {
-        //         var status = response.status;
-        //         var details = response.data;
-        //         if (status == 200) {
-        //             vm.hostTeamCount = details.count;
-        //         }
-        //     },
-        //     onError: function(response) {
-        //         var status = response.status;
-        //         var error = response.data;
-        //         if (status == 403) {
-        //             vm.error = error;
-
-        //             // navigate to permissions denied page
-        //             $state.go('web.permission-denied');
-        //         } else if (status == 401) {
-        //             alert("Timeout, Please login again to continue!");
-        //             utilities.resetStorage();
-        //             $state.go("auth.login");
-        //             $rootScope.isAuth = false;
-
-        //         }
-        //     }
-        // };
-
-        // utilities.sendRequest(parameters);
-
-        // //check for participated teams.
-        // parameters.url = 'participants/participant_team';
-        // parameters.method = 'GET';
-        // parameters.token = userKey;
-        // parameters.callback = {
-        //     onSuccess: function(response) {
-        //         var status = response.status;
-        //         var details = response.data;
-        //         if (status == 200) {
-        //             vm.participatedTeamCount = details.count;
-        //         }
-        //     },
-        //     onError: function(response) {
-        //         var status = response.status;
-        //         var error = response.data;
-        //         if (status == 403) {
-        //             vm.error = error;
-
-        //             // navigate to permissions denied page
-        //             $state.go('web.permission-denied');
-        //         } else if (status == 401) {
-        //             alert("Timeout, Please login again to continue!");
-        //             utilities.resetStorage();
-        //             $state.go("auth.login");
-        //             $rootScope.isAuth = false;
-
-        //         }
-        //     }
-        // };
-
-        // utilities.sendRequest(parameters);
-
-        // vm.hostChallenge = function() {
-
-        //     var alert = $mdDialog.alert()
-        //         .title('Host a challenge')
-        //         .htmlContent('Please send an email to <a href="mailto:admin@cloudcv.org" class="blue-text">admin@cloudcv.org</a> with the details of the challenge and we will get back soon.')
-        //         .ok('Close');
-
-        //     $mdDialog.show(alert);
-        // };
     }
 
 })();
