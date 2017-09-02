ec1af1f53280f11454c1aa62ab16044a95130b8a
HargovindArora <hargovinds50@gmail.com>
2017-09-02 16:47:59 +0530

    Urls updated

--- apps/analytics/urls.py
+++ apps/analytics/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 urlpatterns = [
 
-    url(r'challenge/(?P<challenge_pk>[0-9]+)/team/count$',
+    url(r'^challenge/(?P<challenge_pk>[0-9]+)/team/count$',
         views.get_participant_team_count, name='get_participant_team_count'),
-    url(r'challenge/(?P<challenge_pk>[0-9]+)/participant/count$',
+    url(r'^challenge/(?P<challenge_pk>[0-9]+)/participant/count$',
         views.get_participant_count, name='get_participant_count'),
-    url(r'challenge/(?P<challenge_pk>[0-9]+)/submission/(?P<duration>[A-Za-z]+)/count$',
+    url(r'^challenge/(?P<challenge_pk>[0-9]+)/submission/(?P<duration>[A-Za-z]+)/count$',
         views.get_submission_count, name='get_submission_count'),
 
 ]
