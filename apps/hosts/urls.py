c34cf2537f4e357728b0c1c6e39c058025b76d92
HargovindArora <hargovinds50@gmail.com>
2017-09-05 13:42:39 +0530

    URLs files updated

--- apps/hosts/urls.py
+++ apps/hosts/urls.py
@@ -4,16 +4,16 @@ from . import views
 
 urlpatterns = [
 
-    url(r'challenge_host_team/$', views.challenge_host_team_list, name='get_challenge_host_team_list'),
-    url(r'challenge_host_team/(?P<pk>[0-9]+)$', views.challenge_host_team_detail,
+    url(r'^challenge_host_team/$', views.challenge_host_team_list, name='get_challenge_host_team_list'),
+    url(r'^challenge_host_team/(?P<pk>[0-9]+)$', views.challenge_host_team_detail,
         name='get_challenge_host_team_details'),
-    url(r'create_challenge_host_team', views.create_challenge_host_team, name='create_challenge_host_team'),
-    url(r'challenge_host_team/(?P<challenge_host_team_pk>[0-9]+)/challenge_host$', views.challenge_host_list,
+    url(r'^create_challenge_host_team', views.create_challenge_host_team, name='create_challenge_host_team'),
+    url(r'^challenge_host_team/(?P<challenge_host_team_pk>[0-9]+)/challenge_host$', views.challenge_host_list,
         name='get_challenge_host_list'),
-    url(r'challenge_host_team/(?P<challenge_host_team_pk>[0-9]+)/challenge_host/(?P<pk>[0-9]+)$',
+    url(r'^challenge_host_team/(?P<challenge_host_team_pk>[0-9]+)/challenge_host/(?P<pk>[0-9]+)$',
         views.challenge_host_detail, name='get_challenge_host_details'),
-    url(r'remove_self_from_challenge_host/(?P<challenge_host_team_pk>[0-9]+)$',
+    url(r'^remove_self_from_challenge_host/(?P<challenge_host_team_pk>[0-9]+)$',
         views.remove_self_from_challenge_host_team, name='remove_self_from_challenge_host_team'),
-    url(r'challenge_host_teams/(?P<pk>[0-9]+)/invite$', views.invite_host_to_team,
+    url(r'^challenge_host_teams/(?P<pk>[0-9]+)/invite$', views.invite_host_to_team,
         name='invite_host_to_team'),
 ]
