113c4ab3c82f5305a7930ef5f195e44662f7fd6a
rishabhjain <rishabhjain2018@gmail.com>
2017-09-12 23:38:38 +0530

    Fix #1087: Add APIs with tests for challenge creation using UI. (#1085)

--- apps/challenges/utils.py
+++ apps/challenges/utils.py
@@ -2,12 +2,18 @@ import os
 
 from base.utils import get_model_object
 
-from .models import Challenge, ChallengePhase
+from .models import Challenge, ChallengePhase, Leaderboard, DatasetSplit, ChallengePhaseSplit
 
 
 get_challenge_model = get_model_object(Challenge)
 
 get_challenge_phase_model = get_model_object(ChallengePhase)
+
+get_leaderboard_model = get_model_object(Leaderboard)
+
+get_dataset_split_model = get_model_object(DatasetSplit)
+
+get_challenge_phase_split_model = get_model_object(ChallengePhaseSplit)
 
 
 def get_file_content(file_path, mode):
