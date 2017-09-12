e2be3d696598ec277e77c559748d38723288539d
rishabhjain <rishabhjain2018@gmail.com>
2017-09-12 23:33:11 +0530

    Fix #1180: Update serializer for when made public timestamp. (#1199)

--- apps/jobs/serializers.py
+++ apps/jobs/serializers.py
@@ -32,7 +32,7 @@ class SubmissionSerializer(serializers.ModelSerializer):
         fields = ('id', 'participant_team', 'participant_team_name', 'execution_time', 'challenge_phase',
                   'created_by', 'status', 'input_file', 'stdout_file', 'stderr_file', 'submitted_at',
                   'method_name', 'method_description', 'project_url', 'publication_url', 'is_public',
-                  'submission_result_file',)
+                  'submission_result_file', 'when_made_public',)
 
     def get_participant_team_name(self, obj):
         return obj.participant_team.team_name
