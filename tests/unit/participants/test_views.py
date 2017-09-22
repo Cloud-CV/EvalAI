89b5fe4161ae23d99c81f2fcfc08c06e4e38230c
Ram81 <ramramrakhya81@gmail.com>
2017-09-23 00:01:55 +0530

    Fix: TravisCI Build Errors Fixed

--- tests/unit/participants/test_views.py
+++ tests/unit/participants/test_views.py
@@ -568,7 +568,7 @@ class GetTeamsAndCorrespondingChallengesForAParticipant(BaseAPITestClass):
             'participants:get_teams_and_corresponding_challenges_for_a_participant',
             kwargs={'challenge_pk': self.challenge1.pk})
 
-        self.time = timezone.now()
+        self.time = timezone.now().replace(microsecond=0)
 
     def test_get_teams_and_corresponding_challenges_for_a_participant(self):
 
