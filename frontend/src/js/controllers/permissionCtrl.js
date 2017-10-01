8267496c1866f195fcda1646feb44b9c859c6568
Adarsh S <4darshofficial@gmail.com>
2017-10-01 21:18:49 +0530

    Add frontend for resend E-mail feature.
    * Add a new function requestLink().
    
    * Add UI for the functionality.

--- frontend/src/js/controllers/permissionCtrl.js
+++ frontend/src/js/controllers/permissionCtrl.js
@@ -7,13 +7,33 @@
         .module('evalai')
         .controller('PermCtrl', PermCtrl);
 
-    PermCtrl.$inject = ['utilities'];
+    PermCtrl.$inject = ['utilities', '$rootScope'];
 
-    function PermCtrl(utilities) {
+    function PermCtrl(utilities, $rootScope) {
         var vm = this;
 
         // message for not verified users
         vm.emailError = utilities.getData('emailError');
+        vm.sendMail = false;
+        // Function to request a new verification email.
+        vm.requestLink = function () {
+            var userKey = utilities.getData('userKey');
+            var parameters = {};
+            parameters.url = 'accounts/user/resend-email';
+            parameters.method = 'POST';
+            parameters.token = userKey;
+            parameters.callback = {
+                onSuccess: function() {
+                    vm.sendMail = true;
+                    $rootScope.notify("success", "The verification link was sent again.");
+                },
+                onError: function() {
+                    vm.stopLoader();
+                    $rootScope.notify("error", "Something went wrong, try again.");
+                }
+            };
+            utilities.sendRequest(parameters);
+        };
     }
 
 })();
