75b16ab6f7783530ca6344490122c85c8ed730dc
Adam Štafa <34075272+adamstafa@users.noreply.github.com>
2018-01-06 15:38:55 +0100

    Added loader to challenge submission process.

--- frontend/src/js/controllers/challengeCreate.js
+++ frontend/src/js/controllers/challengeCreate.js
@@ -46,6 +46,7 @@
                     parameters.token = userKey;
                     parameters.callback = {
                         onSuccess: function(response) {
+                            utilities.hideLoader();
                             var status = response.status;
                             var details =  response.data;
                             if (status === 201) {
@@ -64,6 +65,7 @@
                             }
                         },
                         onError: function(response) {
+                            utilities.hideLoader();
                             var error = response.data;
                             angular.element(".file-path").val(null);
                             $rootScope.notify("error", error.error);
@@ -71,6 +73,7 @@
                         }
                     };
                 }
+                utilities.showLoader();
                 utilities.sendRequest(parameters, 'header', 'upload');
             }
             else {
