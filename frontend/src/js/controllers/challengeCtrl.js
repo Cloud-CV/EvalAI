43f626346bb6172ad40096d3d737562d1e3412e4
Vivek Raj <vivek@laptop16.04>
2018-01-16 00:52:35 +0530

    Add functionality to delete challenges

--- frontend/src/js/controllers/challengeCtrl.js
+++ frontend/src/js/controllers/challengeCtrl.js
@@ -1155,6 +1155,46 @@
             }
         };
 
+// Delete challenge
+        vm.deleteChallengeDialog = function(ev) {
+            vm.titleInput = "";
+            $mdDialog.show({
+                scope: $scope,
+                preserveScope: true,
+                targetEvent: ev,
+                templateUrl: 'dist/views/web/challenge/delete-challenge/delete-challenge.html',
+                escapeToClose: false
+            });
+        };
+
+        vm.deleteChallenge = function(deleteChallengeForm) {
+            if(deleteChallengeForm){
+                var parameters = {};
+                parameters.url = "challenges/challenge/" + vm.challengeId + "/disable";
+                parameters.method = 'POST';
+                parameters.token = userKey;
+                parameters.callback = {
+                    onSuccess: function(response) {
+                        var status = response.status;
+                        if (status === 204){
+                            $mdDialog.hide();
+                            $rootScope.notify("success", "The Challenge was successfully removed");
+                        }
+                    },
+                    onError: function(response) {
+                        $mdDialog.hide();
+                        var error = response.data;
+                        console.log(error);
+                        $rootScope.notify("error", error);
+                    }
+                };
+
+                utilities.sendRequest(parameters);
+            } else {
+                $mdDialog.hide();
+            }
+        };
+
 // Edit submission guidelines
         vm.submissionGuidelinesDialog = function(ev) {
             vm.tempSubmissionGuidelines = vm.page.submission_guidelines;
