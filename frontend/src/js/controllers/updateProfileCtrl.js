61667c5afabb8a605cb8d5455265b8a1a103cfe4
rishabhjain <rishabhjain2018@gmail.com>
2018-01-15 19:21:59 +0530

    Frontend: Add feature to update affiliation in profile. (#1462)
    * Update Affiliation Field
    
    * Frontend: Update to retrieve affiliation
    
    * Frontend: Input Field for Affiliation
    
    * Working Affiliation feature

--- frontend/src/js/controllers/updateProfileCtrl.js
+++ frontend/src/js/controllers/updateProfileCtrl.js
@@ -66,6 +66,7 @@
                     "username": vm.user.username,
                     "first_name": vm.user.first_name,
                     "last_name": vm.user.last_name,
+                    "affiliation": vm.user.affiliation,
                 };
                 parameters.token = userKey;
                 parameters.callback = {
@@ -81,19 +82,21 @@
                         if (response.status == 400) {
                             vm.stopLoader();
                             vm.isFormError = true;
-                            var isUsername_valid, isFirstname_valid, isLastname_valid;
+                            var isUsername_valid, isFirstname_valid, isLastname_valid, isAffiliation_valid;
                             try {
                                 isUsername_valid = typeof(response.data.username) !== 'undefined' ? true : false;
                                 isFirstname_valid = typeof(response.data.first_name) !== 'undefined' ? true : false;
                                 isLastname_valid = typeof(response.data.last_name) !== 'undefined' ? true : false;
+                                isAffiliation_valid = typeof(response.data.affiliation) !== 'undefined' ? true : false;
                                 if (isUsername_valid) {
                                     vm.FormError = response.data.username[0];
                                 } else if (isFirstname_valid) {
                                     vm.FormError = response.data.first_name[0];
                                 } else if (isLastname_valid) {
                                     vm.FormError = response.data.last_name[0];
-
-                                } else {
+                                }else if (isAffiliation_valid) {              
+                                    vm.FormError = response.data.affiliation[0]; 
+                                }else {
                                     $rootScope.notify("error", "Some error have occured . Please try again !");
                                 }
 
