c54ce7d5994135d8b6143328343b9302b4ed4f7e
Hargovind Singh Arora <hargovinds50@gmail.com>
2018-01-26 11:21:37 +0530

    Fix #1368: Fix search result error on django admin (#1372)

--- apps/challenges/admin.py
+++ apps/challenges/admin.py
@@ -17,7 +17,7 @@ class ChallengeAdmin(ImportExportTimeStampedAdmin):
     list_display = ("title", "start_date", "end_date", "creator", "published", "enable_forum", "anonymous_leaderboard",
                     "featured")
     list_filter = ("creator", "published", "enable_forum", "anonymous_leaderboard", "featured")
-    search_fields = ("title", "creator")
+    search_fields = ("title", "creator__team_name")
 
 
 @admin.register(DatasetSplit)
@@ -44,24 +44,26 @@ class LeaderboardAdmin(ImportExportTimeStampedAdmin):
 class ChallengePhaseSplitAdmin(ImportExportTimeStampedAdmin):
     list_display = ("id", "challenge_phase", "dataset_split", "leaderboard", "visibility")
     list_filter = ("challenge_phase", "dataset_split", "leaderboard", "visibility")
-    search_fields = ("challenge_phase", "dataset_split", "leaderboard")
+    search_fields = ("challenge_phase__name", "dataset_split__name", "leaderboard__id",
+                     "dataset_split__codename")
 
 
 @admin.register(LeaderboardData)
 class LeaderboardDataAdmin(ImportExportTimeStampedAdmin):
     list_display = ("challenge_phase_split", "submission", "leaderboard", "result")
     list_filter = ("challenge_phase_split", "leaderboard",)
-    search_fields = ("challenge_phase_split", "submission", "leaderboard", "result")
+    search_fields = ("challenge_phase_split__challenge_phase__name",
+                     "submission__participant_team__team_name", "leaderboard__schema", "result")
 
 
 @admin.register(ChallengeConfiguration)
 class ChallengeConfigurationAdmin(ImportExportTimeStampedAdmin):
     list_display = ('user', 'challenge', 'is_created', 'zip_configuration',)
     list_filter = ('user', 'is_created',)
-    search_fields = ('user', 'challenge',)
+    search_fields = ('user__username', 'challenge__title',)
 
 
 @admin.register(StarChallenge)
 class StarChallengeAdmin(ImportExportTimeStampedAdmin):
     list_display = ('user', 'challenge', 'is_starred')
-    search_fields = ('user', 'challenge',)
+    search_fields = ('user__username', 'challenge__title',)
