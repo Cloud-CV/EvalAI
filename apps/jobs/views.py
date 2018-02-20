389335e4a5a911632a514c471e4fd262347ec24a
rishabhjain <rishabhjain2018@gmail.com>
2018-02-21 04:31:59 +0530

    Fix #1533: Add submitted_at field on leaderboard (#1534)
    * Add submitted_at field in the leaderboard.
    
    * Add submission time to leaderboard

--- apps/jobs/views.py
+++ apps/jobs/views.py
@@ -222,7 +222,7 @@ def leaderboard(request, challenge_phase_split_id):
     leaderboard_data = leaderboard_data.annotate(
         filtering_score=RawSQL('result->>%s', (default_order_by, ), output_field=FloatField())).values(
             'id', 'submission__participant_team__team_name',
-            'challenge_phase_split', 'result', 'filtering_score', 'leaderboard__schema')
+            'challenge_phase_split', 'result', 'filtering_score', 'leaderboard__schema', 'submission__submitted_at')
 
     sorted_leaderboard_data = sorted(leaderboard_data, key=lambda k: float(k['filtering_score']), reverse=True)
 
