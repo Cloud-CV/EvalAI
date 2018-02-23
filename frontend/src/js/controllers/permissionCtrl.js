de7ef56224a782225a173454fa639c9a245f016a
tinaoberoi <tinaoberoi70@gmail.com>
2018-02-24 01:35:37 +0530

    Fix300: remove unnecessary changes

--- frontend/src/js/controllers/permissionCtrl.js
+++ frontend/src/js/controllers/permissionCtrl.js
@@ -14,26 +14,6 @@
 
         // message for not verified users
         vm.emailError = utilities.getData('emailError');
-
-        //user email redirect
-        vm.user={};
-
-        var userKey = utilities.getData('userKey');
-
-        var parameters = {};
-        parameters.url = 'auth/user/';
-        parameters.method = 'GET';
-        parameters.token = userKey;
-        parameters.callback = {
-            onSuccess: function(response) {
-//                var status = response.status;
-                var result = response.data;
-                vm.user = result;
-            },
-        };
-
-        utilities.sendRequest(parameters);
-
-   }
+    }
 
 })();
