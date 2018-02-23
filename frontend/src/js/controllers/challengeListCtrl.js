1330cf82a87ba6626e1a607055f1c429d15d4cfd
tinaoberoi <tinaoberoi70@gmail.com>
2018-02-24 02:42:15 +0530

    Fix300: add pagination for currentList

--- frontend/src/js/controllers/challengeListCtrl.js
+++ frontend/src/js/controllers/challengeListCtrl.js
@@ -6,21 +6,20 @@
         .module('evalai')
         .controller('ChallengeListCtrl', ChallengeListCtrl);
 
-    ChallengeListCtrl.$inject = ['utilities', '$window'];
+    ChallengeListCtrl.$inject = ['utilities', '$window', '$http'];
 
-    function ChallengeListCtrl(utilities, $window) {
+    function ChallengeListCtrl(utilities, $window, $http) {
         var vm = this;
         var userKey = utilities.getData('userKey');
 
         utilities.showLoader();
 
-        vm.currentList = {};
-        vm.upcomingList = {};
-        vm.pastList = {};
-
-        vm.noneCurrentChallenge = false;
-        vm.noneUpcomingChallenge = false;
-        vm.nonePastChallenge = false;
+        // default variables/objects
+        vm.existTeam = {};
+        vm.currentPage = '';
+        vm.isNext = '';
+        vm.isPrev = '';
+        vm.showPagination = false;
 
         // calls for ongoing challneges
         vm.challengeCreator = {};
@@ -31,40 +30,69 @@
 
         parameters.callback = {
             onSuccess: function(response) {
+                var status= response.status;
                 var data = response.data;
                 vm.currentList = data.results;
 
-                if (vm.currentList.length === 0) {
-                    vm.noneCurrentChallenge = true;
-                } else {
-                    vm.noneCurrentChallenge = false;
-                }
-
-                for (var i in vm.currentList) {
-
-                    var descLength = vm.currentList[i].description.length;
-                    if (descLength >= 50) {
-                        vm.currentList[i].isLarge = "...";
+                if(status=200){
+                  vm.existList=data;
+                  if (vm.existList.count === 0) {
+                        vm.showPagination = false;
+                        vm.paginationMsg = "No team exists for now, start by creating a new team!";
                     } else {
-                        vm.currentList[i].isLarge = "";
+                        vm.showPagination = true;
+                        vm.paginationMsg = "";
+                    }
+                    // condition for pagination
+                    if (vm.existList.next === null) {
+                        vm.isNext = 'disabled';
+                    } else {
+                        vm.isNext = '';
                     }
 
-                    var id = vm.currentList[i].id;              
-                    vm.challengeCreator[id]= vm.currentList[i].creator.id;
-                    utilities.storeData("challengeCreator", vm.challengeCreator);
-                }
-                if(vm.currentList.length !=0){
-                     vm.presentLoadIndex=3;
-                     vm.showMore = function() {
-                        if (vm.presentLoadIndex < vm.currentList.length){
-                           vm.presentLoadIndex = vm.presentLoadIndex+1;
-                        }
+                    if (vm.existList.previous === null) {
+                        vm.isPrev = 'disabled';
+                    } else {
+                        vm.isPrev = '';
+                    }
+                    if (vm.existList.next !== null) {
+                        vm.currentPage = vm.existList.next.split('page=')[1] - 1;
+                    } else {
+                        vm.currentPage = 1;
+                    }
+                    vm.load=function(url){
+
+                        if (url !== null) {
+
+                            console.log(url);
+                            //store the header data in a variable
+                            var headers = {
+                                'Authorization': "Token " + userKey
+                            };
+
+                            //Add headers with in your request
+                            $http.get(url, { headers: headers }).then(function(response) {
+                                // reinitialized data
+                                var details = response.data;
+                                vm.existList = details;
+
+                                // condition for pagination
+                                if (vm.existList.next === null) {
+                                    vm.isNext = 'disabled';
+                                    vm.currentPage = vm.existList.count / 10;
+                                } else {
+                                    vm.isNext = '';
+                                    vm.currentPage = parseInt(vm.existList.next.split('page=')[1] - 1);
+                                }
+                                if (vm.existList.previous === null) {
+                                    vm.isPrev = 'disabled';
+                                } else {
+                                    vm.isPrev = '';
+                                }
+                            });
+                       }
                      };
-                     vm.showLess = function() {
-                        if (vm.presentLoadIndex > 3){
-                           vm.presentLoadIndex = vm.presentLoadIndex-1;
-                        }
-                     };
+
                 }
 
                 // dependent api
