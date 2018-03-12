04e0015776a8266d06ca7feb1b50a8356a5ac2f0
rishabhjain <rishabhjain2018@gmail.com>
2018-03-12 12:12:44 +0530

    Fix #1549: Add feature to notify users about new challenge via email. (#1550)

--- apps/web/urls.py
+++ apps/web/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^contact/$', views.contact_us, name='contact_us'),
     url(r'^team/$', views.our_team, name='our_team'),
+    url(r'^notify_users/$', views.notify_users_about_challenge, name='notify_users_about_challenge'),
 ]
