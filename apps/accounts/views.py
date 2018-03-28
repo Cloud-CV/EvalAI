ce42899d693c91d810f91e8a984389143745be75
Adarsh S <4darshofficial@gmail.com>
2018-03-29 03:24:51 +0530

    Fix #630: Add backend for resend email feature. (#1355)

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,20 +1,37 @@
 from django.contrib.auth import logout
 
+from allauth.account.utils import send_email_confirmation
+
 from rest_framework.response import Response
 from rest_framework import permissions, status
 from rest_framework.decorators import (api_view,
                                        authentication_classes,
-                                       permission_classes,)
+                                       permission_classes,
+                                       throttle_classes,)
 from rest_framework_expiring_authtoken.authentication import (ExpiringTokenAuthentication,)
+
+from .throttles import ResendEmailThrottle
 
 
 @api_view(['POST'])
 @permission_classes((permissions.IsAuthenticated,))
 @authentication_classes((ExpiringTokenAuthentication,))
 def disable_user(request):
-
     user = request.user
     user.is_active = False
     user.save()
     logout(request)
+    return Response(status=status.HTTP_200_OK)
+
+
+@throttle_classes([ResendEmailThrottle])
+@api_view(['POST'])
+@permission_classes((permissions.IsAuthenticated,))
+@authentication_classes((ExpiringTokenAuthentication,))
+def resend_email_confirmation(request):
+    """
+    Resends the confirmation Email by the users request.
+    """
+    user = request.user
+    send_email_confirmation(request._request, user)
     return Response(status=status.HTTP_200_OK)
