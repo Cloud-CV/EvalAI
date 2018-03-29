af9423d04790d4277508d56f259f9b01c941fd42
Deshraj Yadav <deshraj@gatech.edu>
2018-03-29 01:01:56 -0400

    Revert "Fix #630: Add frontend for resend email feature. (#1354)" (#1608)
    This reverts commit ce02c299dd557aed25c1cf3d3dc04ee7cca95478.

--- frontend/src/js/controllers/permissionCtrl.js
+++ frontend/src/js/controllers/permissionCtrl.js
@@ -7,33 +7,13 @@
         .module('evalai')
         .controller('PermCtrl', PermCtrl);
 
-    PermCtrl.$inject = ['utilities', '$rootScope'];
+    PermCtrl.$inject = ['utilities'];
 
-    function PermCtrl(utilities, $rootScope) {
+    function PermCtrl(utilities) {
         var vm = this;
 
         // message for not verified users
         vm.emailError = utilities.getData('emailError');
-        vm.sendMail = false;
-        // Function to request a new verification email.
-        vm.requestLink = function () {
-            var userKey = utilities.getData('userKey');
-            var parameters = {};
-            parameters.url = 'accounts/user/resend-email';
-            parameters.method = 'POST';
-            parameters.token = userKey;
-            parameters.callback = {
-                onSuccess: function() {
-                    vm.sendMail = true;
-                    $rootScope.notify("success", "The verification link was sent again.");
-                },
-                onError: function() {
-                    vm.stopLoader();
-                    $rootScope.notify("error", "Something went wrong, try again.");
-                }
-            };
-            utilities.sendRequest(parameters);
-        };
     }
 
 })();
