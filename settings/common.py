419c64633abe85edd59dc9f4711381a18977efdf
Deshraj Yadav <deshraj@gatech.edu>
2018-03-29 01:32:48 -0400

    Revert "Fix #630: Add backend for resend email feature. (#1355)" (#1609)
    This reverts commit fb017bb86899e714013cdb5edbd42ecf5c6a4e02.

--- settings/common.py
+++ settings/common.py
@@ -158,13 +158,11 @@ REST_FRAMEWORK = {
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
     'DEFAULT_THROTTLE_CLASSES': (
         'rest_framework.throttling.AnonRateThrottle',
-        'rest_framework.throttling.UserRateThrottle',
-        'accounts.throttles.ResendEmailThrottle',
+        'rest_framework.throttling.UserRateThrottle'
     ),
     'DEFAULT_THROTTLE_RATES': {
         'anon': '100/minute',
-        'user': '100/minute',
-        'resend_email': '3/hour',
+        'user': '100/minute'
     },
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
