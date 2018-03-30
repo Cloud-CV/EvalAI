b81038712c6e803d1539c10a58bdee2757943a8c
rishabhjain <rishabhjain2018@gmail.com>
2018-03-31 04:19:03 +0530

    Settings: Fix development runserver error (#1613)

--- settings/__init__.py
+++ settings/__init__.py
@@ -4,9 +4,7 @@ from __future__ import absolute_import
 try:
     from .dev import *  # noqa
 except ImportError:
-    pass
-
-try:
-    from .prod import *  # noqa
-except ImportError:
-    pass
+    try:
+        from .prod import *  # noqa
+    except ImportError:
+        pass
