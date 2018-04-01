56a2fcb8fc803808abd3e71da9435a2fc0a90c44
Deshraj <deshraj@gatech.edu>
2018-04-01 17:38:44 -0400

    Update test settings

--- settings/__init__.py
+++ settings/__init__.py
@@ -1,10 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
-try:
-    from .dev import *  # noqa
-except ImportError:
+import sys
+
+if "test" in sys.argv:
+    print("\033[1;91mNo django tests.\033[0m")
+    print("Try: \033[1;33mpy.test\033[0m")
+    sys.exit(0)
+
+TEST = [arg for arg in sys.argv if 'py.test' in arg]
+if TEST:
+    print("Using Test settings")
+    from .test import *
+else:
     try:
-        from .prod import *  # noqa
+        from .dev import *  # noqa
+        print("Using Dev settings")
     except ImportError:
-        pass
+        try:
+            from .prod import *  # noqa
+            print("Using Prod settings")
+        except ImportError:
+            pass
