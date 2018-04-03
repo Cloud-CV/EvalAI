28c0d37b144032578b6003fea8b55d2ef2995d73
RishabhJain2018 <rishabhjain2018@gmail.com>
2018-04-03 11:27:50 +0530

    flake8 fixes

--- tests/unit/web/test_tasks.py
+++ tests/unit/web/test_tasks.py
@@ -7,23 +7,22 @@ from web.tasks import notify_admin_on_receiving_contact_message
 
 class NotifyAdminOnContactMessage(TestCase):
 
+    def setUp(self):
+        self.name = 'user'
+        self.email = 'user@domain.com'
+        self.message = 'Sample message'
+        self.host_url = 'https://hooks.slack.com/services/token/token/token'
 
-	def setUp(self):
-		self.name = 'user'
-		self.email = 'user@domain.com'
-		self.message = 'Sample message'
-		self.host_url = 'https://hooks.slack.com/services/token/token/token'
+    @responses.activate
+    def test_notify_admin_on_contact_message(self):
+        with responses.RequestsMock() as rsps:
+            rsps.add(responses.POST,
+                     self.host_url,
+                     status=200,
+                     content_type='application/json',
+                     body='ok')
 
-	@responses.activate
-	def test_notify_admin_on_contact_message(self):
-		with responses.RequestsMock() as rsps:
-			rsps.add(responses.POST,
-					 self.host_url,
-					 status=200,
-					 content_type='application/json',
-					 body='ok')
-
-			resp = notify_admin_on_receiving_contact_message(self.host_url,
-														     self.name,
-														     self.email,
-														     self.message)
+            resp = notify_admin_on_receiving_contact_message(self.host_url,
+                                                             self.name,
+                                                             self.email,
+                                                             self.message)
