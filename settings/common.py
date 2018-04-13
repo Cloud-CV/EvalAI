998590e2f0a3d4914615a6f7ca7280adfe8cb2f4
Deshraj Yadav <deshraj@gatech.edu>
2018-04-13 18:30:44 -0400

    Update common.py

--- settings/common.py
+++ settings/common.py
@@ -280,7 +280,6 @@ REST_AUTH_SERIALIZERS = {
 }
 
 # For inviting users to participant and host teams.
-<<<<<<< HEAD
 ADMIN_EMAIL = "admin@cloudcv.org"
 CLOUDCV_TEAM_EMAIL = "EvalAI Team <team@cloudcv.org>"
 
@@ -298,9 +297,6 @@ SWAGGER_SETTINGS = {
 REDOC_SETTINGS = {
     'SPEC_URL': ('docs.yaml', {'format': '.yaml'}),
 }
-=======
-ADMIN_EMAIL = "deshrajdry@gmail.com"
->>>>>>> Add tests and minir fixes
 
 # Celery Broker Url
 CELERY_BROKER_URL = 'amqp://localhost'
