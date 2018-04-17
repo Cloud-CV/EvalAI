5cf34d4af49fac1115e54746e78b2ff69efccf8d
Adarsh S <4darshofficial@gmail.com>
2018-04-17 14:17:07 +0530

    Add challenge host section

--- frontend/src/js/controllers/challengeListCtrl.js
+++ frontend/src/js/controllers/challengeListCtrl.js
@@ -159,45 +159,6 @@
                 }
             });
         };
-
-        parameters = {};
-        parameters.url = 'hosts/challenge_host_team/';
-        parameters.method = 'GET';
-        parameters.token = userKey;
-        parameters.callback = {
-            onSuccess: function(response) {
-                var participant_host_team = response["data"]["results"];
-                parameters.method = 'GET';
-                for (var i=0; i<participant_host_team.length; i++) {
-                    parameters.url = "challenges/challenge_host_team/" + participant_host_team[i]["id"] + "/challenge";
-                    parameters.callback = {
-                        onSuccess: function(response) {
-                            var data = response.data;
-                            for (var j=0; j<data.results.length; j++){
-                                vm.myChallengeList.push(data.results[j]);
-                            }
-                        },
-                        onError: function() {
-                            utilities.hideLoader();
-                        }
-                    };
-                    utilities.sendRequest(parameters);
-                }
-                console.log(vm.myChallengeList);
-                if (vm.noneMyChallenges.length === 0) {
-                    vm.noneMyChallenges = true;
-                } else {
-                    vm.noneMyChallenges = false;
-                }
-
-            },
-            onError: function() {
-                utilities.hideLoader();
-            }
-        };
-
-        utilities.sendRequest(parameters);
-
     }
 
 })();
