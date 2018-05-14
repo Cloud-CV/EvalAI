b69de0f6abc49ac53072b9f43d4f65a3c2effef3
Adarsh S <4darshofficial@gmail.com>
2018-05-14 13:03:14 +0530

    Fixes #1618: Limit number of concurrent submissions for a participant. (#1660)

--- apps/jobs/views.py
+++ apps/jobs/views.py
@@ -103,6 +103,19 @@ def challenge_submission(request, challenge_id, challenge_phase_id):
             response_data = {'error': 'You haven\'t participated in the challenge'}
             return Response(response_data, status=status.HTTP_403_FORBIDDEN)
 
+        # Fetch the number of submissions under progress.
+        submissions_in_progress_status = [Submission.SUBMITTED, Submission.SUBMITTING, Submission.RUNNING]
+        submissions_in_progress = Submission.objects.filter(
+                                                participant_team=participant_team_id,
+                                                challenge_phase=challenge_phase,
+                                                status__in=submissions_in_progress_status).count()
+
+        if submissions_in_progress >= challenge_phase.max_concurrent_submissions_allowed:
+            message = 'You have {} submissions that are being processed. \
+                       Please wait for them to finish and then try again.'
+            response_data = {'error': message.format(submissions_in_progress)}
+            return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
+
         serializer = SubmissionSerializer(data=request.data,
                                           context={'participant_team': participant_team,
                                                    'challenge_phase': challenge_phase,
