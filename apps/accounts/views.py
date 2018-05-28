99b8218b6c2cacea1a0b80d117ef213b2b4c6fb0
Adarsh S <4darshofficial@gmail.com>
2018-05-28 21:59:47 +0530

    Fix #1688: Add API endpoint to fetch or create user auth token from database (#1683)
    * Add endpoint to process token
    
    * Add tests for token backend

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,11 +1,17 @@
 from django.contrib.auth import logout
+from django.contrib.auth.models import User
 
+from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework import permissions, status
 from rest_framework.decorators import (api_view,
                                        authentication_classes,
-                                       permission_classes,)
+                                       permission_classes,
+                                       throttle_classes,)
+from rest_framework.throttling import UserRateThrottle
 from rest_framework_expiring_authtoken.authentication import (ExpiringTokenAuthentication,)
+
+from .permissions import HasVerifiedEmail
 
 
 @api_view(['POST'])
@@ -18,3 +24,24 @@ def disable_user(request):
     user.save()
     logout(request)
     return Response(status=status.HTTP_200_OK)
+
+
+@throttle_classes([UserRateThrottle])
+@api_view(['GET'])
+@permission_classes((permissions.IsAuthenticated, HasVerifiedEmail))
+@authentication_classes((ExpiringTokenAuthentication,))
+def get_auth_token(request):
+    try:
+        user = User.objects.get(email=request.user.email)
+    except User.DoesNotExist:
+        response_data = {"error": "This User account doesn't exist."}
+        Response(response_data, status.HTTP_404_NOT_FOUND)
+
+    try:
+        token = Token.objects.get(user=user)
+    except Token.DoesNotExist:
+        token = Token.objects.create(user=user)
+        token.save()
+
+    response_data = {"token": "{}".format(token)}
+    return Response(response_data, status=status.HTTP_200_OK)
