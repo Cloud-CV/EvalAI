d52b24ac0fd41a24eba5af0bcb225c038f4d6a8d
Dhruv Batheja <dhruv.life@hotmail.com>
2018-06-15 11:53:48 +0200

    Experimenting with surge (#58)

--- pr_deploy.sh
+++ pr_deploy.sh
@@ -3,11 +3,14 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     echo "Not a Pull Request. Skipping surge deployment"
     exit 0
 fi
-
+echo "PR Deploy routine START"
 npm i -g surge
 
-export SURGE_LOGIN=$SURGE_LOGIN
-export SURGE_TOKEN=$SURGE_TOKEN
+# export SURGE_LOGIN=$SURGE_LOGIN
+# export SURGE_TOKEN=$SURGE_TOKEN
 
 export DEPLOY_DOMAIN=https://pr-${TRAVIS_PULL_REQUEST}-evalai.surge.sh
+echo $SURGE_LOGIN
+echo $DEPLOY_DOMAIN
+
 surge --project ./dist --domain $DEPLOY_DOMAIN;
