74e6647c3117330ab90975ae005742310692d63c
RishabhJain2018 <rishabhjain2018@gmail.com>
2018-09-07 18:24:58 -0400

    Add Review Comments

--- scripts/deploy/deploy.sh
+++ scripts/deploy/deploy.sh
@@ -3,33 +3,25 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "staging" -o "$TRA
     exit 0
 fi
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" -a "TRAVIS_BRANCH" != "staging"]; then
+build_and_push() {
 
-    # Set AWS default region
-    aws configure set default.region us-east-1
+    if [ $2 == "false" -a $1 == "staging" -o $1 == "production"]; then
 
-    # Authenticate against our Docker registry
-    eval $(aws ecr get-login --no-include-email)
+        # Set AWS default region
+        aws configure set default.region us-east-1
 
-    # Build and push the container images
-    docker-compose -f docker-compose-staging.yml build
-    docker-compose -f docker-compose-staging.yml push
-    exit 0
+        # Authenticate against our Docker registry
+        eval $(aws ecr get-login --no-include-email)
 
-elif [ "$TRAVIS_PULL_REQUEST" != "false" -a "TRAVIS_BRANCH" != "production"]; then
+        # Build and push the container images
+        docker-compose -f docker-compose-$1.yml build
+        docker-compose -f docker-compose-$1.yml push
 
-    # Set AWS default region
-    aws configure set default.region us-east-1
+    else
+        echo "Skipping deploy to both Staging or Production Server"
+        exit 0
+    fi
 
-    # Authenticate against our Docker registry
-    eval $(aws ecr get-login --no-include-email)
+}
 
-    # Build and push the container images
-    docker-compose -f docker-compose-prod.yml build
-    docker-compose -f docker-compose-prod.yml push
-    exit 0
-
-else
-    echo "Skipping deploy to both Staging or Production Server"
-    exit 0
-fi
+build_and_push $TRAVIS_BRANCH, $TRAVIS_PULL_REQUEST
