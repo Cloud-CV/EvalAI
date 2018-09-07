c3620739aa35dcbd494c3cb4601c69138549cc3e
RishabhJain2018 <rishabhjain2018@gmail.com>
2018-09-07 00:28:09 -0400

    Add production env settings

--- scripts/deploy/production.sh
+++ scripts/deploy/production.sh
@@ -1,8 +1,8 @@
-BRANCH="staging"
+BRANCH="production"
 SHA=`git rev-parse --verify HEAD`
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$BRANCH" ]; then
-    echo "Skipping deploy to staging server; The request or commit is not on staging branch"
+    echo "Skipping deploy to production server; The request or commit is not on production branch"
     exit 0
 fi
 
@@ -13,5 +13,5 @@ aws configure set default.region us-east-1
 eval $(aws ecr get-login --no-include-email)
 
 # Build and push the Django container image
-docker-compose -f docker-compose-staging.yml build
-docker-compose -f docker-compose-staging.yml push
+docker-compose -f docker-compose-prod.yml build
+docker-compose -f docker-compose-prod.yml push
