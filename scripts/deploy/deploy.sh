b36236385629583c65afd649499eb4f4bb049717
RishabhJain2018 <rishabhjain2018@gmail.com>
2018-09-07 22:57:32 -0400

    Modified conditions for deploying

--- scripts/deploy/deploy.sh
+++ scripts/deploy/deploy.sh
@@ -1,12 +1,4 @@
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "staging" -o "$TRAVIS_BRANCH" != "production" ]; then
-    echo "Skipping deploy to staging or production server; The request or commit is not on staging or production branch"
-    exit 0
-fi
-
 build_and_push() {
-
-    if [ $2 == "false" -a $1 == "staging" -o $1 == "production"]; then
-
         # Set AWS default region
         aws configure set default.region us-east-1
 
@@ -16,12 +8,17 @@ build_and_push() {
         # Build and push the container images
         docker-compose -f docker-compose-$1.yml build
         docker-compose -f docker-compose-$1.yml push
-
-    else
-        echo "Skipping deploy to both Staging or Production Server"
-        exit 0
-    fi
-
 }
 
-build_and_push $TRAVIS_BRANCH, $TRAVIS_PULL_REQUEST
+if [ "${TRAVIS_PULL_REQUEST}" != "false"]; then
+    echo "Skipping deploy to staging or production server; The request or commit is not on staging or production branch"
+    exit 0
+
+elif [ "${TRAVIS_BRANCH}" == "staging" -o "${TRAVIS_BRANCH}" == "production"]; then
+    build_and_push $TRAVIS_BRANCH
+    exit 0
+
+else
+    echo "Skipping deploy!"
+    exit 0
+fi
