ad5515c11e39f09aa7c332440375eabbdd1011ae
Deshraj <deshraj@gatech.edu>
2018-09-11 16:52:25 -0400

    Add sample docker env files

--- scripts/deploy/deploy.sh
+++ scripts/deploy/deploy.sh
@@ -8,17 +8,15 @@ build_and_push() {
         docker-compose -f docker-compose-$1.yml push
 }
 
-# if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
-#     echo "Skipping deploy to staging or production server; The request or commit is not on staging or production branch"
-#     exit 0
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+    echo "Skipping deploy to staging or production server; The request or commit is not on staging or production branch"
+    exit 0
 
-# elif [ "${TRAVIS_BRANCH}" == "staging" -o "${TRAVIS_BRANCH}" == "production" ]; then
-#     build_and_push $TRAVIS_BRANCH
-#     exit 0
+elif [ "${TRAVIS_BRANCH}" == "staging" -o "${TRAVIS_BRANCH}" == "production" ]; then
+    build_and_push $TRAVIS_BRANCH
+    exit 0
 
-# else
-#     echo "Skipping deploy!"
-#     exit 0
-# fi
-
-build_and_push staging
+else
+    echo "Skipping deploy!"
+    exit 0
+fi
