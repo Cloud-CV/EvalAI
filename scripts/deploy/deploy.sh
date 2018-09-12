3cbf931000eec921619b529b4289fdfeedd5d0c1
RishabhJain2018 <rishabhjain2018@gmail.com>
2018-09-12 17:20:57 -0400

    Tag & push the images with latest & travis commit id tag

--- scripts/deploy/deploy.sh
+++ scripts/deploy/deploy.sh
@@ -6,6 +6,12 @@ build_and_push() {
         eval $(aws ecr get-login --no-include-email)
         docker-compose -f docker-compose-$1.yml build
         docker-compose -f docker-compose-$1.yml push
+        images=$(cat docker-compose-staging.yml | grep 'image: ' | cut -d':' -f 2 | tr -d '"')
+        for image in $images
+        do
+            docker tag "${image}":"$2" "${image}":"latest"
+            docker push "${image}":"latest"
+        done
 }
 
 if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
@@ -13,7 +19,7 @@ if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
     exit 0
 
 elif [ "${TRAVIS_BRANCH}" == "staging" -o "${TRAVIS_BRANCH}" == "production" ]; then
-    build_and_push $TRAVIS_BRANCH
+    build_and_push $TRAVIS_BRANCH $TRAVIS_COMMIT
     exit 0
 
 else
