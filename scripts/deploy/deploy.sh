34f1acfaaf575c4db73aef0cb8168af84f3691e6
RishabhJain2018 <rishabhjain2018@gmail.com>
2018-09-12 17:37:40 -0400

    Add comments in the script

--- scripts/deploy/deploy.sh
+++ scripts/deploy/deploy.sh
@@ -6,7 +6,11 @@ build_and_push() {
         eval $(aws ecr get-login --no-include-email)
         docker-compose -f docker-compose-$1.yml build
         docker-compose -f docker-compose-$1.yml push
+
+        # Get the build docker images
         images=$(cat docker-compose-$1.yml | grep 'image: ' | cut -d':' -f 2 | tr -d '"')
+
+        # It is used to tag & push the images with latest tag
         for image in $images
         do
             docker tag "${image}":"$2" "${image}":"latest"
