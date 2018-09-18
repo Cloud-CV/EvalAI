ba86756fedf00397930611e137819b355b901905
Deshraj Yadav <deshraj@gatech.edu>
2018-09-18 08:34:02 -0400

    Update staging.py

--- settings/staging.py
+++ settings/staging.py
@@ -4,9 +4,6 @@ ALLOWED_HOSTS = [
     'staging.evalai.cloudcv.org',
 ]
 
-# Database
-# https://docs.djangoproject.com/en/1.10.2/ref/settings/#databases
-
 CORS_ORIGIN_ALLOW_ALL = False
 
 CORS_ORIGIN_WHITELIST = (
