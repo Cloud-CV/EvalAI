8716003cf25f5df5663b632e924438c2c52264ca
Kartik Verma <3920286+vkartik97@users.noreply.github.com>
2018-09-21 19:19:31 +0530

    CI/CD: Add short circuit behaviour using logical AND (#1818)

--- docker/prod/django/container-start.sh
+++ docker/prod/django/container-start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+python manage.py migrate --noinput  && \
+python manage.py collectstatic --noinput  && \
 uwsgi --ini /code/docker/prod/django/uwsgi.ini
