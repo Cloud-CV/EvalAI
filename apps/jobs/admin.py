5b6fd38813d3ee6bb509be655c73b6d5aa71b955
rishabhjain <rishabhjain2018@gmail.com>
2018-10-21 01:26:05 -0400

    Add admin action to evaluate the submissions again(#1827)
    * Add admin action to run submissions again
    
    * Flake8 fixes

--- apps/jobs/admin.py
+++ apps/jobs/admin.py
@@ -1,8 +1,14 @@
+import logging
+
 from django.contrib import admin
 
 from base.admin import ImportExportTimeStampedAdmin
 
 from .models import Submission
+from .sender import publish_submission_message
+
+
+logger = logging.getLogger(__name__)
 
 
 @admin.register(Submission)
@@ -14,3 +20,17 @@ class SubmissionAdmin(ImportExportTimeStampedAdmin):
                    'status', 'is_public')
     search_fields = ('participant_team__team_name', 'challenge_phase__name',
                      'created_by__username', 'status')
+
+    actions = ['submit_job_to_worker']
+
+    def submit_job_to_worker(self, request, queryset):
+        for submission in queryset:
+            challenge_id = submission.challenge_phase.challenge.id
+            challenge_phase_id = submission.challenge_phase.id
+            submission_id = submission.id
+            logger.info("[x] Received submission message with challenge id {}, challenge phase id {}, submission id {}"
+                        .format(challenge_id, challenge_phase_id, submission_id))
+            publish_submission_message(challenge_id, challenge_phase_id, submission.id)
+            queryset.update(status=Submission.SUBMITTED)
+
+    submit_job_to_worker.short_description = "Run selected submissions"
