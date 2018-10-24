e64e5975e664eae69f8bc6c30f93bcef83998ae2
Deshraj Yadav <deshraj@gatech.edu>
2018-10-24 01:48:34 -0400

    Update docker configuration and optimize docker image size(#1860)
    * Update docker configuration and optimize image size
    
    * Remove AWS_ACCOUNT_ID
    
    * Re-add containe name

--- scripts/deploy/deploy.sh
+++ scripts/deploy/deploy.sh
@@ -1,32 +1,24 @@
 #!/bin/bash
 set -e
 
+export COMMIT_ID=$(git rev-parse HEAD)
+
 build_and_push() {
         aws configure set default.region us-east-1
         eval $(aws ecr get-login --no-include-email)
         docker-compose -f docker-compose-$1.yml build
         docker-compose -f docker-compose-$1.yml push
 
-        # Get the build docker images
+        # Get already built docker images
         images=$(cat docker-compose-$1.yml | grep 'image: ' | cut -d':' -f 2 | tr -d '"')
 
-        # It is used to tag & push the images with latest tag
+        # Tag & push images with latest tag
         for image in $images
         do
-            docker tag "${image}":"$2" "${image}":"latest"
-            docker push "${image}":"latest"
+            eval image=${image}
+            docker tag ${image}:${COMMIT_ID} ${image}:latest
+            docker push ${image}:latest
         done
 }
 
-if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
-    echo "Skipping deploy to staging or production server; The request or commit is not on staging or production branch"
-    exit 0
-
-elif [ "${TRAVIS_BRANCH}" == "staging" -o "${TRAVIS_BRANCH}" == "production" ]; then
-    build_and_push $TRAVIS_BRANCH $TRAVIS_COMMIT
-    exit 0
-
-else
-    echo "Skipping deploy!"
-    exit 0
-fi
+build_and_push $1
