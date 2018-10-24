5fee577395ea000d142822e43d322421f6f70c76
Deshraj <deshraj@gatech.edu>
2018-10-23 20:29:59 -0400

    Minor fix

--- apps/jobs/sender.py
+++ apps/jobs/sender.py
@@ -18,7 +18,7 @@ def get_or_create_sqs_queue():
     Returns:
         Returns the SQS Queue object
     """
-    if settings.DEBUG:
+    if settings.DEBUG or settings.TEST:
         sqs = boto3.resource('sqs',
                              endpoint_url=os.environ.get('AWS_SQS_ENDPOINT', 'http://sqs:9324'),
                              region_name=os.environ.get('AWS_DEFAULT_REGION', 'us-east-1'),
