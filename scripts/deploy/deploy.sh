8d738c03808346cbf0a09469e41a763d59e3595f
Deshraj <deshraj@gatech.edu>
2018-10-24 01:22:19 -0400

    Update docker configuration and optimize image size

--- scripts/deploy/deploy.sh
+++ scripts/deploy/deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+export AWS_ACCOUNT_ID=937891341272
+export COMMIT_ID=$(git rev-parse HEAD)
+
 build_and_push() {
         aws configure set default.region us-east-1
         eval $(aws ecr get-login --no-include-email)
@@ -13,20 +16,10 @@ build_and_push() {
         # It is used to tag & push the images with latest tag
         for image in $images
         do
-            docker tag "${image}":"$2" "${image}":"latest"
-            docker push "${image}":"latest"
+            eval image=${image}
+            docker tag ${image}:${COMMIT_ID} ${image}:latest
+            docker push ${image}:latest
         done
 }
 
-if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
-    echo "Skipping deploy to staging or production server; The request or commit is not on staging or production branch"
-    exit 0
-
-elif [ "${TRAVIS_BRANCH}" == "staging" -o "${TRAVIS_BRANCH}" == "production" ]; then
-    build_and_push $TRAVIS_BRANCH $TRAVIS_COMMIT
-    exit 0
-
-else
-    echo "Skipping deploy!"
-    exit 0
-fi
+build_and_push $1
