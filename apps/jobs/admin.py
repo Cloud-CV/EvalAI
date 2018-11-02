d3dc67d5dae90b74b227153c04146be32317d270
Deshraj Yadav <deshraj@gatech.edu>
2018-11-01 20:41:06 -0400

    Update admin.py

--- apps/jobs/admin.py
+++ apps/jobs/admin.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 
 @admin.register(Submission)
 class SubmissionAdmin(ImportExportTimeStampedAdmin):
-    list_display = ('participant_team', 'challenge_phase', 'challenge', 'created_by', 'status', 'is_public',
+    list_display = ('participant_team', 'challenge', 'challenge_phase', 'created_by', 'status', 'is_public',
                     'submission_number', 'submitted_at', 'execution_time', 'input_file', 'stdout_file', 'stderr_file',
                     'submission_result_file', 'submission_metadata_file', )
     list_filter = ('participant_team', 'challenge_phase',
