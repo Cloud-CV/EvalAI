f1a484cec15e267463ffafef7e9de7c6d73feb03
RishabhJain2018 <rishabhjain2018@gmail.com>
2018-12-03 04:59:38 -0500

    Add production deployment ssl fixes

--- scripts/deployment/deploy.sh
+++ scripts/deployment/deploy.sh
@@ -46,8 +46,8 @@ case $opt in
             echo "Environment varibles file successfully downloaded."
             if [ ${env} == "production" ]; then
                 echo "Pulling ssl certificates and nginx configuration..."
-                aws s3 cp s3://cloudcv-secrets/evalai/${env}/ssl/ ./ssl/
-                aws s3 cp s3://cloudcv-secrets/evalai/${env}/nginx_${env}.conf ./docker/prod/nodejs/nginx_${env}.conf
+                aws s3 cp s3://cloudcv-secrets/evalai/${env}/ssl/ ./ssl/ --recursive
+                aws s3 cp s3://cloudcv-secrets/evalai/${env}/nginx_${env}.conf ./docker/prod/nodejs/nginx_${env}.conf --recursive
             fi
             echo "Pulling docker images from ECR..."
             docker-compose -f docker-compose-${env}.yml pull
