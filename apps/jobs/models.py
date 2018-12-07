40daea7f0ab5dd8dc5e59e39ae625bc9dd13b38c
Kartik Verma <vkartik97@gmail.com>
2018-12-07 12:19:13 +0530

    Fixed flake8 for jobs

--- apps/jobs/models.py
+++ apps/jobs/models.py
@@ -158,11 +158,12 @@ class Submission(TimeStampedModel):
                     participant_team=self.participant_team,
                     status=Submission.FAILED,
                     submitted_at__gte=timezone.now().replace(day=1, hour=0, minute=0, second=0, microsecond=0)).count()
-                if ((submissions_done_in_month_count + 1 - failed_submissions_count_in_month > self.challenge_phase.max_submissions_per_month) or
+                if ((submissions_done_in_month_count + 1 - failed_submissions_count_in_month >
+                        self.challenge_phase.max_submissions_per_month) or
                         (self.challenge_phase.max_submissions_per_month == 0)):
                     logger.info("Permission Denied: The maximum number of submission for this Month has been reached")
-                    raise PermissionDenied({'error': 'The maximum number of submission for this Month has been reached'})
-                
+                    raise PermissionDenied({'error':
+                                            'The maximum number of submission for this Month has been reached'})
                 if ((submissions_done_today_count + 1 - failed_count > self.challenge_phase.max_submissions_per_day) or
                         (self.challenge_phase.max_submissions_per_day == 0)):
                     logger.info("Permission Denied: The maximum number of submission for today has been reached")
