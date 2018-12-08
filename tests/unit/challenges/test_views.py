787038f11826ad0d8e5ae99aaf26d429b16c1fbd
rishabhjain <rishabhjain2018@gmail.com>
2018-12-08 12:35:05 -0500

    Fix Travis errors

--- tests/unit/challenges/test_views.py
+++ tests/unit/challenges/test_views.py
@@ -1096,7 +1096,6 @@ class GetChallengeByPk(BaseAPITestClass):
             is_disabled=True,
         )
 
-
     def test_get_challenge_by_pk_when_challenge_does_not_exists(self):
         self.url = reverse_lazy('challenges:get_challenge_by_pk',
                                 kwargs={'pk': self.challenge3.pk + 10})
@@ -1188,7 +1187,7 @@ class GetChallengeByPk(BaseAPITestClass):
         self.client.force_authenticate(user=self.user1)
         response = self.client.get(self.url, {})
         self.assertEqual(response.data, expected)
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_get_challenge_by_pk_when_challenge_is_disabled(self):
         self.url = reverse_lazy('challenges:get_challenge_by_pk',
