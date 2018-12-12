f37673405c8f58111151a5b6c82501740a36a982
rishabhjain <rishabhjain2018@gmail.com>
2018-12-12 05:35:04 -0500

    Scripts: Add script to populate existing challenge Broker URLs (#1949)

---
+++ scripts/migration/generate_and_set_broker_url.py
@@ -0,0 +1,21 @@
+# Command to run: python manage.py shell < scripts/migration/generate_and_set_broker_url.py
+# TODO: Run the code using a function based approach
+
+import traceback
+import uuid
+
+from challenges.models import Challenge
+
+challenges = Challenge.objects.filter(approved_by_admin=True)
+
+try:
+    for challenge in challenges:
+        challenge_title = challenge.title.split(' ')
+        challenge_title = '-'.join(challenge_title).lower()
+        random_challenge_id = uuid.uuid4()
+        challenge_queue_name = "{}-{}".format(challenge_title, random_challenge_id)
+        challenge.queue = challenge_queue_name
+        challenge.save()
+except Exception as e:
+    print(e)
+    print(traceback.print_exc())
