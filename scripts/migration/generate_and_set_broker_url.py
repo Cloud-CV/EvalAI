a3e6578e7c2c11cee2c04c8b8c1d59ea0ce0680f
RishabhJain2018 <rishabhjain2018@gmail.com>
2018-12-14 01:58:04 -0500

    Update queue name to include challenge ID

--- scripts/migration/generate_and_set_broker_url.py
+++ scripts/migration/generate_and_set_broker_url.py
@@ -6,14 +6,15 @@ import uuid
 
 from challenges.models import Challenge
 
-challenges = Challenge.objects.filter(approved_by_admin=True)
+challenges = Challenge.objects.all()
 
 try:
     for challenge in challenges:
         challenge_title = challenge.title.split(' ')
+        challenge_pk = challenge.pk
         challenge_title = '-'.join(challenge_title).lower()
         random_challenge_id = uuid.uuid4()
-        challenge_queue_name = "{}-{}".format(challenge_title, random_challenge_id)
+        challenge_queue_name = "{}-{}-{}".format(challenge_title, challenge_pk, random_challenge_id)[:75]
         challenge.queue = challenge_queue_name
         challenge.save()
 except Exception as e:
