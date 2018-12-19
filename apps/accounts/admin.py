938fffc134e40d66af7dea147cdfa8a4e00abbd6
Deshraj <deshraj@gatech.edu>
2018-12-19 14:43:34 -0500

    Import/Export: Minor improvements in import-export for different apps

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,1 +1,19 @@
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+from import_export import resources
+from import_export.admin import ExportMixin
+
+
+class UserResource(resources.ModelResource):
+    class Meta:
+        model = User
+        fields = ('date_joined', 'email', 'first_name', 'last_login', 'last_name', 'staff_status', 'username',)
+
+
+class UserAdmin(ExportMixin, UserAdmin):
+    resource_class = UserResource
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
