45ff0fe0a305587e81fd8434f039002ab2f9c9bb
Deshraj Yadav <deshraj@gatech.edu>
2018-12-20 04:38:08 -0500

    Import/Export: Minor improvements in import-export for different apps (#1971)

--- apps/challenges/admin.py
+++ apps/challenges/admin.py
@@ -15,56 +15,76 @@ from .models import (Challenge,
 @admin.register(Challenge)
 class ChallengeAdmin(ImportExportTimeStampedAdmin):
     readonly_fields = ('created_at',)
-    list_display = ("title", "start_date", "end_date", "creator", "published", "enable_forum", "anonymous_leaderboard",
-                    "featured", "created_at")
-    list_filter = ("creator", "published", "enable_forum", "anonymous_leaderboard", "featured")
-    search_fields = ("title", "creator__team_name")
-
-
-@admin.register(DatasetSplit)
-class DatasetSplitAdmin(ImportExportTimeStampedAdmin):
-    list_display = ("name", "codename")
-    list_filter = ("name", "codename")
-    search_fields = ("name", "codename")
-
-
-@admin.register(ChallengePhase)
-class ChallengePhaseAdmin(ImportExportTimeStampedAdmin):
-    list_display = ("name", "challenge", "start_date", "end_date", "test_annotation", "is_public", "leaderboard_public")
-    list_filter = ("leaderboard_public", "challenge")
-    search_fields = ("name",)
-
-
-@admin.register(Leaderboard)
-class LeaderboardAdmin(ImportExportTimeStampedAdmin):
-    list_display = ("id", "schema")
-    search_fields = ("id",)
-
-
-@admin.register(ChallengePhaseSplit)
-class ChallengePhaseSplitAdmin(ImportExportTimeStampedAdmin):
-    list_display = ("id", "challenge_phase", "dataset_split", "leaderboard", "visibility")
-    list_filter = ("challenge_phase", "dataset_split", "leaderboard", "visibility")
-    search_fields = ("challenge_phase__name", "dataset_split__name", "leaderboard__id",
-                     "dataset_split__codename")
-
-
-@admin.register(LeaderboardData)
-class LeaderboardDataAdmin(ImportExportTimeStampedAdmin):
-    list_display = ("challenge_phase_split", "submission", "leaderboard", "result")
-    list_filter = ("challenge_phase_split", "leaderboard",)
-    search_fields = ("challenge_phase_split__challenge_phase__name",
-                     "submission__participant_team__team_name", "leaderboard__schema", "result")
+    list_display = ('id', 'title', 'start_date', 'end_date', 'creator', 'published',
+                    'enable_forum', 'anonymous_leaderboard', 'featured', 'created_at',)
+    list_filter = ('published', 'enable_forum', 'anonymous_leaderboard', 'featured', 'start_date', 'end_date',)
+    search_fields = ('title', 'creator', 'creator__team_name',)
 
 
 @admin.register(ChallengeConfiguration)
 class ChallengeConfigurationAdmin(ImportExportTimeStampedAdmin):
-    list_display = ('user', 'challenge', 'created_at', 'is_created', 'zip_configuration',)
-    list_filter = ('user', 'is_created',)
-    search_fields = ('user__username', 'challenge__title',)
+    list_display = ('id', 'user', 'challenge', 'created_at', 'is_created', 'zip_configuration',)
+    list_filter = ('is_created', 'created_at',)
+    search_fields = ('challenge__title',)
+
+
+@admin.register(ChallengePhase)
+class ChallengePhaseAdmin(ImportExportTimeStampedAdmin):
+    list_display = ('id', 'name', 'get_challenge_name_and_id', 'start_date',
+                    'end_date', 'test_annotation', 'is_public', 'leaderboard_public',)
+    list_filter = ('leaderboard_public', 'start_date', 'end_date',)
+    search_fields = ('name', 'challenge__title',)
+
+    def get_challenge_name_and_id(self, obj):
+        """Return challenge name corresponding to phase"""
+        return "%s - %s" % (obj.challenge.title, obj.challenge.id)
+    get_challenge_name_and_id.short_description = 'Challenge'
+    get_challenge_name_and_id.admin_order_field = 'challenge'
+
+
+@admin.register(ChallengePhaseSplit)
+class ChallengePhaseSplitAdmin(ImportExportTimeStampedAdmin):
+    list_display = ('id', 'get_challenge', 'challenge_phase', 'dataset_split', 'leaderboard', 'visibility',)
+    list_filter = ('dataset_split', 'leaderboard', 'visibility',)
+    search_fields = ('challenge_phase__name', 'dataset_split__name', 'leaderboard__id',
+                     'dataset_split__codename',)
+
+    def get_challenge(self, obj):
+        """Returns challenge name corresponding to phase-split"""
+        return obj.challenge_phase.challenge
+    get_challenge.short_description = 'Challenge'
+    get_challenge.admin_order_field = 'challenge_phase__challenge'
+
+
+@admin.register(DatasetSplit)
+class DatasetSplitAdmin(ImportExportTimeStampedAdmin):
+    list_display = ('name', 'codename',)
+    list_filter = ('name', 'codename',)
+    search_fields = ('name', 'codename',)
+
+
+@admin.register(Leaderboard)
+class LeaderboardAdmin(ImportExportTimeStampedAdmin):
+    list_display = ('id', 'schema',)
+    search_fields = ('schema',)
+
+
+@admin.register(LeaderboardData)
+class LeaderboardDataAdmin(ImportExportTimeStampedAdmin):
+    list_display = ('get_challenge', 'challenge_phase_split', 'submission', 'leaderboard', 'result',)
+    list_filter = ('challenge_phase_split', 'created_at', 'modified_at',)
+    search_fields = ('challenge_phase_split__challenge_phase__name',
+                     'submission__participant_team__team_name', 'leaderboard__schema', 'result',)
+
+    def get_challenge(self, obj):
+        """Returns challenge name corresponding to leaderboard data entry"""
+        return obj.challenge_phase.challenge
+    get_challenge.short_description = 'Challenge'
+    get_challenge.admin_order_field = 'challenge_phase__challenge'
 
 
 @admin.register(StarChallenge)
 class StarChallengeAdmin(ImportExportTimeStampedAdmin):
-    list_display = ('user', 'challenge', 'is_starred')
+    list_display = ('user', 'challenge', 'is_starred',)
+    list_filter = ('is_starred',)
     search_fields = ('user__username', 'challenge__title',)
--- apps/jobs/admin.py
+++ apps/jobs/admin.py
@@ -13,15 +13,21 @@ logger = logging.getLogger(__name__)
 
 @admin.register(Submission)
 class SubmissionAdmin(ImportExportTimeStampedAdmin):
-    list_display = ('participant_team', 'challenge_phase', 'created_by', 'status', 'is_public',
-                    'submission_number', 'submitted_at', 'execution_time', 'input_file', 'stdout_file', 'stderr_file',
-                    'submission_result_file', 'submission_metadata_file', )
-    list_filter = ('participant_team', 'challenge_phase',
-                   'status', 'is_public')
-    search_fields = ('participant_team__team_name', 'challenge_phase__name',
-                     'created_by__username', 'status')
-
     actions = ['submit_job_to_worker']
+    list_display = ('participant_team', 'get_challenge_name_and_id', 'challenge_phase',
+                    'created_by', 'status', 'is_public', 'submission_number', 'submitted_at',
+                    'execution_time', 'input_file', 'stdout_file', 'stderr_file',
+                    'submission_result_file', 'submission_metadata_file',)
+    list_filter = ('participant_team', 'challenge_phase',
+                   'status', 'is_public',)
+    search_fields = ('participant_team__team_name', 'challenge_phase__name',
+                     'created_by__username', 'status',)
+
+    def get_challenge_name_and_id(self, obj):
+        """Return challenge name corresponding to phase"""
+        return "%s - %s" % (obj.challenge_phase.challenge.title, obj.challenge_phase.challenge.id)
+    get_challenge_name_and_id.short_description = 'Challenge'
+    get_challenge_name_and_id.admin_order_field = 'challenge_phase__challenge'
 
     def submit_job_to_worker(self, request, queryset):
         for submission in queryset:
