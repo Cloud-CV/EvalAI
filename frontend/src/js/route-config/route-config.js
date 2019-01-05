51a8e9fd9777dabbc72c1e129e8d258e336d9c43
Rishabh Jain <rishabhjain2018@gmail.com>
2019-01-05 18:08:19 -0500

    Fix #1977: Fix back behaviour on landing page (#1978)

--- frontend/src/js/route-config/route-config.js
+++ frontend/src/js/route-config/route-config.js
@@ -546,7 +546,7 @@
         $rootScope.$on('$stateChangeStart', function(event, to, params) {
             if (to.redirectTo) {
                 event.preventDefault();
-                $state.go(to.redirectTo, params, { location: 'replace' });
+                $state.go(to.redirectTo, params, { location: $location.path() });
             }
         });
 
