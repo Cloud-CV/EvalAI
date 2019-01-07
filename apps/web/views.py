a676b18881d51d9d18b7d5873fd599871ae652d5
Charanjit Singh <charanjitdotsingh@gmail.com>
2019-01-07 20:14:26 +0530

    Update views.py
    Using decorators instead of nested if elses for authenticated requests.

--- apps/web/views.py
+++ apps/web/views.py
@@ -41,41 +41,37 @@ def internal_server_error(request):
     response.status_code = 500
     return response
 
-
+@permission_classes((permissions.IsAuthenticated,))
 def notify_users_about_challenge(request):
     """
     Email New Challenge Details to EvalAI Users
     """
-    if request.user.is_authenticated() and request.user.is_superuser:
-        if request.method == 'GET':
-            template_name = 'notification_email_data.html'
-            return render(request, template_name)
+    if request.method == 'GET':
+        template_name = 'notification_email_data.html'
+        return render(request, template_name)
 
-        elif request.method == 'POST':
-            users = User.objects.exclude(email__exact='').values_list('email', flat=True)
-            subject = request.POST.get('subject')
-            body_html = request.POST.get('body')
+    elif request.method == 'POST':
+        users = User.objects.exclude(email__exact='').values_list('email', flat=True)
+        subject = request.POST.get('subject')
+        body_html = request.POST.get('body')
 
-            sender = settings.CLOUDCV_TEAM_EMAIL
+        sender = settings.CLOUDCV_TEAM_EMAIL
 
-            email = EmailMessage(subject, body_html, sender, [settings.CLOUDCV_TEAM_EMAIL], bcc=users)
-            email.content_subtype = 'html'
+        email = EmailMessage(subject, body_html, sender, [settings.CLOUDCV_TEAM_EMAIL], bcc=users)
+        email.content_subtype = 'html'
 
-            try:
-                email.send()
-                return render(request,
-                              'notification_email_conformation.html',
-                              {'message': 'All the emails are sent successfully!'})
-            except SMTPException:
-                logger.exception(traceback.format_exc())
-                return render(request,
-                              'notification_email_data.html',
-                              {'errors': 1})
-        else:
-            return render(request, 'error404.html')
+        try:
+            email.send()
+            return render(request,
+                          'notification_email_conformation.html',
+                          {'message': 'All the emails are sent successfully!'})
+        except SMTPException:
+            logger.exception(traceback.format_exc())
+            return render(request,
+                          'notification_email_data.html',
+                          {'errors': 1})
     else:
         return render(request, 'error404.html')
-
 
 @throttle_classes([AnonRateThrottle, ])
 @api_view(['GET', 'POST'])
