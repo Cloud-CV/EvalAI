9172cbb7b52e5e5492330aa010f32003a80ed6d4
Yash Dusing <yashdusing145@gmail.com>
2019-01-26 14:08:13 +0530

    Update views.py

--- apps/challenges/views.py
+++ apps/challenges/views.py
@@ -84,8 +84,7 @@ def challenge_list(request, challenge_host_team_pk):
         return Response(response_data, status=status.HTTP_406_NOT_ACCEPTABLE)
 
     if request.method == 'GET':
-        challenge = Challenge.objects.filter(
-            creator=challenge_host_team, is_disabled=False).order_by('-id')
+        challenge = Challenge.objects.filter(creator=challenge_host_team, is_disabled=False).order_by('-id')
         paginator, result_page = paginated_queryset(challenge, request)
         serializer = ChallengeSerializer(
             result_page, many=True, context={'request': request})
@@ -173,8 +172,7 @@ def add_participant_team_to_challenge(request, challenge_pk, participant_team_pk
         return Response(response_data, status=status.HTTP_406_NOT_ACCEPTABLE)
 
     if challenge.end_date < timezone.now() or challenge.start_date > timezone.now():
-        response_data = {
-            'error': 'Sorry, cannot accept participant team since challenge is not active.'}
+        response_data = {'error': 'Sorry, cannot accept participant team since challenge is not active.'}
         return Response(response_data, status=status.HTTP_406_NOT_ACCEPTABLE)
 
     try:
@@ -280,8 +278,7 @@ def get_all_challenges(request, challenge_time):
 
     challenge = Challenge.objects.filter(**q_params).order_by('-pk')
     paginator, result_page = paginated_queryset(challenge, request)
-    serializer = ChallengeSerializer(
-        result_page, many=True, context={'request': request})
+    serializer = ChallengeSerializer(result_page, many=True, context={'request': request})
     response_data = serializer.data
     return paginator.get_paginated_response(response_data)
 
@@ -314,8 +311,7 @@ def get_challenge_by_pk(request, pk):
         if is_user_a_host_of_challenge(request.user, pk):
             challenge = Challenge.objects.get(pk=pk)
         else:
-            challenge = Challenge.objects.get(
-                pk=pk, approved_by_admin=True, published=True)
+            challenge = Challenge.objects.get(pk=pk, approved_by_admin=True, published=True)
         if (challenge.is_disabled):
             response_data = {'error': 'Sorry, the challenge was removed!'}
             return Response(response_data, status=status.HTTP_406_NOT_ACCEPTABLE)
@@ -469,8 +465,7 @@ def challenge_phase_split_list(request, challenge_pk):
     challenge_host = is_user_a_host_of_challenge(request.user, challenge_pk)
 
     if not challenge_host:
-        challenge_phase_split = challenge_phase_split.filter(
-            visibility=ChallengePhaseSplit.PUBLIC)
+        challenge_phase_split = challenge_phase_split.filter(visibility=ChallengePhaseSplit.PUBLIC)
 
     serializer = ChallengePhaseSplitSerializer(
         challenge_phase_split, many=True)
@@ -559,8 +554,7 @@ def create_challenge_using_zip_file(request, challenge_host_team_pk):
         return Response(response_data, status=status.HTTP_406_NOT_ACCEPTABLE)
 
     if yaml_file_count > 1:
-        message = 'There are {0} YAML files instead of one in zip folder!'.format(
-            yaml_file_count)
+        message = 'There are {0} YAML files instead of one in zip folder!'.format(yaml_file_count)
         response_data = {
             'error': message
         }
@@ -810,8 +804,7 @@ def create_challenge_using_zip_file(request, challenge_host_team_pk):
                 challenge_title = challenge.title.split(' ')
                 challenge_title = '-'.join(challenge_title).lower()
                 random_challenge_id = uuid.uuid4()
-                challenge_queue_name = "{}-{}".format(
-                    challenge_title, random_challenge_id)
+                challenge_queue_name = "{}-{}".format(challenge_title, random_challenge_id)
                 challenge.queue = challenge_queue_name
                 challenge.save()
             else:
@@ -1350,8 +1343,7 @@ def get_broker_urls(request):
         q_params['end_date__gt'] = timezone.now()
 
     if not request.user.is_superuser:
-        response_data = {
-            'error': 'You are not authorized to make this request!'}
+        response_data = {'error': 'You are not authorized to make this request!'}
         return Response(response_data, status=status.HTTP_403_FORBIDDEN)
     else:
         challenges = Challenge.objects.filter(**q_params)
@@ -1369,16 +1361,13 @@ def get_broker_url_by_challenge_pk(request, challenge_pk):
         Queue name of challenge with challenge pk
     """
     if not request.user.is_superuser:
-        response_data = {
-            'error': 'You are not authorized to make this request!'}
+        response_data = {'error': 'You are not authorized to make this request!'}
         return Response(response_data, status=status.HTTP_403_FORBIDDEN)
     else:
         try:
-            challenge = Challenge.objects.get(
-                pk=challenge_pk, approved_by_admin=True)
+            challenge = Challenge.objects.get(pk=challenge_pk, approved_by_admin=True)
         except Challenge.DoesNotExist:
-            response_data = {
-                'error': 'Challenge {} does not exist'.format(challenge_pk)}
+            response_data = {'error': 'Challenge {} does not exist'.format(challenge_pk)}
             return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
 
         response_data = [challenge.queue]
