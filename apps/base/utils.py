eac2d960bdc9e6d4b497c618f8b8ca1bc189669e
yongzx <zhengxin.yong@minerva.kgi.edu>
2019-02-01 17:37:24 +0530

    Use custom pagination class

--- apps/base/utils.py
+++ apps/base/utils.py
@@ -15,12 +15,12 @@ class StandardResultSetPagination(PageNumberPagination):
     max_page_size = 1000
 
 
-def paginated_queryset(queryset, request, pagination_class=PageNumberPagination()):
+def paginated_queryset(queryset, request, pagination_class=StandardResultSetPagination()):
     '''
         Return a paginated result for a queryset
     '''
     paginator = pagination_class
-    paginator.page_size = settings.REST_FRAMEWORK['PAGE_SIZE']
+    # paginator.page_size = settings.REST_FRAMEWORK['PAGE_SIZE']
     result_page = paginator.paginate_queryset(queryset, request)
     return (paginator, result_page)
 
