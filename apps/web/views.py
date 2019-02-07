c1464d275625cedf1bbdbe362f3f813aed11cbe9
Gaurav Jain <gauravcdj.98@gmail.com>
2019-02-07 13:49:11 +0530

    notifications

--- apps/web/views.py
+++ apps/web/views.py
@@ -18,6 +18,8 @@ from rest_framework.decorators import (api_view,
 from rest_framework.response import Response
 from rest_framework.throttling import AnonRateThrottle
 
+from allauth.account.models import EmailAddress
+
 logger = logging.getLogger(__name__)
 
 
