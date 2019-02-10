09741c1ccf259660c426d70f955d8d935fa68550
Deshraj Yadav <deshraj@gatech.edu>
2019-02-10 10:15:17 -0500

    Challenges: Create docker repository for participant team in docker based challenges(#2085)

--- apps/challenges/utils.py
+++ apps/challenges/utils.py
@@ -1,5 +1,10 @@
 import os
 
+import boto3
+import json
+
+from botocore.exceptions import ClientError
+
 from base.utils import get_model_object
 
 from .models import Challenge, ChallengePhase, Leaderboard, DatasetSplit, ChallengePhaseSplit
@@ -20,3 +25,118 @@ def get_file_content(file_path, mode):
     if os.path.isfile(file_path):
         with open(file_path, mode) as file_content:
             return file_content.read()
+
+
+def convert_to_aws_ecr_compatible_format(string):
+    '''Make string compatible with AWS ECR repository naming
+
+    Arguments:
+        string {string} -- Desired ECR repository name
+
+    Returns:
+        string -- Valid ECR repository name
+    '''
+    return string.replace(" ", "-").lower()
+
+
+def get_or_create_ecr_repository(name, region_name='us-east-1'):
+    '''Get or create AWS ECR Repository
+
+    Arguments:
+        name {string} -- name of ECR repository
+
+    Keyword Arguments:
+        region_name {str} -- AWS region name (default: {'us-east-1'})
+
+    Returns:
+        tuple -- Contains repository dict and boolean field to represent whether ECR repository was created
+        Eg: (
+                {
+                    'repositoryArn': 'arn:aws:ecr:us-east-1:1234567890:repository/some-repository-name',
+                    'registryId': '1234567890',
+                    'repositoryName': 'some-repository-name',
+                    'repositoryUri': '1234567890.dkr.ecr.us-east-1.amazonaws.com/some-repository-name',
+                    'createdAt': datetime.datetime(2019, 2, 6, 9, 12, 5, tzinfo=tzlocal())
+                },
+                False
+            )
+    '''
+    AWS_ACCOUNT_ID = os.environ.get('AWS_ACCOUNT_ID')
+    repository, created = None, False
+    client = boto3.client('ecr', region_name=region_name)
+    try:
+        response = client.describe_repositories(
+            registryId=AWS_ACCOUNT_ID,
+            repositoryNames=[
+                name,
+            ]
+        )
+        repository = response['repositories'][0]
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'RepositoryNotFoundException':
+            response = client.create_repository(repositoryName=name)
+            repository = response['repository']
+            created = True
+    return (repository, created)
+
+
+def create_federated_user(name, repository):
+    '''Create AWS federated user
+
+    Arguments:
+        name {string} -- Name of participant team for which federated user is to be created
+        repository {string} -- Name of the AWS ECR repository to which user should be granted permission
+
+    Returns:
+        dict -- Dict containing user related credentials such as access_key_id, access_secret etc.
+        Eg:
+        {
+            'Credentials': {
+                'AccessKeyId': 'ABCDEFGHIJKLMNOPQRTUVWXYZ',
+                'SecretAccessKey': 'NMgBB75gfVBCDEFGHIJK8g00qVyyzQW+4XjJGQALMNOPQRSTUV',
+                'SessionToken': 'FQoGZX.....',
+                'Expiration': datetime.datetime(2019, 2, 7, 5, 43, 58, tzinfo=tzutc())
+            },
+            'FederatedUser': {
+                'FederatedUserId': '1234567890:test-user',
+                'Arn': 'arn:aws:sts::1234567890:federated-user/test-user'
+            },
+            'PackedPolicySize': 28,
+            'ResponseMetadata': {
+                'RequestId': 'fb47f78b-2a92-11e9-84b9-33527429b818',
+                'HTTPStatusCode': 200,
+                'HTTPHeaders': {
+                'x-amzn-requestid': 'fb47f78b-2a92-11e9-84b9-33527429b818',
+                'content-type': 'text/xml',
+                'content-length': '1245',
+                'date': 'Thu, 07 Feb 2019 04:43:57 GMT'
+                },
+                'RetryAttempts': 0
+            }
+        }
+    '''
+    AWS_ACCOUNT_ID = os.environ.get('AWS_ACCOUNT_ID')
+    policy = {
+        "Version": "2019-02-07",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Action": "ecr:*",
+                "Resource": "arn:aws:ecr:us-east-1:{}:repository/{}".format(AWS_ACCOUNT_ID, repository),
+            },
+            {
+                "Effect": "Allow",
+                "Action": [
+                    "ecr:GetAuthorizationToken"
+                ],
+                "Resource": "*"
+            }
+        ]
+    }
+    client = boto3.client('sts')
+    response = client.get_federation_token(
+        Name=name,
+        Policy=json.dumps(policy),
+        DurationSeconds=3600
+    )
+    return response
