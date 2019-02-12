24b1e48dffa82fb7cbdcb6703409bed0f1e1387b
Kartik Verma <3920286+vkartik97@users.noreply.github.com>
2019-02-12 07:24:20 +0530

    Fix #132: Add copy to clipboard feature in docs(#133)

--- docs/assets/active.js
+++ docs/assets/active.js
@@ -14,3 +14,36 @@ function scroll_to_anchor(anchor_id){
 $("#main-button").click(function() {
    scroll_to_anchor('examples');
 });
+
+$(document).ready(function() {
+    $("body").tooltip({ selector: '[data-toggle=tooltip]' });
+    $.notify.addStyle('notification', {
+        html: "<div><span data-notify-text/></div>",
+        classes: {
+          base: {
+            "white-space": "nowrap",
+            "color": "grey",
+            "background-color": "#d3d3d3",
+            "padding": "5px",
+            "border-radius": "4px",
+            "width": "auto",
+            "font-weight": "600"
+          },
+          
+        }
+      });   
+});
+
+
+
+function CopyToClipboard(elementId) {
+  var aux = document.createElement("input");
+  aux.setAttribute("value", document.getElementById(elementId).innerHTML);
+  document.body.appendChild(aux);
+  aux.select();
+  document.execCommand("copy");
+  document.body.removeChild(aux);
+  $.notify("Command copied to clipboard", {
+      style: 'notification'
+  });
+}
