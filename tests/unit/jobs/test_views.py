7eb5ab418d2d763bc64c99bdb4d13767a973aba8
Kartik Verma <3920286+vkartik97@users.noreply.github.com>
2019-02-19 02:58:25 +0530

    Merge Issue#2090-updatePR

--- tests/unit/jobs/test_views.py
+++ tests/unit/jobs/test_views.py
@@ -904,6 +904,10 @@ class GetRemainingSubmissionTest(BaseAPITestClass):
         setattr(self.challenge_phase, 'max_submissions_per_day', 15)
         setattr(self.challenge_phase, 'max_submissions_per_month', 13)
         self.challenge_phase.save()
+        self.submission3.status = 'cancelled'
+        self.submission2.status = 'failed'
+        self.submission3.save()
+        self.submission2.save()
         expected = {
             'participant_team': self.participant_team.team_name,
             'participant_team_id': self.participant_team.id,
@@ -913,17 +917,15 @@ class GetRemainingSubmissionTest(BaseAPITestClass):
                 'start_date': self.challenge_phase.start_date,
                 'end_date': self.challenge_phase.end_date,
                 'message': {
-                    'remaining_submissions_today_count': 11,
-                    'remaining_submissions_this_month_count': 11,
-                    'remaining_submissions': 98
+                    'remaining_submissions_today_count': 12,
+                    'remaining_submissions_this_month_count': 12,
+                    'remaining_submissions': 99
                 }
             }]
         }
-
         self.challenge.participant_teams.add(self.participant_team)
         self.challenge.save()
         response = self.client.get(self.url, {})
-        print(response.data)
         self.assertEqual(response.data, expected)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
