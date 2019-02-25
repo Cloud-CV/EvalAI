b6073e8a698f42243ea95a49cc3ec892cbe2786c
Sanjeev Singh <sanjeevsinghdevil97@gmail.com>
2019-02-25 13:07:40 +0530

    flake8 errors

--- tests/unit/participants/test_views.py
+++ tests/unit/participants/test_views.py
@@ -276,7 +276,12 @@ class InviteParticipantToTeamTest(BaseAPITestClass):
         full_url = "{}invitation/{}".format(web_url, combined_url)
         subject = "You have been invited to join {} team at CloudCV!".format(
             self.participant_team.team_name)
-        email_template_path = join(settings.BASE_DIR, 'apps', 'participants', 'templates', 'participant_team_email_invite.html')
+        email_template_path = join(
+            settings.BASE_DIR,
+            'apps',
+            'participants',
+            'templates',
+            'participant_team_email_invite.html')
         message = render_to_string(email_template_path, {
             'full_url': full_url,
             'team_name': self.participant_team.team_name
