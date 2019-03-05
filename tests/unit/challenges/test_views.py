573cc84841cc5396bfcebb7f49d19edd68c2dadf
KhalidRmb <khalidriyazgit@gmail.com>
2019-03-05 22:32:37 +0530

    Fix API Tests.(Working)

--- tests/unit/challenges/test_views.py
+++ tests/unit/challenges/test_views.py
@@ -82,6 +82,13 @@ class GetParticipantTeamNameTest(BaseAPITestClass):
     def setUp(self):
         super(GetParticipantTeamNameTest, self).setUp()
 
+        self.participant = Participant.objects.create(
+            user=self.user,
+            status=Participant.ACCEPTED,
+            team=self.participant_team)
+
+        self.challenge.participant_teams.add(self.participant_team)
+
     def test_team_name_for_challenge(self):
         self.url = reverse_lazy('challenges:get_team_name_for_challenge',
                                 kwargs={'challenge_pk': self.challenge.pk})
@@ -89,7 +96,7 @@ class GetParticipantTeamNameTest(BaseAPITestClass):
         expected = {"team_name": "Participant Team for Challenge"}
         response = self.client.get(self.url, {})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['results'], expected)
+        self.assertEqual(response.data, expected)
 
 
 class GetChallengeTest(BaseAPITestClass):
