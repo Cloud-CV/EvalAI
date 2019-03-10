ccf54ccce1bb1bf924e7a3933cec56ec46eddba1
Rishabh Jain <rishabhjain2018@gmail.com>
2019-03-10 19:49:33 -0400

    Add auth token in surge deployment script

--- pr_deploy.sh
+++ pr_deploy.sh
@@ -3,14 +3,11 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     echo "Not a Pull Request. Skipping surge deployment"
     exit 0
 fi
-echo "PR Deploy routine START"
+echo "PR deployment start"
 npm i -g surge
-
-export SURGE_LOGIN=$SURGE_LOGIN
-export SURGE_TOKEN=$SURGE_TOKEN
-
+echo "Installed surge successfully"
 export DEPLOY_DOMAIN=https://pr-${TRAVIS_PULL_REQUEST}-evalai.surge.sh
-echo $SURGE_LOGIN
+echo "Deployment domain -"
 echo $DEPLOY_DOMAIN
 
-surge --project ./dist --domain $DEPLOY_DOMAIN;
+surge --project ./dist --domain $DEPLOY_DOMAIN --token $SURGE_TOKEN
