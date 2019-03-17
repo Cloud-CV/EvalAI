724f39370d82fa3f17882c68ca0d44fdd8df9377
Rishabh Jain <rishabhjain2018@gmail.com>
2019-03-17 04:09:49 -0400

    Fix #2197: Increase default pagination size to 100(#2198)

--- settings/common.py
+++ settings/common.py
@@ -152,7 +152,7 @@ SITE_ID = 1
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': (
         'rest_framework.pagination.LimitOffsetPagination'),
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 100,
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly'
     ],
