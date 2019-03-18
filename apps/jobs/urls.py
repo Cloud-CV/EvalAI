1dde5467e6cfc4c7338f0107757025f5f80d1fed
Kartik Verma <3920286+vkartik97@users.noreply.github.com>
2019-03-19 03:41:22 +0530

    Fix #2090: Add API to get remaining submissions limits for all phases & submissions page for docker based challenges(#2190)

--- apps/jobs/urls.py
+++ apps/jobs/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
         r'challenge_phase/(?P<challenge_phase_id>[0-9]+)/submission/$',
         views.challenge_submission, name='challenge_submission'),
     url(r'^(?P<challenge_pk>[0-9]+)/'
-        r'phases/(?P<challenge_phase_pk>[0-9]+)/'
-        r'remaining_submissions$',
-        views.get_remaining_submissions, name='get_remaining_submissions'),
+        r'remaining_submissions/$',
+        views.get_remaining_submissions,
+        name='get_remaining_submissions'),
     url(r'^challenge_phase_split/(?P<challenge_phase_split_id>[0-9]+)/leaderboard/$',
         views.leaderboard, name='leaderboard'),
     url(r'^submission/(?P<submission_id>[0-9]+)$',
