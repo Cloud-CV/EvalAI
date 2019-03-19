fd9bd9205704f4af80476205d7c9213ac14dd18f
RishabhJain2018 <rishabhjain2018@gmail.com>
2019-03-19 17:16:07 -0400

    Fix installation errors from PyPi

--- setup.py
+++ setup.py
@@ -10,12 +10,30 @@ PROJECT = "evalai"
 with io.open("README.md", encoding="utf-8") as f:
     long_description = f.read()
 
-with open('requirements.txt') as f:
-    install_requires = f.read().strip().split('\n')
+install_requires = [
+    'beautifulsoup4==4.7.1',
+    'beautifultable==0.7.0',
+    'boto==2.49.0',
+    'boto3==1.9.88',
+    'botocore==1.12.116',
+    'click==6.7',
+    'coverage==4.5.1',
+    'coveralls==1.3.0',
+    'docker==3.6.0',
+    'flake8==3.0.4',
+    'lxml==4.2.1',
+    'pytest==3.5.1',
+    'pytest-cov==2.5.1',
+    'python-dateutil==2.7.3',
+    'requests==2.20.0',
+    'requests-toolbelt==0.8.0',
+    'responses==0.9.0',
+    'validators==0.12.2',
+]
 
 setup(
     name=PROJECT,
-    version="1.1.1",
+    version="1.1.4",
     description="Use EvalAI through command line interface",
     long_description=long_description,
     long_description_content_type="text/markdown",
