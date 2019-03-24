2e1e444bad84826679c382ad8304e7ed99864a5e
sudharsana-kjl <kjlsudharsana@gmail.com>
2019-03-24 06:00:48 +0530

    fix build issues

--- apps/jobs/filters.py
+++ apps/jobs/filters.py
@@ -1,8 +1,9 @@
 import django_filters
 from .models import Submission
 
+
 class SubmissionFilter(django_filters.FilterSet):
-    
+  
     class Meta:
         model = Submission
-        fields = ['challenge_phase__challenge', 'status',]
+        fields = ['challenge_phase__challenge', 'status', ]
