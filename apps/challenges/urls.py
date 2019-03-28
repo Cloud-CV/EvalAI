82dfe27173893fcb3c8ed5b3e09dcd42d365b211
Rishabh Jain <rishabhjain2018@gmail.com>
2019-03-27 22:09:36 -0400

    Backend: Add API to get challenge phase by pk(#2247)

--- apps/challenges/urls.py
+++ apps/challenges/urls.py
@@ -141,4 +141,9 @@ urlpatterns = [
         views.get_challenge_phases_by_challenge_pk,
         name="get_challenge_phases_by_challenge_pk",
     ),
+    url(
+        r"^challenge/phase/(?P<pk>[0-9]+)/$",
+        views.get_challenge_phase_by_pk,
+        name="get_challenge_phase_by_pk",
+    ),
 ]
