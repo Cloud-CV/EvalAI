306f22b0a0088e544185643c9af3b9aed1b34b01
RishabhJain2018 <rishabhjain2018@gmail.com>
2019-03-31 04:21:48 -0400

    Add script to deploy on test-pypi.org for testing

---
+++ deploy-staging.sh
@@ -0,0 +1,17 @@
+build_and_push() {
+    python3 -m pip install --upgrade setuptools wheel twine
+    python3 setup.py sdist bdist_wheel
+    python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/* -u ${TEST_PYPI_USERNAME}  -p ${TEST_PYPI_PASSWORD}
+
+}
+
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+    echo "Skipping deploy to https://test-pypi.org; The commit is not on staging branch"
+    exit 0
+elif [ "${TRAVIS_BRANCH}" == "staging"; then
+    build_and_push $TRAVIS_BRANCH
+    exit 0
+else
+    echo "Skipping deploy!"
+    exit 0
+fi
