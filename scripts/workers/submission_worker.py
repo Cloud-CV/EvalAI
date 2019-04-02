e7b0c277320f54bbdf29b6435b7942911d8e905f
Deshraj Yadav <deshraj@gatech.edu>
2019-04-02 02:32:00 -0400

    Update submission_worker.py

--- scripts/workers/submission_worker.py
+++ scripts/workers/submission_worker.py
@@ -664,7 +664,7 @@ def main():
             if not challenge_pk:
                 logger.exception(
                     "Please add CHALLENGE_PK for the challenge to be loaded "
-                    "as the environment variable in the docker.env file."
+                    "in the docker.env file."
                 )
                 sys.exit(1)
             maximum_concurrent_submissions = load_challenge_and_return_max_submissions(
