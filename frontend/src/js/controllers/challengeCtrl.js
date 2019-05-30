d8562087f51f2143668b6f6e20e245c3fec834e5
Sanji515 <sanjeevsinghdevil97@gmail.com>
2019-05-30 05:08:12 +0000

    removed comments

--- frontend/src/js/controllers/challengeCtrl.js
+++ frontend/src/js/controllers/challengeCtrl.js
@@ -1936,49 +1936,4 @@
         
     }
 
-    // angular.module('evalai').directive('slideable', function () {
-    //     return {
-    //         restrict:'C',
-    //         compile: function (element, attr) {
-    //             // wrap tag
-    //             var contents = element.html();
-    //             element.html('<div class="slideable_content" style="margin:0 !important; padding:0 !important" >' + contents + '</div>');
-    
-    //             return function postLink($scope, element, attrs) {
-    //                 // default properties
-    //                 attrs.duration = (!attrs.duration) ? '1s' : attrs.duration;
-    //                 attrs.easing = (!attrs.easing) ? 'ease-in-out' : attrs.easing;
-    //                 element.css({
-    //                     'overflow': 'hidden',
-    //                     'height': '0px',
-    //                     'transitionProperty': 'height',
-    //                     'transitionDuration': attrs.duration,
-    //                     'transitionTimingFunction': attrs.easing
-    //                 });
-    //             };
-    //         }
-    //     };
-    // })
-    // angular.module('evalai').directive('slideToggle', function() {
-    //     return {
-    //         restrict: 'A',
-    //         link: function($scope, element, attrs) {
-    //             var target = document.querySelector(attrs.slideToggle);
-    //             attrs.expanded = false;
-    //             element.bind('click', function() {
-    //                 var content = target.querySelector('.slideable_content');
-    //                 if(!attrs.expanded) {
-    //                     content.style.border = '1px solid rgba(0,0,0,0)';
-    //                     var y = content.clientHeight;
-    //                     content.style.border = 0;
-    //                     target.style.height = y + 'px';
-    //                 } else {
-    //                     target.style.height = '0px';
-    //                 }
-    //                 attrs.expanded = !attrs.expanded;
-    //             });
-    //         }
-    //     }
-    // });
-
 })();
