342c80fa2a85330d6596ce3903646a9b043173d4
nikochiko <kaustubh299792458@gmail.com>
2019-12-07 22:35:55 +0530

    Make requested changes, add new class variables

--- tests/unit/worker/test_submission_worker.py
+++ tests/unit/worker/test_submission_worker.py
@@ -32,12 +32,46 @@ from scripts.workers.submission_worker import (
 
 class BaseAPITestClass(APITestCase):
     def setUp(self):
+
         self.BASE_TEMP_DIR = tempfile.mkdtemp()
+        self.COMPUTE_DIRECTORY_PATH = join(
+            self.BASE_TEMP_DIR, "compute"
+        )
+        self.CHALLENGE_DATA_BASE_DIR = join(
+            self.COMPUTE_DIRECTORY_PATH, "challenge_data"
+        )
+        self.SUBMISSION_DATA_BASE_DIR = join(
+            self.COMPUTE_DIRECTORY_PATH, "submission_files"
+        )
+        self.CHALLENGE_DATA_DIR = join(
+            self.CHALLENGE_DATA_BASE_DIR, "challenge_{challenge_id}"
+        )
+        self.PHASE_DATA_BASE_DIR = join(
+            self.CHALLENGE_DATA_DIR, "phase_data"
+        )
+        self.PHASE_DATA_DIR = join(
+            self.PHASE_DATA_BASE_DIR, "phase_{phase_id}"
+        )
+        self.PHASE_ANNOTATION_FILE_PATH = join(
+            self.PHASE_DATA_DIR, "{annotation_file}"
+        )
+        self.SUBMISSION_DATA_DIR = join(
+            self.SUBMISSION_DATA_BASE_DIR, "submission_{submission_id}"
+        )
+        self.SUBMISSION_INPUT_FILE_PATH = join(
+            self.SUBMISSION_DATA_DIR, "{input_file}"
+        )
+        self.CHALLENGE_IMPORT_STRING = "challenge_data.challenge_{challenge_id}"
+
         self.temp_directory = join(self.BASE_TEMP_DIR, "temp_dir")
+
+        self.testserver = "http://testserver"
         self.url = "/test/url"
+
         self.input_file = open(join(self.BASE_TEMP_DIR, 'dummy_input.txt'), "w+")
         self.input_file.write("file_content")
         self.input_file.close()
+
         self.sqs_client = boto3.client(
             "sqs",
             endpoint_url=os.environ.get("AWS_SQS_ENDPOINT", "http://sqs:9324"),
@@ -45,14 +79,17 @@ class BaseAPITestClass(APITestCase):
             aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
             aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
         )
+
         self.user = User.objects.create(
             username="someuser",
             email="user@test.com",
             password="secret_password",
         )
+
         self.challenge_host_team = ChallengeHostTeam.objects.create(
             team_name="Test Challenge Host Team", created_by=self.user
         )
+
         self.challenge = Challenge.objects.create(
             title="Test Challenge",
             description="Description for test challenge",
@@ -64,16 +101,18 @@ class BaseAPITestClass(APITestCase):
             published=False,
             enable_forum=True,
             anonymous_leaderboard=False,
-            max_concurrent_submission_evaluation=200000,
+            max_concurrent_submission_evaluation=100,
             evaluation_script=SimpleUploadedFile(
                 "test_sample_file.txt",
                 b"Dummy file content",
                 content_type="text/plain",
             ),
         )
+
         self.participant_team = ParticipantTeam.objects.create(
             team_name="Some Participant Team", created_by=self.user
         )
+
         self.challenge_phase = ChallengePhase.objects.create(
             name="Challenge Phase",
             description="Description for Challenge Phase",
@@ -87,11 +126,12 @@ class BaseAPITestClass(APITestCase):
                 b"Dummy file content",
                 content_type="text/plain",
             ),
-            max_submissions_per_day=100000,
-            max_submissions_per_month=100000,
-            max_submissions=100000,
+            max_submissions_per_day=100,
+            max_submissions_per_month=500,
+            max_submissions=1000,
             codename="Phase Code Name",
         )
+
         self.submission = Submission.objects.create(
             participant_team=self.participant_team,
             challenge_phase=self.challenge_phase,
@@ -102,14 +142,13 @@ class BaseAPITestClass(APITestCase):
                 b"Dummy file content",
                 content_type="text/plain",
             ),
-            method_name="Test Method 1",
-            method_description="Test Description 1",
-            project_url="http://testserver1/",
-            publication_url="http://testserver1/",
+            method_name="Test Method",
+            method_description="Test Description",
+            project_url=self.testserver,
+            publication_url=self.testserver,
             is_public=True,
             is_flagged=True,
         )
-        self.testserver = "http://testserver"
 
     def test_create_dir(self):
         create_dir(self.temp_directory)
@@ -125,18 +164,27 @@ class BaseAPITestClass(APITestCase):
         returned_url = return_file_url_per_environment(self.url)
         self.assertEqual(returned_url, "{0}{1}".format(self.testserver, self.url))
 
-    @mock.patch("scripts.workers.submission_worker.SUBMISSION_DATA_DIR", "/tmp/test-dir/compute/submission_files/submission_{submission_id}")
-    @mock.patch("scripts.workers.submission_worker.SUBMISSION_INPUT_FILE_PATH", "/tmp/test-dir/compute/submission_files/submission_{submission_id}/{input_file}")
     @mock.patch("scripts.workers.submission_worker.create_dir_as_python_package")
     @mock.patch("scripts.workers.submission_worker.download_and_extract_file")
-    def test_extract_submission_data_successful(self, mock_download_and_extract_file, mock_create_dir_as_python_package):
+    def test_extract_submission_data_success(self, mock_download_and_extract_file, mock_create_dir_as_python_package):
+        patcher_submission_data_dir = mock.patch("scripts.workers.submission_worker.SUBMISSION_DATA_DIR", self.SUBMISSION_DATA_DIR)
+        patcher_submission_input_file_path = mock.patch("scripts.workers.submission_worker.SUBMISSION_INPUT_FILE_PATH", self.SUBMISSION_INPUT_FILE_PATH)
+        patcher_submission_data_dir.start()
+        patcher_submission_input_file_path.start()
+
         submission = extract_submission_data(self.submission.pk)
-        expected_submission_data_dir = "/tmp/test-dir/compute/submission_files/submission_{submission_id}".format(submission_id=self.submission.pk)
+
+        expected_submission_data_dir = self.SUBMISSION_DATA_DIR.format(submission_id=self.submission.pk)
         mock_create_dir_as_python_package.assert_called_with(expected_submission_data_dir)
+
         expected_submission_input_file = "{0}{1}".format(self.testserver, self.submission.input_file.url)
-        expected_submission_input_file_path = "/tmp/test-dir/compute/submission_files/submission_{submission_id}/{input_file}".format(submission_id=self.submission.pk, input_file=os.path.basename(self.submission.input_file.name))
+        expected_submission_input_file_path = self.SUBMISSION_INPUT_FILE_PATH.format(submission_id=self.submission.pk, input_file=os.path.basename(self.submission.input_file.name))
         mock_download_and_extract_file.assert_called_with(expected_submission_input_file, expected_submission_input_file_path)
+
         self.assertEqual(submission, self.submission)
+
+        patcher_submission_data_dir.stop()
+        patcher_submission_input_file_path.stop()
 
     @mock.patch("scripts.workers.submission_worker.logger.critical")
     def test_extract_submission_data_when_submission_does_not_exist(self, mock_logger):
