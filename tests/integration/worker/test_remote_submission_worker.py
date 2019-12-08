63f919e3eb29696aad64212333c8fac14945c477
takitsuse <takitsuse.nagisa@gmail.com>
2019-12-08 23:52:40 +0900

    Add tests for download_and_extract_file

---
+++ tests/integration/worker/test_remote_submission_worker.py
@@ -0,0 +1,53 @@
+import responses
+import mock
+import os
+import unittest
+
+import scripts.workers.remote_submission_worker as remote_submission_worker
+
+
+class DownloadAndExtractFileBaseTestClass(unittest.TestCase):
+    def setUp(self):
+        self.download_dir = "/tmp/evalai/"
+        self.download_filename = "file"
+        self.download_path = "{}{}".format(self.download_dir, self.download_filename)
+        try:
+            os.makedirs(self.download_dir)
+        except OSError:
+            pass
+
+
+class DownloadAndExtractFileWithProperURL(DownloadAndExtractFileBaseTestClass):
+    def setUp(self):
+        super().setUp()
+
+        self.url = "http://example.com"
+        self.body = """{"content": "test-data"}"""
+        responses.add(
+            responses.GET,
+            self.url,
+            body=self.body,
+            status=200
+        )
+
+    @responses.activate
+    def test_download_and_extract_file_when_proper_url_is_given(self):
+        remote_submission_worker.download_and_extract_file(self.url, self.download_path)
+
+        with open(self.download_path, "r") as f:
+            assert f.read() == self.body
+
+
+class DownloadAndExtractFileTestClass(DownloadAndExtractFileBaseTestClass):
+    def setUp(self):
+        super().setUp()
+
+        self.url = "invalid-url"
+
+    @mock.patch("scripts.workers.remote_submission_worker.logger.error")
+    def test_download_and_extract_file_when_improper_url_is_given(self, mock_logger):
+        remote_submission_worker.download_and_extract_file(self.url, "")
+        mock_logger.assert_called_with(
+            "Failed to fetch file from {}, error "
+            "Invalid URL 'invalid-url': No schema supplied. Perhaps you meant http://invalid-url?".format(self.url)
+        )
