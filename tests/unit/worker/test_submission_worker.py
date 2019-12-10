fe3849f9ce5eaf99339551ad67ffe2497227c618
nikochiko <kaustubh299792458@gmail.com>
2019-12-10 18:38:29 +0530

    Add missing mock directory
    Added `PHASE_ANNOTATION_FILE_PATH` to `mock.patch.multiple`

--- tests/unit/worker/test_submission_worker.py
+++ tests/unit/worker/test_submission_worker.py
@@ -160,6 +160,7 @@ class BaseAPITestClass(APITestCase):
             CHALLENGE_DATA_DIR=self.CHALLENGE_DATA_DIR,
             PHASE_DATA_BASE_DIR=self.PHASE_DATA_BASE_DIR,
             PHASE_DATA_DIR=self.PHASE_DATA_DIR,
+            PHASE_ANNOTATION_FILE_PATH=join(self.PHASE_DATA_DIR, "{annotation_file}")
         )
         mock_phase_annotation_file_name_map = mock.patch.dict(
             "scripts.workers.submission_worker.PHASE_ANNOTATION_FILE_NAME_MAP",
