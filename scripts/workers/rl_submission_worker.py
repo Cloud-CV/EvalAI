4ff176bdce3c2ac554be099d79fd54f51ff98a37
Rishabh Jain <rishabhjain2018@gmail.com>
2019-12-26 17:30:47 -0500

    RL Worker: Minor code and indentation fixes(#2548)

--- scripts/workers/rl_submission_worker.py
+++ scripts/workers/rl_submission_worker.py
@@ -3,9 +3,7 @@ import os
 import signal
 import time
 
-from .worker_util import (
-    EvalAI_Interface
-)
+from worker_util import EvalAI_Interface
 
 from kubernetes import client, config
 
@@ -23,59 +21,57 @@ class GracefulKiller:
 
 logger = logging.getLogger(__name__)
 
-AUTH_TOKEN = os.environ.get("AUTH_TOKEN", "x")
-DJANGO_SERVER = os.environ.get("DJANGO_SERVER", "http://localhost")
-DJANGO_SERVER_PORT = os.environ.get("DJANGO_SERVER_PORT", "8000")
+AUTH_TOKEN = os.environ.get("AUTH_TOKEN", "auth_token")
+EVALAI_API_SERVER = os.environ.get(
+    "EVALAI_API_SERVER", "http://localhost:8000"
+)
 QUEUE_NAME = os.environ.get("QUEUE_NAME", "evalai_submission_queue")
-ENVIRONMENT_IMAGE = os.environ.get("ENVIRONMENT_IMAGE", "x:tag")
+ENVIRONMENT_IMAGE = os.environ.get("ENVIRONMENT_IMAGE", "image_name:tag")
 MESSAGE_FETCH_DEPLAY = int(os.environ.get("MESSAGE_FETCH_DEPLAY", "5"))
 
 
 def create_deployment_object(image, submission, message):
-    PYTHONUNBUFFERED_ENV = client.V1EnvVar(
-        name="PYTHONUNBUFFERED",
-        value="1",
+    PYTHONUNBUFFERED_ENV = client.V1EnvVar(name="PYTHONUNBUFFERED", value="1")
+    AUTH_TOKEN_ENV = client.V1EnvVar(name="AUTH_TOKEN", value=AUTH_TOKEN)
+    EVALAI_API_SERVER_ENV = client.V1EnvVar(
+        name="EVALAI_API_SERVER", value=EVALAI_API_SERVER
     )
-    AUTH_TOKEN_ENV = client.V1EnvVar(
-        name="AUTH_TOKEN",
-        value=AUTH_TOKEN
-    )
-    DJANGO_SERVER_ENV = client.V1EnvVar(
-        name="DJANGO_SERVER",
-        value=DJANGO_SERVER
-    )
-    MESSAGE_BODY_ENV = client.V1EnvVar(
-        name="BODY",
-        value=str(message)
-    )
+    MESSAGE_BODY_ENV = client.V1EnvVar(name="BODY", value=str(message))
     agent_container = client.V1Container(
-        name="agent",
-        image=image,
-        env=[PYTHONUNBUFFERED_ENV]
+        name="agent", image=image, env=[PYTHONUNBUFFERED_ENV]
     )
     environment_container = client.V1Container(
         name="environment",
         image=ENVIRONMENT_IMAGE,
-        env=[PYTHONUNBUFFERED_ENV, AUTH_TOKEN_ENV, DJANGO_SERVER_ENV, MESSAGE_BODY_ENV]
+        env=[
+            PYTHONUNBUFFERED_ENV,
+            AUTH_TOKEN_ENV,
+            EVALAI_API_SERVER_ENV,
+            MESSAGE_BODY_ENV,
+        ],
     )
     template = client.V1PodTemplateSpec(
         metadata=client.V1ObjectMeta(labels={"app": "evaluation"}),
-        spec=client.V1PodSpec(containers=[environment_container, agent_container]))
+        spec=client.V1PodSpec(
+            containers=[environment_container, agent_container]
+        ),
+    )
     spec = client.ExtensionsV1beta1DeploymentSpec(
-        replicas=1,
-        template=template)
+        replicas=1, template=template
+    )
     deployment = client.ExtensionsV1beta1Deployment(
         api_version="extensions/v1beta1",
         kind="Deployment",
         metadata=client.V1ObjectMeta(name="submission-{0}".format(submission)),
-        spec=spec)
+        spec=spec,
+    )
     return deployment
 
 
 def create_deployment(api_instance, deployment):
     api_response = api_instance.create_namespaced_deployment(
-        body=deployment,
-        namespace="default")
+        body=deployment, namespace="default"
+    )
     logger.info("Deployment created. status='%s'" % str(api_response.status))
 
 
@@ -90,9 +86,7 @@ def process_submission_callback(message, api):
     logger.info(submission_data)
     api.update_submission_status(submission_data, message["challenge_pk"])
     dep = create_deployment_object(
-        message["submitted_image_uri"],
-        message["submission_pk"],
-        message
+        message["submitted_image_uri"], message["submission_pk"], message
     )
     create_deployment(extensions_v1beta1, dep)
 
@@ -100,11 +94,14 @@ def process_submission_callback(message, api):
 def main():
     api = EvalAI_Interface(
         AUTH_TOKEN=AUTH_TOKEN,
-        DJANGO_SERVER=DJANGO_SERVER,
-        DJANGO_SERVER_PORT=DJANGO_SERVER_PORT,
+        EVALAI_API_SERVER=EVALAI_API_SERVER,
         QUEUE_NAME=QUEUE_NAME,
     )
-    logger.info("String RL Worker for {}".format(api.get_challenge_by_queue_name()["title"]))
+    logger.info(
+        "String RL Worker for {}".format(
+            api.get_challenge_by_queue_name()["title"]
+        )
+    )
     killer = GracefulKiller()
     while True:
         logger.info(
@@ -128,7 +125,9 @@ def main():
                         "Processing message body: {}".format(message_body)
                     )
                     process_submission_callback(message_body, api)
-                    api.delete_message_from_sqs_queue(message.get("receipt_handle"))
+                    api.delete_message_from_sqs_queue(
+                        message.get("receipt_handle")
+                    )
         time.sleep(MESSAGE_FETCH_DEPLAY)
         if killer.kill_now:
             break
