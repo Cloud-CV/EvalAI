7cf424fd6c4716859d796667915af7fc2c7a8fda
abinav62 <abichari62@gmail.com>
2019-12-29 17:02:01 -0500

    Update permissionCtrl.js

--- frontend/src/js/controllers/permissionCtrl.js
+++ frontend/src/js/controllers/permissionCtrl.js
@@ -11,9 +11,21 @@
 
     function PermCtrl(utilities) {
         var vm = this;
-
+        
         // message for not verified users
         vm.emailError = utilities.getData('emailError');
+        vm.sendMail = false;
+        
+        vm.requestLink = function() {
+            var userKey = utilities.getData('userKey');
+            var parameters = {};
+            
+            parameters.url = 'accounts/user/resend-email';
+            parameters.method = 'POST';
+            parameters.token = userKey;
+            
+            utilities.sendRequest(parameters);
+            
     }
 
 })();
