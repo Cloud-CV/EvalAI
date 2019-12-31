614895d6cccc46d5a88b94fe051a95e575f45102
nikochiko <kaustubh299792458@gmail.com>
2019-12-31 19:32:38 +0530

    Use with blocks for mock.patch

--- tests/unit/rl_worker/test_worker_util.py
+++ tests/unit/rl_worker/test_worker_util.py
@@ -56,8 +56,6 @@ class WorkerUtilTestClass(BaseTestClass):
         self.success_response = {"success": "success_message", "status_code": 200}
         self.example_error_description = "Example description"
 
-        self.patcher = mock.patch(interface.make_request)
-
     def test_return_url_per_environment(self):
         returned_url = interface.return_url_per_environment(self.make_request_url())
         expected = "{0}{1}".format(evalai_api_server, self.make_request_url())
@@ -101,85 +99,76 @@ class WorkerUtilTestClass(BaseTestClass):
         mock_logger.assert_called_with("The worker is not able to establish connection with EvalAI")
 
     def test_get_message_from_sqs_queue(self):
-        mock_make_request = patcher.start()
         url = "{}{}".format(evalai_api_server, self.get_message_from_sqs_queue_url(queue_name))
-        mock_make_request.return_value = self.success_response
-
-        response = interface.get_message_from_sqs_queue()
+        with mock.patch(interface.make_request, return_value=self.success_response) as mock_make_request:
+            response = interface.get_message_from_sqs_queue()
 
         mock_make_request.assert_called_with(url, "GET")
         self.assertEqual(response, self.success_response)
 
     def test_get_submission_by_pk(self):
-        mock_make_request = patcher.start()
         url = "{}{}".format(evalai_api_server, self.get_submission_by_pk_url(self.submission_pk))
-        mock_make_request.return_value = self.success_response
 
-        response = interface.get_submission_by_pk(self.submission_pk)
+        with mock.patch(interface.make_request, return_value=self.success_response) as mock_make_request:
+            response = interface.get_submission_by_pk(self.submission_pk)
 
         mock_make_request.assert_called_with(url, "GET")
         self.assertEqual(response, self.success_response)
 
     def test_get_challenge_phases_by_challenge_pk(self):
-        mock_make_request = patcher.start()
         url = "{}{}".format(evalai_api_server, self.get_challenge_phases_by_challenge_pk_url(self.challenge_pk))
-        mock_make_request.return_value = self.success_response
 
-        response = interface.get_challenge_phases_by_challenge_pk(self.challenge_pk)
+        with mock.patch(interface.make_request, return_value=self.success_response) as mock_make_request:
+            response = interface.get_challenge_phases_by_challenge_pk(self.challenge_pk)
 
         mock_make_request.assert_called_with(url, "GET")
         self.assertEqual(response, self.success_response)
 
     def test_get_challenge_by_queue_name(self):
-        mock_make_request = patcher.start()
         url = "{}{}".format(evalai_api_server, self.get_challenge_by_queue_name_url(queue_name))
-        mock_make_request.return_value = self.success_response
 
-        response = interface.get_challenge_by_queue_name()
+        with mock.patch(interface.make_request, return_value=self.success_response) as mock_make_request:
+            response = interface.get_challenge_by_queue_name()
 
         mock_make_request.assert_called_with(url, "GET")
         self.assertEqual(response, self.success_response)
 
     def test_get_challenge_phase_by_pk(self):
-        mock_make_request = patcher.start()
         url = "{}{}".format(evalai_api_server, self.get_challenge_phase_by_pk_url(self.challenge_phase_pk))
-        mock_make_request.return_value = self.success_response
 
-        response = interface.get_challenge_phase_by_pk(self.challenge_phase_pk)
+        with mock.patch(interface.make_request, return_value=self.success_response) as mock_make_request:
+            response = interface.get_challenge_phase_by_pk(self.challenge_phase_pk)
 
         mock_make_request.assert_called_with(url, "GET")
         self.assertEqual(response, self.success_response)
 
     def test_update_submission_data(self):
-        mock_make_request = patcher.start()
         url = "{}{}".format(evalai_api_server, self.update_submission_data_url(self.challenge_pk))
         data = {"submission_pk": self.submission_pk, "test_field": "new_value"}
-        mock_make_request.return_value = self.success_response
 
-        response = interface.update_submission_data(data, self.challenge_pk)
+        with mock.patch(interface.make_request, return_value=self.success_response) as mock_make_request:
+            response = interface.update_submission_data(data, self.challenge_pk)
 
         mock_make_request.assert_called_with(url, "PUT", data=data)
         self.assertEqual(response, self.success_response)
 
     def test_update_submission_status(self):
-        mock_make_request = patcher.start()
         url = "{}{}".format(evalai_api_server, self.update_submission_status_url(self.challenge_pk))
         data = {"test_field": "new_value"}
-        mock_make_request.return_value = self.success_response
 
-        response = interface.update_submission_status(data, self.challenge_pk)
+        with mock.patch(interface.make_request, return_value=self.success_response) as mock_make_request:
+            response = interface.update_submission_status(data, self.challenge_pk)
 
         mock_make_request.assert_called_with(url, "PATCH", data=data)
         self.assertEqual(response, self.success_response)
 
     def test_delete_message_from_sqs_queue(self):
-        mock_make_request = patcher.start()
         test_receipt_handle = "MbZj6wDWli+JvwwJaBV+3dcjk2YW2vA3+STFFljTM8tJJg6HRG6PYSasuWXPJB+Cw"
         data = {"receipt_handle": test_receipt_handle}
         url = "{}{}".format(evalai_api_server, self.delete_message_from_sqs_queue_url(queue_name))
-        mock_make_request.return_value = self.success_response
 
-        response = interface.delete_message_from_sqs_queue()
+        with mock.patch(interface.make_request, return_value=self.success_response) as mock_make_request:
+            response = interface.delete_message_from_sqs_queue()
 
         mock_make_request.assert_called_with(url, "POST", data=data)
         self.assertTrue("success" in response.keys())
