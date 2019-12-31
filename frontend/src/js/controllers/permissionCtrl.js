add29645bb1f8f191565989df27029440efd8fcb
abinav62 <abichari62@gmail.com>
2019-12-31 00:06:56 -0500

    Update permissionCtrl.js
    Created onSuccess function to set sendMail to true after a successful send

--- frontend/src/js/controllers/permissionCtrl.js
+++ frontend/src/js/controllers/permissionCtrl.js
@@ -1,4 +1,5 @@
 // Invoking IIFE for permission denied
+// Invoking IIFE for permission denied
 (function() {
 
     'use strict';
@@ -11,21 +12,28 @@
 
     function PermCtrl(utilities) {
         var vm = this;
-        
+
         // message for not verified users
         vm.emailError = utilities.getData('emailError');
-        vm.sendMail = false;
-        
-        vm.requestLink = function() {
-            var userKey = utilities.getData('userKey');
-            var parameters = {};
-            
-            parameters.url = 'accounts/user/resend-email';
-            parameters.method = 'POST';
-            parameters.token = userKey;
-            
-            utilities.sendRequest(parameters);
-            
-        };
+	vm.sendMail = false;
+	
+	vm.requestLink = function() {
+	    var userKey = utilities.getData("userKey");
+	    var parameters = {};
+	    
+	    parameters.url = 'accounts/user/resend-email';
+	    parameters.method = 'POST';
+	    parameters.token = userKey;
+	    
+        parameters.callback = {
+            onSuccess: function(response) {
+                vm.sendMail = true;
+		        },
+            onError: function(response) {
+                }
+	    };
+	    utilities.sendRequest(parameters);
+	};
     }
+
 })();
