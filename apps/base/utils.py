45687a584114244cf8455ba2bb85d8a2c8859cd2
jayy <35180217+nsjcorps@users.noreply.github.com>
2020-01-02 12:39:17 +0100

    Fixed errors

--- apps/base/utils.py
+++ apps/base/utils.py
@@ -75,7 +75,7 @@ def encode_data(data):
     """
     encoded = []
     for i in data:
-        encoded.append(base64.encodestring(i.encode("utf-8")).split("=")[0])
+        encoded.append(base64.encodestring(i).split("=")[0])
     return encoded
 
 
