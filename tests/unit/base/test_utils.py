ed3a42aac9d329f2ef14ac560900a101e57bae3d
jayy <35180217+nsjcorps@users.noreply.github.com>
2020-01-02 20:47:55 +0100

    Fixed errors

--- tests/unit/base/test_utils.py
+++ tests/unit/base/test_utils.py
@@ -17,14 +17,14 @@ from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
 
 from base.utils import (
-        RandomFileName,
-        send_slack_notification,
-        encode_data, decode_data,
-        get_url_from_hostname,
-        get_slug,
-        get_queue_name,
-        get_or_create_sqs_queue_object
-    )
+    RandomFileName,
+    send_slack_notification,
+    encode_data, decode_data,
+    get_url_from_hostname,
+    get_slug,
+    get_queue_name,
+    get_or_create_sqs_queue_object
+)
 from challenges.models import Challenge, ChallengePhase
 from hosts.models import ChallengeHostTeam
 from jobs.models import Submission
