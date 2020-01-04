5a24193659073c4c98fcfea4360f058dc09c4c84
nikochiko <kaustubh299792458@gmail.com>
2020-01-05 01:28:24 +0530

    Fix syntax

--- evalai/dramatiq.py
+++ evalai/dramatiq.py
@@ -3,8 +3,10 @@ import dramatiq
 from dramatiq.middleware import AgeLimit, TimeLimit, Callbacks, Pipelines, Prometheus, Retries
 from dramatiq_sqs import SQSBroker
 
+ENDPOINT_URL = os.environ.get('AWS_SQS_ENDPOINT', 'http://127.0.0.1:9324')
+
 broker = SQSBroker(
-    namespace="evalai_submission_queue",
+    namespace="dramatiq_sqs_tests",
     middleware=[
         Prometheus(),
         AgeLimit(),
@@ -12,6 +14,6 @@ broker = SQSBroker(
         Callbacks(),
         Pipelines(),
         Retries(min_backoff=1000, max_backoff=900000, max_retries=96),
-        endpoint_url="http://127.0.0.1:9324",
     ],
+    endpoint_url=ENDPOINT_URL,
 )
