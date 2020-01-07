5608b376047dcce308f7fefe3fde33f0cb9155f6
nikochiko <kaustubh299792458@gmail.com>
2020-01-07 12:27:11 +0530

    Use localhost:9324 when not running in docker

--- evalai/dramatiq_conf.py
+++ evalai/dramatiq_conf.py
@@ -3,6 +3,13 @@ import dramatiq
 from dramatiq.middleware import AgeLimit, TimeLimit, Callbacks, Pipelines, Prometheus, Retries
 from dramatiq_sqs import SQSBroker
 
+# These settings are to be used in developer env only.
+
+if os.environ.get('IN_DOCKER'):
+    endpoint_url = "http://sqs:9324"
+else:
+    endpoint_url = "http://localhost:9324"
+
 broker = SQSBroker(
     namespace="dramatiq_sqs_tests",
     middleware=[
@@ -13,7 +20,7 @@ broker = SQSBroker(
         Pipelines(),
         Retries(min_backoff=1000, max_backoff=900000, max_retries=96),
     ],
-    endpoint_url="http://sqs:9324",
+    endpoint_url=endpoint_url,
     region_name="elasticmq",
     aws_access_key_id="x",
     aws_secret_access_key="x",
