ffe385ff6b44c1269d8b2b309e4a8c34a7c47966
nikochiko <kaustubh299792458@gmail.com>
2020-01-07 11:45:03 +0530

    Change endpoint_url to sqs:9324

--- evalai/dramatiq_conf.py
+++ evalai/dramatiq_conf.py
@@ -13,7 +13,7 @@ broker = SQSBroker(
         Pipelines(),
         Retries(min_backoff=1000, max_backoff=900000, max_retries=96),
     ],
-    endpoint_url="http://0.0.0.0:9324",
+    endpoint_url="http://sqs:9324",
     region_name="elasticmq",
     aws_access_key_id="x",
     aws_secret_access_key="x",
