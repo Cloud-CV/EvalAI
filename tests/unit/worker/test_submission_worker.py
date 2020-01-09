f90afe7362ff8ec16e9870bc1ef1621a742e57ab
Daniel Wang <daniel22373@gmail.com>
2020-01-09 16:31:49 -0500

    Fix test_submission_output_not_saved_when_submission_not_successful ModuleNotFoundError

--- tests/unit/worker/test_submission_worker.py
+++ tests/unit/worker/test_submission_worker.py
@@ -213,14 +213,14 @@ class BaseAPITestClass(APITestCase):
             load_challenge_and_return_max_submissions({"pk": non_existing_challenge_pk})
             mock_logger.assert_called_with("Challenge with pk {} doesn't exist".format(non_existing_challenge_pk))
 
-    @mock.patch("scripts.workers.submission_worker.submission.output")
-    @mock.patch("scripts.workers.submission_worker.challenge_phase_split.dataset_split")
-    def test_submission_output_not_saved_when_submission_not_successful(self, mock_output, mock_dataset_split):
-        original_output = mock_output
-        e = "error description"
-        mock_dataset_split.side_effect = Exception(e);
-        run_submission(self.challenge_id, self.challenge_phase, self.submission, "test_sample_file.txt")
-        self.assertEquals(original_output, mock_output)
+    @mock.patch("scripts.workers.submission_worker.run_submission")
+    @mock.patch("scripts.workers.submission_worker.ChallengePhaseSplit")
+    def test_submission_output_not_saved_when_submission_not_successful(self, mock_run_submission, mock_challenge_phase_split):
+        original_output = self.submission.output
+        mock_challenge_phase_split.return_value = Exception("error message")
+        arbitrary_challenge_id = 1;
+        mock_run_submission(arbitrary_challenge_id, self.challenge_phase, self.submission, "test_sample_file.txt")
+        self.assertEquals(original_output, self.submission.output)
 
     @mock_sqs()
     def test_get_or_create_sqs_queue_for_existing_queue(self):
