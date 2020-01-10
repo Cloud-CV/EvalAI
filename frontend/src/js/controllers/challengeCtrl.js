540fd17fbfe5553824435a446b4c2bac21fff6f4
Suryansh Pathak <suryanshpathak5545@gmail.com>
2020-01-10 17:37:11 +0530

    Fixed And reverted Unneccessary changes

--- frontend/src/js/controllers/challengeCtrl.js
+++ frontend/src/js/controllers/challengeCtrl.js
@@ -344,8 +344,6 @@
                         onError: function(response) {
                             var error = response.data;                            
                             $rootScope.notify("error", error.detail);
-                            // document.getElementById("showonVerified").style.display = 'none';
-                            // document.getElementById("showonUnverified").style.display = 'block';
                             vm.emailVerification = !true;
                             vm.emailError = error.detail;
                             utilities.hideLoader();
