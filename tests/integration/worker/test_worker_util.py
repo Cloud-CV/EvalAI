0e67be2c6ac71a5922c51a61953f47db64f4e742
jayy <35180217+nsjcorps@users.noreply.github.com>
2020-01-11 13:05:36 +0100

    fixed errors

--- tests/integration/worker/test_worker_util.py
+++ tests/integration/worker/test_worker_util.py
@@ -71,7 +71,7 @@ class APICallsTestClass(BaseAPITestClass):
     def test_get_message_from_sqs_queue(self, mock_make_request, mock_url):
         url = URLS.get("get_message_from_sqs_queue").format(QUEUE_NAME)
         self.evalai_interface.get_message_from_sqs_queue()
-        mock_url.assert_called_with(self.test_url)
+        mock_url.assert_called_with(url)
         url = mock_url(url)
         mock_make_request.assert_called_with(url, "GET")
 
@@ -79,49 +79,49 @@ class APICallsTestClass(BaseAPITestClass):
         test_receipt_handle = "MbZj6wDWli+JvwwJaBV+3dcjk2YW2vA3+STFFljTM8tJJg6HRG6PYSasuWXPJB+Cw"
         url = URLS.get("delete_message_from_sqs_queue").format(QUEUE_NAME)
         self.evalai_interface.delete_message_from_sqs_queue(test_receipt_handle)
-        mock_url.assert_called_with(self.url)
-        url = mock_url(self.url)
+        mock_url.assert_called_with(url)
+        url = mock_url(url)
         expected_data = {"receipt_handle": "MbZj6wDWli+JvwwJaBV+3dcjk2YW2vA3+STFFljTM8tJJg6HRG6PYSasuWXPJB+Cw"}
         mock_make_request.assert_called_with(url, "POST", data=expected_data)
 
     def test_get_challenge_by_queue_name(self, mock_make_request, mock_url):
         url = URLS.get("get_challenge_by_queue_name").format(QUEUE_NAME)
         self.evalai_interface.get_challenge_by_queue_name()
-        mock_url.assert_called_with(self.url)
-        url = mock_url(self.url)
+        mock_url.assert_called_with(url)
+        url = mock_url(url)
         mock_make_request.assert_called_with(url, "GET")
 
     def test_get_submission_by_pk(self, mock_make_request, mock_url):
         self.evalai_interface.get_submission_by_pk(self.submission_pk)
         url = URLS.get("get_submission_by_pk").format(self.submission.pk)
-        mock_url.assert_called_with(self.url)
-        url = mock_url(self.url)
+        mock_url.assert_called_with(url)
+        url = mock_url(url)
         mock_make_request.assert_called_with(url, "GET")
 
     def test_get_challenge_phases_by_challenge_pk(self, mock_make_request, mock_url):
         self.evalai_interface.get_challenge_phases_by_challenge_pk(self.challenge_pk)
         url = URLS.get("get_challenge_phases_by_challenge_pk").format(self.challenge.pk)
-        mock_url.assert_called_with(self.url)
-        url = mock_url(self.url)
+        mock_url.assert_called_with(url)
+        url = mock_url(url)
         mock_make_request.assert_called_with(url, "GET")
 
     def test_get_challenge_phase_by_pk(self, mock_make_request, mock_url):
         self.evalai_interface.get_challenge_phase_by_pk(self.challenge_pk, self.challenge_phase_pk)
         url = URLS.get("get_challenge_phase_by_pk").format(self.challenge_pk, self.challenge_phase_pk)
-        mock_url.assert_called_with(self.url)
-        url = mock_url(self.url)
+        mock_url.assert_called_with(url)
+        url = mock_url(url)
         mock_make_request.assert_called_with(url, "GET")
 
     def test_update_submission_data(self, mock_make_request, mock_url):
         self.evalai_interface.update_submission_data(self.data, self.challenge_pk, self.submission_pk)
         url = URLS.get("update_submission_data").format(self.challenge_pk)
-        mock_url.assert_called_with(self.test_url)
-        url = mock_url(self.url)
+        mock_url.assert_called_with(url)
+        url = mock_url(url)
         mock_make_request.assert_called_with(url, "PUT", data=self.data)
 
     def test_update_submission_status(self, mock_make_request, mock_url):
         self.evalai_interface.update_submission_status(self.data, self.challenge_pk)
         url = URLS.get("update_submission_status").format(self.challenge_pk)
-        mock_url.assert_called_with(self.url)
-        url = mock_url(self.url)
+        mock_url.assert_called_with(url)
+        url = mock_url(url)
         mock_make_request.assert_called_with(url, "PATCH", data=self.data)
