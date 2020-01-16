19103a16850735d6bfaff0ec686caf5376dfe9d1
Suryansh pathak <34577232+Suryansh5545@users.noreply.github.com>
2020-01-16 15:26:31 +0530

    Delete test_permissions.py

--- tests/unit/accounts/test_permissions.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-from apps.accounts.permissions import HasVerifiedEmail
-from unittest.mock import patch
-
-
-class PermissionTest(TestCase):
-  def test_is_user_annoymous(self, request):
-    request.user.is_anonymous = True
-    self.assertEqual(HasVerifiedEmail.has_permission(), True)
