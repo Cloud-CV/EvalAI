e7bff8655492637524474552bcea00b6598b096f
Suryansh Pathak <suryanshpathak5545@gmail.com>
2020-01-16 15:30:28 +0530

    Removed test

--- tests/unit/accounts/test_permissions.py
+++ tests/unit/accounts/test_permissions.py
@@ -1,9 +1,8 @@
 from unittest import TestCase
 from apps.accounts.permissions import HasVerifiedEmail
-from unittest.mock import patch
 
 
 class PermissionTest(TestCase):
-  def test_is_user_annoymous(self, request):
-    request.user.is_anonymous = True
+  def test_is_user_annoymous(self):
+    is_anonymous = True
     self.assertEqual(HasVerifiedEmail.has_permission(), True)
