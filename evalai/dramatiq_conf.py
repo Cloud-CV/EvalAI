08e360be462edcca159ec4618e770035fed97235
nikochiko <kaustubh299792458@gmail.com>
2020-01-19 17:44:27 +0530

    Merge #2593

---
+++ evalai/dramatiq_conf.py
@@ -0,0 +1,21 @@
+import os
+
+from dramatiq.middleware import AgeLimit, TimeLimit, Callbacks, Pipelines, Prometheus, Retries
+from dramatiq_sqs import SQSBroker
+
+
+broker = SQSBroker(
+    namespace="dramatiq_sqs_tests",
+    middleware=[
+        Prometheus(),
+        AgeLimit(),
+        TimeLimit(),
+        Callbacks(),
+        Pipelines(),
+        Retries(min_backoff=1000, max_backoff=900000, max_retries=96),
+    ],
+    endpoint_url=os.environ.get("AWS_SQS_ENDPOINT_URL", "http://localhost:9324"),
+    region_name=os.environ.get("AWS_DEFAULT_REGION", "elasticmq"),
+    aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID", "x"),
+    aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY", "x"),
+)
