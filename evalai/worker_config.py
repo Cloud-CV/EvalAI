82b2aa169df99599297b63b4fe9ba2858504542a
nikochiko <kaustubh299792458@gmail.com>
2020-01-21 14:46:45 +0530

    Move imports to top

--- evalai/worker_config.py
+++ evalai/worker_config.py
@@ -1,16 +1,20 @@
 from django.conf import settings
 
 
+if settings.DEBUG:
+    import dramatiq  # noqa
+    from evalai.dramatiq_conf import broker  # noqa
+else:
+    from evalai.celery import app  # noqa
+
+
 def task_wrapper(fn, *args, **kwargs):
     def inner(fn):
         if settings.DEBUG:
-            import dramatiq  # noqa
-            from evalai.dramatiq_conf import broker  # noqa
             dramatiq.set_broker(broker)
             res = dramatiq.actor(fn, *args, **kwargs)
             res.action = res.send
         else:
-            from evalai.celery import app  # noqa
             res = app.task(fn, *args, **kwargs)
             res.action = res.delay
 
