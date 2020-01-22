13086b0e988942e6f478b7bfb0e6782aca4a9072
nikochiko <kaustubh299792458@gmail.com>
2020-01-22 17:05:00 +0530

    Add tests for dramatiq worker

---
+++ tests/integration/dramatiq/test_dramatiq.py
@@ -0,0 +1,46 @@
+import time
+from unittest import TestCase
+
+import dramatiq
+
+from evalai.dramatiq_conf import broker
+
+
+class TestDramatiqWorker(TestCase):
+
+    def setup(self):
+        self.db = []  # Dummy database to perform operations
+        self.n_messages = 3
+        self.dummy_data = {"test_field": "test_data"}
+        self.queue_name = "dramatiq_sqs_tests"
+        self.dummy_task = dramatiq.actor(self.dummy_method, queue_name=self.queue_name)
+
+        dramatiq.set_broker(broker)
+
+    def dummy_method(self, data):
+        self.db.append(data)
+
+    def test_enqueue_and_process_message(self):
+        message = self.dummy_task.send(self.dummy_data)
+
+        # wait for task to complete
+        time.sleep(1)
+
+        # verify that message was sent to correct queue
+        self.assertEqual(message.queue_name, self.queue_name)
+
+        # verify task success
+        self.assertEqual(self.db, [self.dummy_data])
+
+    def test_enqueue_and_process_multiple_messages(self):
+        data_list = [i for i in range(self.n_messages)]
+
+        messages = [self.dummy_task.send(data) for data in data_list]
+        time.sleep(2)  # wait for task to finish
+
+        # verify all messages are sent to correct queue
+        for message in messages:
+            self.assertEqual(message.queue_name, self.queue_name)
+
+        # verify task completion
+        self.assertEqual(self.db, data_list)
