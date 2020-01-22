de706e84ab2fbf991e3285e532d5020ed8db0f3c
nikochiko <kaustubh299792458@gmail.com>
2020-01-22 20:08:18 +0530

    Fix tests

--- tests/integration/dramatiq/test_dramatiq.py
+++ tests/integration/dramatiq/test_dramatiq.py
@@ -16,8 +16,8 @@ class TestDramatiqWorker(TestCase):
         self.db = []  # Dummy database to perform operations
         self.n_messages = 3
         self.dummy_data = {"test_field": "test_data"}
-        self.queue_name = "dramatiq_test_queue"
-        self.dummy_task = dramatiq.actor(self.dummy_method)
+        self.queue_name = "default"
+        self.dummy_task = dramatiq.actor(self.dummy_method, queue_name=self.queue_name)
 
     def dummy_method(self, data):
         self.db.append(data)
