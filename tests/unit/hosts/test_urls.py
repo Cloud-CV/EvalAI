8bdeade198c4c51f194bc37328fb3fd9e7592b21
kajol <kajolkumarisingh222@gmail.com>
2020-02-13 02:08:00 +0530

    fixed_bug_host_deletion

--- tests/unit/hosts/test_urls.py
+++ tests/unit/hosts/test_urls.py
@@ -14,6 +14,12 @@ class BaseAPITestClass(APITestCase):
             password="secret_password",
         )
 
+        self.invite_host_user = User.objects.create(
+            username="otheruser",
+            email="other@platform.com",
+            password="other_secret_password",
+        )
+
         self.challenge_host_team = ChallengeHostTeam.objects.create(
             team_name="Test Challenge Host Team", created_by=self.user
         )
@@ -68,7 +74,7 @@ class TestStringMethods(BaseAPITestClass):
             "/api/hosts/challenge_host_team/"
             + str(self.challenge_host_team.pk)
             + "/challenge_host/"
-            + str(self.challenge_host.pk),
+            + str(self.invite_host_user.pk),
         )
 
         url = reverse_lazy(
