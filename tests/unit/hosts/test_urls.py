807819ea9a25713f9adf418890cefaee0e785429
kajol <kajolkumarisingh222@gmail.com>
2020-02-13 16:13:44 +0530

    Improving_code_readability

--- tests/unit/hosts/test_urls.py
+++ tests/unit/hosts/test_urls.py
@@ -57,7 +57,7 @@ class TestStringMethods(BaseAPITestClass):
         )
 
         url = reverse_lazy(
-            "hosts:get_challenge_host_deletes",
+            "hosts:get_challenge_host_delete",
             kwargs={
                 "challenge_host_team_pk": self.challenge_host_team.pk,
                 "challenge_host_pk": self.challenge_host.pk,
