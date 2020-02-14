018f68e68dc4aaaf814f61e61f319b8d36105fde
RishabhJain2018 <rishabhjain2018@gmail.com>
2020-02-13 19:23:11 -0500

    Add API docs for SQS APIs

--- apps/jobs/views.py
+++ apps/jobs/views.py
@@ -462,7 +462,9 @@ def change_submission_data_and_visibility(
 @api_view(["GET"])
 @throttle_classes([AnonRateThrottle])
 def leaderboard(request, challenge_phase_split_id):
-    """Returns leaderboard for a corresponding Challenge Phase Split"""
+    """
+    Returns leaderboard for a corresponding Challenge Phase Split
+    """
 
     # check if the challenge exists or not
     try:
@@ -1128,15 +1130,72 @@ def get_submissions_for_challenge(request, challenge_pk):
     return Response(serializer.data, status=status.HTTP_200_OK)
 
 
+@swagger_auto_schema(
+    methods=["get"],
+    manual_parameters=[
+        openapi.Parameter(
+            name="queue_name",
+            in_=openapi.IN_PATH,
+            type=openapi.TYPE_STRING,
+            description="Queue Name",
+            required=True,
+        )
+    ],
+    operation_id="get_submission_message_from_queue",
+    responses={
+        status.HTTP_200_OK: openapi.Response(
+            description="",
+            schema=openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    "body": openapi.Schema(
+                        type=openapi.TYPE_OBJECT,
+                        description="SQS message queue dict object",
+                        properties={
+                            "challenge_pk": openapi.Schema(
+                                type=openapi.TYPE_INTEGER,
+                                description="Primary key for the challenge in the database",
+                            ),
+                            "phase_pk": openapi.Schema(
+                                type=openapi.TYPE_INTEGER,
+                                description="Primary key for the challenge phase in the database",
+                            ),
+                            "submission_pk": openapi.Schema(
+                                type=openapi.TYPE_INTEGER,
+                                description="Primary key for the submission in the database",
+                            ),
+                            "submitted_image_uri": openapi.Schema(
+                                type=openapi.TYPE_STRING,
+                                description="The AWS ECR URL for the pushed docker image in docker based challenges",
+                            ),
+                        },
+                    ),
+                    "receipt_handle": openapi.Schema(
+                        type=openapi.TYPE_STRING,
+                        description="SQS message receipt handle",
+                    ),
+                },
+            ),
+        ),
+        status.HTTP_400_BAD_REQUEST: openapi.Response(
+            "{'error': 'Error message goes here'}"
+        ),
+    },
+)
 @api_view(["GET"])
 @throttle_classes([UserRateThrottle])
 @permission_classes((permissions.IsAuthenticated, HasVerifiedEmail))
 @authentication_classes((ExpiringTokenAuthentication,))
 def get_submission_message_from_queue(request, queue_name):
     """
-    API to fetch submission message from AWS SQS queue
-    Arguments:
-        queue_name  -- The unique authentication token provided by challenge hosts
+    API to fetch submission message from AWS SQS queue.
+
+    Path Parameters:
+     - ``queue_name``: AWS SQS queue name
+
+    Response Schema:
+     - ``body``: The message body content
+     - ``receipt_handle``: The message receipt handle
     """
     try:
         challenge = Challenge.objects.get(queue=queue_name)  # noqa
@@ -1176,11 +1235,41 @@ def get_submission_message_from_queue(request, queue_name):
         }
         return Response(response_data, status=status.HTTP_200_OK)
     except botocore.exceptions.ClientError as ex:
-        response_data = ex
+        response_data = {"error": ex}
         logger.exception("Exception raised: {}".format(ex))
         return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
 
 
+@swagger_auto_schema(
+    methods=["post"],
+    manual_parameters=[
+        openapi.Parameter(
+            name="queue_name",
+            in_=openapi.IN_PATH,
+            type=openapi.TYPE_STRING,
+            description="Queue Name",
+            required=True,
+        )
+    ],
+    operation_id="delete_submission_message_from_queue",
+    request_body=openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        properties={
+            "receipt_handle": openapi.Schema(
+                type=openapi.TYPE_STRING,
+                description="Receipt handle for the message to be deleted",
+            )
+        },
+    ),
+    responses={
+        status.HTTP_200_OK: openapi.Response(
+            "{'success': 'Message deleted successfully from the queue <queue-name>'}"
+        ),
+        status.HTTP_400_BAD_REQUEST: openapi.Response(
+            "{'error': 'Error message goes here'}"
+        ),
+    },
+)
 @api_view(["POST"])
 @throttle_classes([UserRateThrottle])
 @permission_classes((permissions.IsAuthenticated, HasVerifiedEmail))
@@ -1188,9 +1277,12 @@ def get_submission_message_from_queue(request, queue_name):
 def delete_submission_message_from_queue(request, queue_name):
     """
     API to delete submission message from AWS SQS queue
-    Arguments:
-        queue_name  -- The unique authentication token provided by challenge hosts
-        receipt_handle -- The receipt handle of the message to be deleted
+
+    - Arguments:
+        ``queue_name``  -- The unique authentication token provided by challenge hosts
+
+    - Request Body:
+        ``receipt_handle`` -- The receipt handle of the message to be deleted
     """
     try:
         challenge = Challenge.objects.get(queue=queue_name)
@@ -1221,7 +1313,7 @@ def delete_submission_message_from_queue(request, queue_name):
         }
         return Response(response_data, status=status.HTTP_200_OK)
     except botocore.exceptions.ClientError as ex:
-        response_data = ex
+        response_data = {"error": ex}
         logger.exception(
             "SQS message is not deleted due to {}".format(response_data)
         )
