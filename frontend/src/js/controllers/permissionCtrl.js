910c8ef0fe522b94ce74b4cc8fc96d2c6c9beec6
Rishabh Jain <rishabhjain2018@gmail.com>
2020-05-17 01:15:45 -0400

    Frontend: Fix indentation and add missing semicolons in permissions controller(#2794)
    * Fix indentation and add mising semicolons
    
    * Update permissionCtrl.js

--- frontend/src/js/controllers/permissionCtrl.js
+++ frontend/src/js/controllers/permissionCtrl.js
@@ -15,25 +15,28 @@
         // message for not verified users
         vm.emailError = utilities.getData('emailError');
 
-         vm.sendMail = false;
+        vm.sendMail = false;
         // Function to request a new verification email.
-        vm.requestLink = function () {
+        vm.requestLink = function() {
             var userKey = utilities.getData('userKey');
             var parameters = {};
             parameters.url = 'accounts/user/resend_email_verification/';
             parameters.method = 'POST';
             parameters.token = userKey;
             parameters.callback = {
-                onSuccess: function(response) {
+                onSuccess: function() {
                     vm.sendMail = true;
                     $rootScope.notify("success", "The verification link was sent again.");
                 },
                 onError: function(response) {
-                    var message = response.data.detail
-                    var time = Math.floor(message.match(/\d+/g)[0] / 60)
-                    if (response.status==429){$rootScope.notify("error", "Request limit exceeded. Please wait for " + time + " minutes and try again.");}
-                    else {$rootScope.notify("error", "Something went wrong. Please try again.");}
+                    var message = response.data.detail;
+                    var time = Math.floor(message.match(/\d+/g)[0] / 60);
+                    if (response.status == 429) {
+                        $rootScope.notify("error", "Request limit exceeded. Please wait for " + time + " minutes and try again.");
+                    } else {
+                        $rootScope.notify("error", "Something went wrong. Please try again.");
                     }
+                }
             };
             utilities.sendRequest(parameters);
         };
