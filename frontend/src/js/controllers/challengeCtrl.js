fad593ced4a15087b4ad1e0c2381457470eb2464
Ram Ramrakhya <ramramrakhya81@gmail.com>
2020-05-25 20:45:50 +0530

    Frontend: Add UI card to show hosts that the challenge is pending for approval(#2820)
    Co-authored-by: Rishabh Jain <rishabhjain2018@gmail.com>

--- frontend/src/js/controllers/challengeCtrl.js
+++ frontend/src/js/controllers/challengeCtrl.js
@@ -44,13 +44,14 @@
         vm.stopFetchingSubmissions = function() {};
         vm.currentDate = null;
         vm.isPublished = false;
+        vm.approved_by_admin = false;
         vm.sortColumn = 'rank';
         vm.reverseSort = false;
         vm.columnIndexSort = 0;
         vm.disableSubmit = true;
         // save initial ranking
         vm.initial_ranking = {};
-      // loader for existing teams
+        // loader for existing teams
         vm.isExistLoader = false;
         vm.loaderTitle = '';
         vm.loaderContainer = angular.element('.exist-team-card');
@@ -178,6 +179,7 @@
                 vm.forumURL = details.forum_url;
                 vm.cliVersion = details.cli_version;
                 vm.isRegistrationOpen = details.is_registration_open;
+                vm.approved_by_admin = details.approved_by_admin;
 
                 if (vm.page.image === null) {
                     vm.page.image = "dist/images/logo.png";
