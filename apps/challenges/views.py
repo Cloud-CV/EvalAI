7f703449662948dfafa1285c301cb530e49bb66f
kajol <kajolkumarisingh222@gmail.com>
2020-06-01 12:39:45 +0530

    fix flake8 error

--- apps/challenges/views.py
+++ apps/challenges/views.py
@@ -237,15 +237,13 @@ def team_name_for_challenge(request, challenge_pk):
             participant_team = ParticipantTeam.objects.get(pk=participant_team_pk)
         except ParticipantTeam.DoesNotExist:
             response_data = {"error": "ParticipantTeam does not exist"}
-            return Response(response_data, status=status.HTTP_406_NOT_ACCEPTABLE)    
+            return Response(response_data, status=status.HTTP_406_NOT_ACCEPTABLE)
         serializer = ParticipantTeamDetailSerializer(participant_team)
         details = serializer.data
         response_data = {"team_name": details['team_name']}
         return Response(response_data, status=status.HTTP_200_OK)
     else:
-        message = (
-                "You are not a participant!"
-        )
+        message = ("You are not a participant!")
         response_data = {"error": message}
         return Response(response_data, status=status.HTTP_406_NOT_ACCEPTABLE)
 
