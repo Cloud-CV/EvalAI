556218512f2fb581b9172a2f98a88485171d2f99
KhalidRmb <khalidriyazgit@gmail.com>
2020-06-28 16:20:32 +0530

    Proper import statement

--- apps/challenges/challenge_notification_util.py
+++ apps/challenges/challenge_notification_util.py
@@ -3,7 +3,7 @@ import logging
 from django.conf import settings
 
 from base.utils import send_email
-from challenges.aws_utils import start_workers
+from .aws_utils import start_workers
 
 logger = logging.getLogger(__name__)
 
