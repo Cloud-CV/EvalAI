2971ea2ceb3bfb7119cf66b02e59f96938526097
Yash Dusing <yashdusing145@gmail.com>
2020-07-05 20:19:45 +0530

    Change to example

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-domains=(yashdusing.me.uk evalai.yashdusing.me.uk evalapi.yashdusing.me.uk)
+domains=(example.com evalai.example.com evalapi.example.com)
 rsa_key_size=4096
 data_path="./certbot"
-email="yashdusing145@gmail.com" # Adding a valid address is strongly recommended
+email="user@email.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
