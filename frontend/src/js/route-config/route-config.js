5d256b6d0fc520144d4186d00b64199d840f1609
kajol <44888949+Kajol-Kumari@users.noreply.github.com>
2020-07-18 23:11:32 +0530

    Frontend: Redirecting user to dashboard on signup with limited privileges to view challenges(#2704)
    * direct_to_dashboard_on_signup_with_limited_priviledges
    
    * tests_changed_accordingly
    
    * redirecting_to_home_page_instead_of_login_page_on_logging_out_feature_added
    
    * added_proper_comment
    
    * adding_commit_for_fixing_runing_build
    
    Co-authored-by: Rishabh Jain <rishabhjain2018@gmail.com>

--- frontend/src/js/route-config/route-config.js
+++ frontend/src/js/route-config/route-config.js
@@ -617,7 +617,7 @@
                 onSuccess: function() {
                     utilities.resetStorage();
                     $rootScope.isLoader = false;
-                    $state.go("auth.login");
+                    $state.go("home");
                     $rootScope.isAuth = false;
                     $rootScope.notify("info", "Successfully logged out!");
                 },
