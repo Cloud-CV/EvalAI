218f4e550eb06e764d0ac676e0035ef5c42cda84
kajol <kajolkumarisingh222@gmail.com>
2020-07-19 15:54:32 +0530

    Modified test

--- tests/unit/hosts/test_views.py
+++ tests/unit/hosts/test_views.py
@@ -375,10 +375,10 @@ class GetParticularChallengeHost(BaseAPITestClass):
                 "pk": self.challenge_host.pk,
             },
         )
-        expected = {"error": "ChallengeHostTeam does not exist"}
+        expected = {"detail": "ChallengeHost " + str(self.inavlid_challenge_pk) + " does not exist"}
         response = self.client.get(self.url, {})
-        self.assertEqual(response.data, expected)
-        self.assertEqual(response.status_code, status.HTTP_406_NOT_ACCEPTABLE)
+        self.assertDictEqual(response.data, expected)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
 
 class UpdateParticularChallengeHost(BaseAPITestClass):
