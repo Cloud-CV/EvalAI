cf35223b00f91e8233acc9c76c14639b66bb6fa8
Yash Dusing <yashdusing145@gmail.com>
2020-07-25 13:54:07 +0530

    Add env file

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-domains=(example.com evalai.example.com evalapi.example.com)
+read -p "Enter your domain name (example.com) : " DOMAIN_NAME
+domains=($DOMAIN_NAME evalai.$DOMAIN_NAME evalapi.$DOMAIN_NAME)
+
+touch .env
+echo "DOMAIN_NAME=$DOMAIN_NAME" > .env
+
 rsa_key_size=4096
 data_path="./certbot"
-email="user@email.com" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+
+# Adding a valid address is strongly recommended
+read -p "Enter your Email ID : " email
+
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
