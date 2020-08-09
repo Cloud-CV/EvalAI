df75991d7b2ff0643d3123051b7547705f14be59
Ram Ramrakhya <ramramrakhya81@gmail.com>
2020-08-09 21:31:54 +0530

    Backend: Add API to fetch submissions count by status for a challenge phase(#2932)
    * Add API to fetch submissions status count for challenge phase
    
    * Update docstring
    
    * Add challenge host check to count api
    
    * Modify URL
    
    * Modify url
    
    * rename response
    
    Co-authored-by: Rishabh Jain <rishabhjain2018@gmail.com>

--- apps/jobs/views.py
+++ apps/jobs/views.py
@@ -13,6 +13,7 @@ from rest_framework.decorators import (
 
 from django.core.files.base import ContentFile
 from django.db import transaction, IntegrityError
+from django.db.models import Count
 from django.utils import timezone
 
 from rest_framework_expiring_authtoken.authentication import (
@@ -2073,3 +2074,39 @@ def get_github_badge_data(
         else:
             data["message"] = f"{challenge_obj.title}"
     return Response(data, status=http_status_code)
+
+
+@api_view(["GET"])
+@throttle_classes([UserRateThrottle])
+@permission_classes((permissions.IsAuthenticated, HasVerifiedEmail))
+@authentication_classes((ExpiringTokenAuthentication,))
+def challenge_phase_submission_count_by_status(request, challenge_phase_pk):
+    """
+        API for fetching count of submissions by status for a challenge phase
+
+        Arguments:
+            request {HttpRequest} -- request object
+            challenge_phase_pk {int} -- challenge phase pk
+
+        Returns:
+            Response object -- Response object with appropriate response code (200/400/404)
+    """
+    # check if the challenge phase exists or not
+    challenge_phase = get_challenge_phase_model(challenge_phase_pk)
+
+    challenge = challenge_phase.challenge
+
+    if not is_user_a_host_of_challenge(request.user, challenge.pk):
+        response_data = {
+            "error": "Sorry, you are not authorized to make this request"
+        }
+        return Response(response_data, status=status.HTTP_401_UNAUTHORIZED)
+
+    submissions = Submission.objects.filter(
+        challenge_phase=challenge_phase,
+    ).values('status').annotate(count=Count("id"))
+
+    response_data = {
+        "status": submissions
+    }
+    return Response(response_data, status=status.HTTP_200_OK)
