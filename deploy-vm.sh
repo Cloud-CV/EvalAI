3f8ba288d2efe48ee748465b7d96a9a42add538c
Yash Dusing <yashdusing145@gmail.com>
2020-08-24 19:38:45 +0530

    Add $

--- deploy-vm.sh
+++ deploy-vm.sh
@@ -47,7 +47,7 @@ touch .env
 
 read -p "Enter domain name (evalai.example.com) : " DOMAIN_NAME
 
-FRONTEND_DOMAIN=DOMAIN_NAME
+FRONTEND_DOMAIN=$DOMAIN_NAME
 echo "FRONTEND_DOMAIN=$FRONTEND_DOMAIN" > .env
 
 BACKEND_DOMAIN="${DOMAIN_NAME}/api"
