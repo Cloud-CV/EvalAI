3fee2108b013461b3de8aa354473ba6eaba6539b
Rishabh Jain <rishabhjain2018@gmail.com>
2020-09-08 11:14:20 -0400

    Update evalai-cli version from 1.3.1 to 1.3.2 (#279)

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ tests_require = [
 setup(
     name=PROJECT,
     cmdclass={"test": PyTest},
-    version="1.3.1",
+    version="1.3.2",
     description="Use EvalAI through command line interface",
     long_description=long_description,
     long_description_content_type="text/markdown",
