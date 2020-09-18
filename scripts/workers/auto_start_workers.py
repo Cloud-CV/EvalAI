0b8245c7e4b433ec7cbf2e92a5f6c249a3409780
Ram81 <ramramrakhya81@gmail.com>
2020-09-18 20:03:57 +0530

    Add stop worker methods

--- scripts/workers/auto_start_workers.py
+++ scripts/workers/auto_start_workers.py
@@ -13,8 +13,13 @@ authorization_header = {
 
 def start_worker(challenge_id):
 	start_worker_endpoint = "{}/api/challenges/{}/manage_worker/start".format(evalai_endpoint, challenge_id)
-	print(start_worker_endpoint)
 	response = requests.post(start_worker_endpoint, headers=authorization_header)
+	return response
+
+
+def stop_worker(challenge_id):
+	stop_worker_endpoint = "{}/api/challenges/{}/manage_worker/stop".format(evalai_endpoint, challenge_id)
+	response = requests.post(stop_worker_endpoint, headers=authorization_header)
 	return response
 
 
@@ -25,23 +30,29 @@ def get_all_challenges():
 	return response.json()
 
 
-def start_workers_for_active_challenges():
+def start_or_stop_workers_for_active_challenges():
 	response = get_all_challenges()
 
 	for challenge in response["results"]:
 		challenge_id = challenge["id"]
 		workers = challenge["workers"]
-		challenge_start_date = parser.parse(challenge["start_date"])
-		current_date = utc.localize(datetime.now())
 		is_docker_based = challenge["is_docker_based"]
+		challenge_start_date = parser.parse(challenge["start_date"])
+		challenge_end_date = parser.parse(challenge["end_date"])
+		current_date = utc.localize(datetime.now())
 
 		print('Start submission worker for challenge id {}'.format(challenge_id))
 		if not is_docker_based:
 			if workers is None and challenge_start_date <= current_date:
 				response = start_worker(challenge_id)
 				if not response.ok:
-					print('ERROR: Start worker for challenge id {} failed!'.format(challenge_id))
+					print('ERROR: Start worker failed for challenge id {}!'.format(challenge_id))
+			if workers is not None and challenge_end_date <= current_date
+				response = stop_worker(challenge_id)
+				if not response.ok:
+					print('ERROR: Stop worker failed for challenge id {}!'.format(challenge_id))
+
 
 if __name__ == '__main__':
-	start_workers_for_active_challenges()
+	start_or_stop_workers_for_active_challenges()
 	print('Quitting worker auto start script!')
