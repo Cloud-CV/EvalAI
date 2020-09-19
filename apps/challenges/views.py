e84a82721c6df5d0f0d2bf5f971c2cc7714b0c1c
Ram Ramrakhya <ramramrakhya81@gmail.com>
2020-09-19 21:28:14 +0530

    Backend: Add inform hosts field to enable/disable email notification for hosts(#3007)

--- apps/challenges/views.py
+++ apps/challenges/views.py
@@ -1390,7 +1390,7 @@ def create_challenge_using_zip_file(request, challenge_host_team_pk):
                 send_slack_notification(message=message)
 
             template_data = get_challenge_template_data(zip_config.challenge)
-            if not challenge.is_docker_based:
+            if not challenge.is_docker_based and challenge.inform_hosts:
                 try:
                     response = start_workers([zip_config.challenge])
                     count, failures = response["count"], response["failures"]
