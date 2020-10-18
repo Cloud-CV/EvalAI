b658c6328553b7d226a9a04e8ff29027754651d1
Deshraj Yadav <deshrajdry@gmail.com>
2020-10-18 13:32:55 -0700

    [Docker] Migrate domain to eval.ai

--- scripts/deployment/push.sh
+++ scripts/deployment/push.sh
@@ -7,7 +7,7 @@ build_and_push() {
         aws configure set default.region us-east-1
         eval $(aws ecr get-login --no-include-email)
         echo "Pulling ssl certificates and nginx configuration..."
-        aws s3 cp s3://cloudcv-secrets/evalai/${TRAVIS_BRANCH}/ssl/ ./ssl/ --recursive
+        aws s3 cp s3://cloudcv-secrets/eval.ai/ssl/ ./ssl/ --recursive
         echo "Pulled ssl certificates and nginx configuration successfully"
         docker-compose -f docker-compose-$1.yml build \
             --build-arg COMMIT_ID=${COMMIT_ID} \
