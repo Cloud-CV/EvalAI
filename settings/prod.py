ca3336dd513f65c10f0242381944dffa05778df9
Rishabh Jain <rishabhjain2018@gmail.com>
2020-11-10 00:05:47 -0500

    Settings: Add staging and prod domains to CORS(#3073)

--- settings/prod.py
+++ settings/prod.py
@@ -12,7 +12,11 @@ ALLOWED_HOSTS = ["eval.ai"]
 
 CORS_ORIGIN_ALLOW_ALL = False
 
-CORS_ORIGIN_WHITELIST = ("evalai.s3.amazonaws.com", "beta.eval.ai:9999")
+CORS_ORIGIN_WHITELIST = (
+    "evalai.s3.amazonaws.com",
+    "eval.ai",
+    "beta.eval.ai:9999",
+)
 
 DATABASES = {
     "default": {
