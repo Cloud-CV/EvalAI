02e239c590ad4a5825746db7306676c190ba05e7
Ram Ramrakhya <ramramrakhya81@gmail.com>
2020-12-13 02:13:13 +0530

    Monitoring Script: Fixes for docker client and add env variable(#3094)
    * BugFix: Container monitoring script
    
    * Flake8 fix
    
    Co-authored-by: Rishabh Jain <rishabhjain2018@gmail.com>

--- scripts/monitoring/run_monitor_containers.sh
+++ scripts/monitoring/run_monitor_containers.sh
@@ -4,4 +4,8 @@ if [ ! -z "$1" ]
   then
     path=$1
 fi
+
+# crontab doesn't have access to env variable, define explicitly
+export MONITORING_SLACK_WEBHOOK_URL=x;
+
 python ${path}/scripts/monitoring/monitor_containers.py
