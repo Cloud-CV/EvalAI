0de98e51d4143d2dad7df871872252a4e3c485ef
Ram Ramrakhya <ramramrakhya81@gmail.com>
2020-12-22 21:58:27 +0530

    Monitoring: Pass slack webhook url as a parameter to the script(#3106)

--- scripts/monitoring/run_monitor_containers.sh
+++ scripts/monitoring/run_monitor_containers.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 path=$PWD
+webhook_url=''
 if [ ! -z "$1" ]
   then
     path=$1
 fi
 
+if [ ! -z "$2" ]
+  then
+    webhook_url=$2
+fi
+
 # crontab doesn't have access to env variable, define explicitly
-export MONITORING_SLACK_WEBHOOK_URL=x;
+export MONITORING_SLACK_WEBHOOK_URL=${webhook_url};
 
 python ${path}/scripts/monitoring/monitor_containers.py
