b30500498c4d0cbcc05f373b2c21f011a82306f5
Ram Ramrakhya <ramramrakhya81@gmail.com>
2021-01-05 23:58:55 +0530

    Monitoring Script: Add env variable for staging and production(#3125)

--- scripts/monitoring/run_monitor_containers.sh
+++ scripts/monitoring/run_monitor_containers.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 path=$PWD
 webhook_url=''
+env='staging'
 if [ ! -z "$1" ]
   then
     path=$1
@@ -11,7 +12,13 @@ if [ ! -z "$2" ]
     webhook_url=$2
 fi
 
+if [ ! -z "$3" ]
+  then
+    env=$3
+fi
+
 # crontab doesn't have access to env variable, define explicitly
 export MONITORING_SLACK_WEBHOOK_URL=${webhook_url};
+export ENV=${env};
 
 python ${path}/scripts/monitoring/monitor_containers.py
