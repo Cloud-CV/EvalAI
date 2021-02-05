4b7a1eeb31896d9f5d9878cf941ac38525047145
Ram Ramrakhya <ramramrakhya81@gmail.com>
2021-02-05 21:59:08 +0530

    Backend: Fix challenge phase lookup in allowed_email_ids API(#3226)
    * Add check in allowed_email_ids api to verify phase is from challenge
    
    * Fix lookup
    
    Co-authored-by: Rishabh Jain <rishabhjain2018@gmail.com>

--- apps/challenges/views.py
+++ apps/challenges/views.py
@@ -3459,7 +3459,18 @@ def update_allowed_email_ids(request, challenge_pk, phase_pk):
     """
     challenge = get_challenge_model(challenge_pk)
 
-    challenge_phase = get_challenge_phase_model(phase_pk)
+    try:
+        challenge_phase = ChallengePhase.objects.get(
+            challenge=challenge, pk=phase_pk
+        )
+    except ChallengePhase.DoesNotExist:
+        response_data = {
+            "error": "Challenge phase {} does not exist for challenge {}".format(
+                phase_pk, challenge.pk
+            )
+        }
+        return Response(response_data, status=status.HTTP_406_NOT_ACCEPTABLE)
+
     allowed_email_ids = challenge_phase.allowed_email_ids
 
     if allowed_email_ids is None:
