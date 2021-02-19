26024ed40b3b1e55573559f629a6c25dd1c019fb
Sandhu, Tanishq Singh <tanishq@gatech.edu>
2021-02-19 15:59:11 -0500

    Changes "challenge_type" to "tags"

--- apps/challenges/models.py
+++ apps/challenges/models.py
@@ -42,8 +42,8 @@ class Challenge(TimeStampedModel):
     terms_and_conditions = models.TextField(null=True, blank=True)
     submission_guidelines = models.TextField(null=True, blank=True)
     evaluation_details = models.TextField(null=True, blank=True)
-    challenge_type = ArrayField(
-        models.CharField(max_length=50, blank=True), default=list, blank=True
+    tags = ArrayField(
+        models.CharField(max_length=200, blank=True), default=list, blank=True, null=True
     )
     image = models.ImageField(
         upload_to=RandomFileName("logos"),
