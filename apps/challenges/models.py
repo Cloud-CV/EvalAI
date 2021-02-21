4bd86750dc814079f2c8e6ac836aba80bea021fd
Sandhu, Tanishq Singh <tanishq@gatech.edu>
2021-02-21 14:54:45 -0500

    cleaning up

--- apps/challenges/models.py
+++ apps/challenges/models.py
@@ -42,7 +42,6 @@ class Challenge(TimeStampedModel):
     terms_and_conditions = models.TextField(null=True, blank=True)
     submission_guidelines = models.TextField(null=True, blank=True)
     evaluation_details = models.TextField(null=True, blank=True)
-    
     image = models.ImageField(
         upload_to=RandomFileName("logos"),
         null=True,
