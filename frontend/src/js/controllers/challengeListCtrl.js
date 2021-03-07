5d4ba605ee453f1cf8b8927a734d48ab9a7f3960
Alabhya268 <singhalabhya32@gmail.com>
2021-03-08 02:06:31 +0530

    Formatted

--- frontend/src/js/controllers/challengeListCtrl.js
+++ frontend/src/js/controllers/challengeListCtrl.js
@@ -1,5 +1,5 @@
 // Invoking IIFE for challenge page
-(function() {
+(function () {
 
     'use strict';
     angular
@@ -32,15 +32,15 @@
             vm.renderAllChallenges();
         }
 
-        vm.submit = function(searchQuery) {
-            if(searchQuery === ""){
+        vm.submit = function (searchQuery) {
+            if (searchQuery === "") {
                 vm.searchModeOn = false;
                 vm.renderAllChallenges();
                 return;
             }
 
             vm.searchModeOn = true;
-            vm.currentSearchTerm=searchQuery;
+            vm.currentSearchTerm = searchQuery;
             var parameters = {};
             parameters.method = 'GET';
             if (userKey) {
@@ -52,7 +52,7 @@
             parameters.url = 'challenges/challenge/' + vm.currentSearchTerm + '/';
 
             parameters.callback = {
-                onSuccess: function(response) {
+                onSuccess: function (response) {
                     var data = response.data;
                     vm.currentList = data;
 
@@ -81,162 +81,162 @@
                         utilities.storeData("challengeCreator", vm.challengeCreator);
                     }
                 },
-                onError: function() {
+                onError: function () {
                     utilities.hideLoader();
                 }
             }
             utilities.sendRequest(parameters);
         };
-        vm.renderAllChallenges = function() {
-        // calls for ongoing challenges
-        vm.challengeCreator = {};
-        var parameters = {};
-        parameters.method = 'GET';
-        if (userKey) {
-            parameters.token = userKey;
-        } else {
-            parameters.token = null;
-        }
-        parameters.url = 'challenges/challenge/present';
+        vm.renderAllChallenges = function () {
+            // calls for ongoing challenges
+            vm.challengeCreator = {};
+            var parameters = {};
+            parameters.method = 'GET';
+            if (userKey) {
+                parameters.token = userKey;
+            } else {
+                parameters.token = null;
+            }
+            parameters.url = 'challenges/challenge/present';
 
-        parameters.callback = {
-            onSuccess: function(response) {
-                var data = response.data;
-                vm.currentList = data.results;
+            parameters.callback = {
+                onSuccess: function (response) {
+                    var data = response.data;
+                    vm.currentList = data.results;
 
-                if (vm.currentList.length === 0) {
-                    vm.noneCurrentChallenge = true;
-                } else {
-                    vm.noneCurrentChallenge = false;
-                }
-
-                var timezone = moment.tz.guess();
-                for (var i in vm.currentList) {
-
-                    var descLength = vm.currentList[i].description.length;
-                    if (descLength >= 50) {
-                        vm.currentList[i].isLarge = "...";
+                    if (vm.currentList.length === 0) {
+                        vm.noneCurrentChallenge = true;
                     } else {
-                        vm.currentList[i].isLarge = "";
+                        vm.noneCurrentChallenge = false;
                     }
-                    var offset = new Date(vm.currentList[i].start_date).getTimezoneOffset();
-                    vm.currentList[i].start_zone = moment.tz.zone(timezone).abbr(offset);
-                    offset = new Date(vm.currentList[i].end_date).getTimezoneOffset();
-                    vm.currentList[i].end_zone = moment.tz.zone(timezone).abbr(offset);
 
-                    var id = vm.currentList[i].id;
-                    vm.challengeCreator[id] = vm.currentList[i].creator.id;
-                    utilities.storeData("challengeCreator", vm.challengeCreator);
-                }
+                    var timezone = moment.tz.guess();
+                    for (var i in vm.currentList) {
 
-                // dependent api
-                // calls for upcoming challneges
-                parameters.url = 'challenges/challenge/future';
-                parameters.method = 'GET';
-
-                parameters.callback = {
-                    onSuccess: function(response) {
-                        var data = response.data;
-                        vm.upcomingList = data.results;
-
-                        if (vm.upcomingList.length === 0) {
-                            vm.noneUpcomingChallenge = true;
+                        var descLength = vm.currentList[i].description.length;
+                        if (descLength >= 50) {
+                            vm.currentList[i].isLarge = "...";
                         } else {
-                            vm.noneUpcomingChallenge = false;
+                            vm.currentList[i].isLarge = "";
                         }
+                        var offset = new Date(vm.currentList[i].start_date).getTimezoneOffset();
+                        vm.currentList[i].start_zone = moment.tz.zone(timezone).abbr(offset);
+                        offset = new Date(vm.currentList[i].end_date).getTimezoneOffset();
+                        vm.currentList[i].end_zone = moment.tz.zone(timezone).abbr(offset);
 
-                        var timezone = moment.tz.guess();
-                        for (var i in vm.upcomingList) {
+                        var id = vm.currentList[i].id;
+                        vm.challengeCreator[id] = vm.currentList[i].creator.id;
+                        utilities.storeData("challengeCreator", vm.challengeCreator);
+                    }
 
-                            var descLength = vm.upcomingList[i].description.length;
+                    // dependent api
+                    // calls for upcoming challneges
+                    parameters.url = 'challenges/challenge/future';
+                    parameters.method = 'GET';
 
-                            if (descLength >= 50) {
-                                vm.upcomingList[i].isLarge = "...";
+                    parameters.callback = {
+                        onSuccess: function (response) {
+                            var data = response.data;
+                            vm.upcomingList = data.results;
+
+                            if (vm.upcomingList.length === 0) {
+                                vm.noneUpcomingChallenge = true;
                             } else {
-                                vm.upcomingList[i].isLarge = "";
+                                vm.noneUpcomingChallenge = false;
                             }
-                            
-                            var offset = new Date(vm.upcomingList[i].start_date).getTimezoneOffset();
-                            vm.upcomingList[i].start_zone = moment.tz.zone(timezone).abbr(offset);
-                            offset = new Date(vm.upcomingList[i].end_date).getTimezoneOffset();
-                            vm.upcomingList[i].end_zone = moment.tz.zone(timezone).abbr(offset);
 
-                            
-                            var id = vm.upcomingList[i].id;
-                            vm.challengeCreator[id] = vm.upcomingList[i].creator.id;
-                            utilities.storeData("challengeCreator", vm.challengeCreator);
-                        }
+                            var timezone = moment.tz.guess();
+                            for (var i in vm.upcomingList) {
 
-                        // dependent api
-                        // calls for past challneges
-                        parameters.url = 'challenges/challenge/past';
-                        parameters.method = 'GET';
+                                var descLength = vm.upcomingList[i].description.length;
 
-                        parameters.callback = {
-                            onSuccess: function(response) {
-                                var data = response.data;
-                                vm.pastList = data.results;
-
-                                if (vm.pastList.length === 0) {
-                                    vm.nonePastChallenge = true;
+                                if (descLength >= 50) {
+                                    vm.upcomingList[i].isLarge = "...";
                                 } else {
-                                    vm.nonePastChallenge = false;
+                                    vm.upcomingList[i].isLarge = "";
                                 }
 
-                                var timezone = moment.tz.guess();
-                                for (var i in vm.pastList) {
+                                var offset = new Date(vm.upcomingList[i].start_date).getTimezoneOffset();
+                                vm.upcomingList[i].start_zone = moment.tz.zone(timezone).abbr(offset);
+                                offset = new Date(vm.upcomingList[i].end_date).getTimezoneOffset();
+                                vm.upcomingList[i].end_zone = moment.tz.zone(timezone).abbr(offset);
 
 
-                                    var descLength = vm.pastList[i].description.length;
-                                    if (descLength >= 50) {
-                                        vm.pastList[i].isLarge = "...";
+                                var id = vm.upcomingList[i].id;
+                                vm.challengeCreator[id] = vm.upcomingList[i].creator.id;
+                                utilities.storeData("challengeCreator", vm.challengeCreator);
+                            }
+
+                            // dependent api
+                            // calls for past challneges
+                            parameters.url = 'challenges/challenge/past';
+                            parameters.method = 'GET';
+
+                            parameters.callback = {
+                                onSuccess: function (response) {
+                                    var data = response.data;
+                                    vm.pastList = data.results;
+
+                                    if (vm.pastList.length === 0) {
+                                        vm.nonePastChallenge = true;
                                     } else {
-                                        vm.pastList[i].isLarge = "";
+                                        vm.nonePastChallenge = false;
                                     }
 
-                                    var offset = new Date(vm.pastList[i].start_date).getTimezoneOffset();
-                                    vm.pastList[i].start_zone = moment.tz.zone(timezone).abbr(offset);
-                                    offset = new Date(vm.pastList[i].end_date).getTimezoneOffset();
-                                    vm.pastList[i].end_zone = moment.tz.zone(timezone).abbr(offset);
+                                    var timezone = moment.tz.guess();
+                                    for (var i in vm.pastList) {
 
-                                    var id = vm.pastList[i].id;
-                                    vm.challengeCreator[id] = vm.pastList[i].creator.id;
-                                    utilities.storeData("challengeCreator", vm.challengeCreator);
+
+                                        var descLength = vm.pastList[i].description.length;
+                                        if (descLength >= 50) {
+                                            vm.pastList[i].isLarge = "...";
+                                        } else {
+                                            vm.pastList[i].isLarge = "";
+                                        }
+
+                                        var offset = new Date(vm.pastList[i].start_date).getTimezoneOffset();
+                                        vm.pastList[i].start_zone = moment.tz.zone(timezone).abbr(offset);
+                                        offset = new Date(vm.pastList[i].end_date).getTimezoneOffset();
+                                        vm.pastList[i].end_zone = moment.tz.zone(timezone).abbr(offset);
+
+                                        var id = vm.pastList[i].id;
+                                        vm.challengeCreator[id] = vm.pastList[i].creator.id;
+                                        utilities.storeData("challengeCreator", vm.challengeCreator);
+                                    }
+
+                                    utilities.hideLoader();
+
+                                },
+                                onError: function () {
+                                    utilities.hideLoader();
                                 }
+                            };
 
-                                utilities.hideLoader();
+                            utilities.sendRequest(parameters);
 
-                            },
-                            onError: function() {
-                                utilities.hideLoader();
-                            }
-                        };
+                        },
+                        onError: function () {
+                            utilities.hideLoader();
+                        }
+                    };
 
-                        utilities.sendRequest(parameters);
+                    utilities.sendRequest(parameters);
 
-                    },
-                    onError: function() {
-                        utilities.hideLoader();
-                    }
-                };
+                },
+                onError: function () {
 
-                utilities.sendRequest(parameters);
+                    utilities.hideLoader();
+                }
+            };
 
-            },
-            onError: function() {
+            utilities.sendRequest(parameters);
 
-                utilities.hideLoader();
-            }
         };
 
-        utilities.sendRequest(parameters);
+        vm.renderAllChallenges();
 
-    };
-
-    vm.renderAllChallenges();
-
-        vm.scrollUp = function() {
-            angular.element($window).bind('scroll', function() {
+        vm.scrollUp = function () {
+            angular.element($window).bind('scroll', function () {
                 if (this.pageYOffset >= 100) {
                     utilities.showButton();
                 } else {
