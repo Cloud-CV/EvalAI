94dbdff892f5adb48af83ecd98d8cb9bac3f6ee6
Alabhya268 <singhalabhya32@gmail.com>
2021-03-18 12:47:51 +0530

    small changes

--- frontend/src/js/filters/filters.js
+++ frontend/src/js/filters/filters.js
@@ -21,12 +21,12 @@
     function format_execution_time() {
         return function(execution_time) {
             var executiontime = new Date(execution_time * 1000);
-            var days = executiontime.getUTCDate() - 1;
+            var days = (executiontime.getUTCDate() - 1);
             var hours = executiontime.getUTCHours();
             var minutes = executiontime.getUTCMinutes();
             var seconds = executiontime.getSeconds();
-            var timeString = days.toString().padStart(2, '0') + ' day ' + hours.toString().padStart(2, '0') + ' hr ' + 
-                minutes.toString().padStart(2, '0') + ' min ' + 
+            var timeString = (days != 0 ? days.toString().padStart(2, '0') + ' day ' : '') + (hours != 0 ? hours.toString().padStart(2, '0') + ' hr ' : '')+ 
+                (minutes != 0 ? minutes.toString().padStart(2, '0') + ' min ' : '') + 
                 seconds.toString().padStart(2, '0') + ' sec';
             return timeString;
         };
