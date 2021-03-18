067e4f8c2cc9d60e3ea7e40bc379e2fd84703bb0
Alabhya268 <singhalabhya32@gmail.com>
2021-03-18 12:50:31 +0530

    Formatted code

--- frontend/src/js/filters/filters.js
+++ frontend/src/js/filters/filters.js
@@ -1,7 +1,7 @@
 // define filters here
 
 // filter to get ceiling value
-(function() {
+(function () {
 
     'use strict';
 
@@ -10,23 +10,24 @@
         .filter('ceil', ceil);
 
     function ceil() {
-        return function(input) {
+        return function (input) {
             return Math.ceil(input);
         };
     }
 
     angular.module('evalai')
-            .filter('format_execution_time', format_execution_time);
+        .filter('format_execution_time', format_execution_time);
 
     function format_execution_time() {
-        return function(execution_time) {
+        return function (execution_time) {
             var executiontime = new Date(execution_time * 1000);
             var days = (executiontime.getUTCDate() - 1);
             var hours = executiontime.getUTCHours();
             var minutes = executiontime.getUTCMinutes();
             var seconds = executiontime.getSeconds();
-            var timeString = (days != 0 ? days.toString().padStart(2, '0') + ' day ' : '') + (hours != 0 ? hours.toString().padStart(2, '0') + ' hr ' : '')+ 
-                (minutes != 0 ? minutes.toString().padStart(2, '0') + ' min ' : '') + 
+            var timeString = (days != 0 ? days.toString().padStart(2, '0') + ' day ' : '') +
+                (hours != 0 ? hours.toString().padStart(2, '0') + ' hr ' : '') +
+                (minutes != 0 ? minutes.toString().padStart(2, '0') + ' min ' : '') +
                 seconds.toString().padStart(2, '0') + ' sec';
             return timeString;
         };
