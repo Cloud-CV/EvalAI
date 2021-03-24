0af7abd3a7b1c1aab94ca77d45149c8fce19207a
gautamjajoo <gowmsigautam2002@gmail.com>
2021-03-24 21:02:43 +0530

    fix row-separator

--- frontend_v2/src/app/components/challenge/challengesubmissions/challengesubmissions.component.ts
+++ frontend_v2/src/app/components/challenge/challengesubmissions/challengesubmissions.component.ts
@@ -24,7 +24,7 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
   styleUrls: ['./challengesubmissions.component.scss'],
   animations: [
     trigger('detailExpand', [
-      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('collapsed', style({ height: '0px', minHeight: '0'})),
       state('expanded', style({ height: '*' })),
       transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
     ]),
