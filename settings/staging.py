6a97a3826bba6ccfe4b38a181310ff1330b0d5ca
gautamjajoo <gowmsigautam2002@gmail.com>
2021-03-30 18:56:58 +0530

    add staging link

--- settings/staging.py
+++ settings/staging.py
@@ -6,6 +6,7 @@ CORS_ORIGIN_ALLOW_ALL = False
 
 CORS_ORIGIN_WHITELIST = (
     "https://evalai.s3.amazonaws.com",
+    "https://staging-evalai.s3.amazonaws.com",
     "https://staging.eval.ai",
     "https://beta-staging.eval.ai",
 )
