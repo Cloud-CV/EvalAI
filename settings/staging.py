4fe16fe5e36e8b732ab66526c4a6969344549236
Gautam Jajoo <gowmsigautam2002@gmail.com>
2021-04-03 08:26:08 +0530

    Add `https://evalai.s3.amazonaws.com` in staging CORS(#3370)
    * fix media assets endpoint
    
    * add staging link
    
    Co-authored-by: Rishabh Jain <rishabhjain2018@gmail.com>

--- settings/staging.py
+++ settings/staging.py
@@ -5,6 +5,7 @@ ALLOWED_HOSTS = ["staging.eval.ai"]
 CORS_ORIGIN_ALLOW_ALL = False
 
 CORS_ORIGIN_WHITELIST = (
+    "https://evalai.s3.amazonaws.com",
     "https://staging-evalai.s3.amazonaws.com",
     "https://staging.eval.ai",
     "https://beta-staging.eval.ai",
