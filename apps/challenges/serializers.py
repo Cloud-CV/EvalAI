e69200b84e82af99fb9ef1644467e0d685a2d14e
savish <savishbedi1@gmail.com>
2021-05-30 04:19:45 +0530

    Add: Active and route_table_association_ids ChallengeEvaluationCluster serializer

--- apps/challenges/serializers.py
+++ apps/challenges/serializers.py
@@ -434,10 +434,11 @@ class ChallengeEvaluationClusterSerializer(serializers.ModelSerializer):
             "security_group_id",
             "internet_gateway_id",
             "route_table_id",
-            "efs_id",
+            "route_table_association_ids" "efs_id",
             "efs_security_group_id",
             "efs_creation_token",
             "efs_mount_target_ids",
+            "active",
         )
 
 
