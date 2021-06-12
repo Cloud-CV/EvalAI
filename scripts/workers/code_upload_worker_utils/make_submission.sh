137dfb199eeae3beaa4346e64e71b8a98a50d17d
savish <savishbedi1@gmail.com>
2021-06-13 01:50:27 +0530

    Shift curl requets to shell script

--- scripts/workers/code_upload_worker_utils/make_submission.sh
+++ scripts/workers/code_upload_worker_utils/make_submission.sh
@@ -1,18 +1,29 @@
 #!/bin/sh
 echo "File Submission Started."
-# Submission_folder $1
-# Sumbission curl request $2
-# File Not found curl request $3
-if [ -f "$1/submission.csv" ]
+submission_url="$EVALAI_API_SERVER/api/jobs/challenge/$CHALLENGE_PK/challenge_phase/$PHASE_PK/submission/$SUBMISSION_PK"
+submission_curl_base_request="curl --location --request PATCH '$submission_url' --header 'Authorization: Bearer $AUTH_TOKEN'"
+if [ -f "$SUBMISSION_PATH/submission.csv" ]
 then
     file_path="submission.csv"
-    eval $2
-elif [ -f "$1/submission.json" ]
+    curl_request="$submission_curl_base_request -F \"submission_input_file=@$SUBMISSION_PATH/$file_path\""
+elif [ -f "$SUBMISSION_PATH/submission.json" ]
 then
-  file_path="submission.json"
-  eval $2
+    file_path="submission.json"
+    curl_request="$submission_curl_base_request -F \"submission_input_file=@$SUBMISSION_PATH/$file_path\""
 else
-    echo "Submission file not found."
-    eval $3
+    echo "submission.json/submission.csv not found."
+    url="$EVALAI_API_SERVER/api/jobs/challenge/$CHALLENGE_PK/update_submission/"
+    submission_data="{\
+      \"challenge_phase\": $PHASE_PK,\
+      \"submission\": $SUBMISSION_PK,\
+      \"stdout\": \"\",\
+      \"stderr\": \"submission.json/submission.csv not found.\",\
+      \"submission_status\": \"failed\",\
+      \"result\": \"[]\",\
+      \"metadata\": \"\"\
+    }"
+    curl_request="curl --location --request PUT '$url' -H 'Content-Type: application/json' --header 'Authorization: Bearer $AUTH_TOKEN' -d '$submission_data'"
 fi
-echo "File Submission Ended"
+echo $curl_request
+eval $curl_request
+echo "\nFile Submission Ended"
