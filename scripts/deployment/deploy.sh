e22cd37b6ef89f6812998b04711f2547e210c96b
Aviral Jain <avi.aviral140@gmail.com>
2021-06-15 01:15:53 +0530

    add configs to deploy gateway and nodeexporter to staging setup

--- scripts/deployment/deploy.sh
+++ scripts/deployment/deploy.sh
@@ -145,6 +145,16 @@ case $opt in
             docker-compose -f docker-compose-${env}.yml up -d grafana
             echo "Completed deploy operation."
             ;;
+        deploy-node-exporter)
+            echo "Deploying node-exporter docker container..."
+            docker-compose -f docker-compose-${env}.yml up -d node-exporter
+            echo "Completed deploy operation."
+            ;;
+        deploy-pushgateway)
+            echo "Deploying prometheus-pushgateway docker container..."
+            docker-compose -f docker-compose-${env}.yml up -d pushgateway
+            echo "Completed deploy operation."
+            ;;
         scale)
             service=${3}
             instances=${4}
@@ -186,6 +196,10 @@ case $opt in
         echo "        Eg. ./scripts/deployment/deploy.sh deploy-prometheus production"
         echo "    deploy-grafana : Deploy grafana container in the respective environment."
         echo "        Eg. ./scripts/deployment/deploy.sh deploy-grafana production"
+        echo "    deploy-node-exporter : Deploy node-exporter container in the respective environment."
+        echo "        Eg. ./scripts/deployment/deploy.sh deploy-node-exporter production"
+        echo "    deploy-pushgateway : Deploy prometheus-pushgateway container in the respective environment."
+        echo "        Eg. ./scripts/deployment/deploy.sh deploy-pushgateway production"
         echo "    scale  : Scale particular docker service in an environment."
         echo "        Eg. ./scripts/deployment/deploy.sh scale production django 5"
         echo "    clean  : Remove all docker containers and images."
