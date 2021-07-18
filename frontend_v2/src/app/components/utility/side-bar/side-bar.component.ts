6b6bca5fc0c0bf2e8c2ce45ae6427ac96448bc29
Gautam Jajoo <gowmsigautam2002@gmail.com>
2021-07-19 00:04:26 +0530

    Frontend_V2: Highlight tab of side-bar when moving to challenge components (#3531)
    * add service
    
    * add the functionality for tab highlight
    
    * fix final issues
    
    * remove console statement
    
    * fix error
    
    Co-authored-by: Rishabh Jain <rishabhjain2018@gmail.com>

--- frontend_v2/src/app/components/utility/side-bar/side-bar.component.ts
+++ frontend_v2/src/app/components/utility/side-bar/side-bar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from "rxjs/internal/operators";
+import { GlobalService } from '../../../services/global.service';
 
 @Component({
   selector: 'app-side-bar',
@@ -6,7 +9,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-bar.component.scss'],
 })
 export class SideBarComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit() {}
+  /**
+   * Current name of tab which needs to be active
+   */
+  tabHighlight: string = "allChallenges";
+
+  /**
+   * Returns true if the string is not a number
+   */
+  isChallengeComponent : boolean = false;
+
+   /**
+   * Constructor
+   * @param globalService  GlobalService Injection.
+   * @param router Router
+   */
+  constructor(
+    private globalService: GlobalService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.globalService.nameTabHighlight.subscribe((tabHighlight) => {
+      this.tabHighlight = tabHighlight;
+    });
+
+    this.router.events
+    .pipe(filter(event => event instanceof NavigationEnd))
+    .subscribe((event) => {
+      if(event) {
+          if(this.router.url.split('/')[2] == "me") {
+            this.tabHighlight = "hostedChallenges";
+            this.globalService.changeTabActiveStatus("hostedChallenges");
+          }
+          else if(this.router.url.split('/')[2] == "hosts") {
+            this.tabHighlight = "createChallenge";
+            this.globalService.changeTabActiveStatus("createChallenge");
+          }
+          else if(this.router.url.split('/')[2] == "participants") {
+            this.tabHighlight = "myParticipantTeams";
+            this.globalService.changeTabActiveStatus("myParticipantTeams");
+          }
+          else if(this.router.url.split('/')[2] == "participated") {
+            this.tabHighlight = "participatedChallenges";
+            this.globalService.changeTabActiveStatus("participatedChallenges");
+          }
+          else if(this.router.url.split('/')[2] == "all") {
+            this.tabHighlight = "allChallenges";
+            this.globalService.changeTabActiveStatus("allChallenges");
+          }
+        this.isChallengeComponent = isNaN(parseInt(this.router.url.split('/')[length]));
+      }
+  });
+  }
 }
