c173f83e37cae0ff4640e20f21a0ea483838e470
Gautam Jajoo <gowmsigautam2002@gmail.com>
2021-08-26 21:39:10 +0530

    Frontend_V2: Fix console error of change in expression value in development setup(#3550)
    * fix change in expression value
    
    * assign value on component initialization
    
    * add line
    
    Co-authored-by: Rishabh Jain <rishabhjain2018@gmail.com>

--- frontend_v2/src/app/components/publiclists/publiclists.component.ts
+++ frontend_v2/src/app/components/publiclists/publiclists.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewChecked, ChangeDetectorRef } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 
 /**
@@ -9,12 +9,36 @@ import { AuthService } from '../../services/auth.service';
   templateUrl: './publiclists.component.html',
   styleUrls: ['./publiclists.component.scss'],
 })
-export class PubliclistsComponent {
+export class PubliclistsComponent implements OnInit, AfterViewChecked {
+
+  isAuth = false;
+
   /**
    * Constructor.
    * @param authService
+   * @param changeDetector
    */
   constructor(
-    public authService: AuthService
+    public authService: AuthService,
+    private cdRef : ChangeDetectorRef
   ) {}
+  
+  /**
+   * Component on Initialization.
+   */
+  ngOnInit() {
+    this.isAuth = this.authService.isAuth;
+  }
+
+  /**
+   * DEV MODE:
+   * For resolving change in expression value after it is checked
+   */
+  ngAfterViewChecked() {
+    let isAuth = this.authService.isAuth;
+    if(isAuth != this.isAuth) {
+      this.isAuth = isAuth;
+      this.cdRef.detectChanges();
+    }
+  }
 }
