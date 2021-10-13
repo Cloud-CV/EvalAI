b8ced7ee23d265cdfc8b6042398e4a56106de36a
Ram Ramrakhya <ramramrakhya81@gmail.com>
2021-10-12 23:55:08 -0400

    Backend: Add statsd middleware for metrics push (#3613)

--- middleware/statsd/statsd_middleware.py
+++ middleware/statsd/statsd_middleware.py
@@ -1,7 +1,7 @@
 import time
 
 from django.utils.deprecation import MiddlewareMixin
-# from monitoring.statsd.metrics import statsd, REQUEST_COUNT_METRIC_NAME, REQUEST_LATENCY_METRIC_NAME
+from monitoring.statsd.metrics import statsd, REQUEST_COUNT_METRIC_NAME, REQUEST_LATENCY_METRIC_NAME
 
 
 class StatsdMetricsMiddleware(MiddlewareMixin):
@@ -17,26 +17,24 @@ class StatsdMetricsMiddleware(MiddlewareMixin):
         return view_name
 
     def process_response(self, request, response):
-        # TODO: Enable statsd metric push once production docker setup is ready
-        # statsd.increment(
-        #     REQUEST_COUNT_METRIC_NAME,
-        #     tags=[
-        #         "service:django_worker",
-        #         "method:%s" % request.method,
-        #         "view:%s" % self.get_view_name(request),
-        #         "status:%s" % str(response.status_code),
-        #     ],
-        # )
+        statsd.increment(
+            REQUEST_COUNT_METRIC_NAME,
+            tags=[
+                "service:django_worker",
+                "method:%s" % request.method,
+                "view:%s" % self.get_view_name(request),
+                "status:%s" % str(response.status_code),
+            ],
+        )
 
-        # resp_time = (time.time() - request.start_time) * 1000
+        resp_time = (time.time() - request.start_time) * 1000
 
-        # statsd.histogram(
-        #     REQUEST_LATENCY_METRIC_NAME,
-        #     resp_time,
-        #     tags=[
-        #         "service:django_worker",
-        #         "view:%s" % self.get_view_name(request),
-        #     ],
-        # )
-
+        statsd.histogram(
+            REQUEST_LATENCY_METRIC_NAME,
+            resp_time,
+            tags=[
+                "service:django_worker",
+                "view:%s" % self.get_view_name(request),
+            ],
+        )
         return response
