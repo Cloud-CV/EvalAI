0c950ad9f0f1508c3f849c3eb06b68d4537ff931
Gunjan Chhablani <chhablani.gunjan@gmail.com>
2022-09-14 18:44:44 -0400

    Fix variables in auto-scaling shell script

--- scripts/monitoring/run_auto_scale_workers.sh
+++ scripts/monitoring/run_auto_scale_workers.sh
@@ -19,12 +19,18 @@ fi
 
 if [ ! -z "$4" ]
   then
-    monitoring_api_url=$3
+    monitoring_api_url=$4
+fi
+
+if [ ! -z "$5" ]
+  then
+    env=$5
 fi
 
 # crontab doesn't have access to env variable, define explicitly
 export AUTH_TOKEN=${auth_token};
 export API_HOST_URL=${api_host_url};
 export MONITORING_API_URL=${monitoring_api_url};
+export ENV=${env}
 
 /home/ubuntu/venv/bin/python ${path}/scripts/monitoring/auto_scale_workers.py
