32c43563eaaaeaa2b1e044980ef14b60a51cf146
Gunjan Chhablani <chhablani.gunjan@gmail.com>
2022-10-10 14:26:05 -0400

    Add flush method to MultiOut

--- scripts/workers/submission_worker.py
+++ scripts/workers/submission_worker.py
@@ -109,7 +109,10 @@ class MultiOut(object):
     def write(self, s):
         for f in self.handles:
             f.write(s)
-
+    
+    def flush(self):
+        for f in self.handles:
+            f.flush()
 
 @contextlib.contextmanager
 def stdout_redirect(where):
