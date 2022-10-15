2c16954710da2f81734f0356bc0cf3760a4e6fcc
So, Matthew <42504035+matthew-so@users.noreply.github.com>
2022-10-15 02:05:48 -0400

    Environment error file requirements removed from non-code-upload functions

--- apps/jobs/views.py
+++ apps/jobs/views.py
@@ -1076,7 +1076,7 @@ def update_submission(request, challenge_pk):
      - ``submission``: submission id, e.g. 123 (**required**)
      - ``stdout``: Stdout after evaluation, e.g. "Evaluation completed in 2 minutes" (**required**)
      - ``stderr``: Stderr after evaluation, e.g. "Failed due to incorrect file format" (**required**)
-     - ``enverr``: Environment error after evaluation, e.g. "Failed due to attempted action being invalid" (**required**)
+     - ``enverr``: Environment error after evaluation, e.g. "Failed due to attempted action being invalid" (**code upload challenge only**)
      - ``submission_status``: Status of submission after evaluation
         (can take one of the following values: `FINISHED`/`CANCELLED`/`FAILED`), e.g. FINISHED (**required**)
      - ``result``: contains accuracies for each metric, (**required**) e.g.
@@ -1482,7 +1482,6 @@ def update_partially_evaluated_submission(request, challenge_pk):
         submission_status = request.data.get("submission_status", "").lower()
         stdout_content = request.data.get("stdout", "")
         stderr_content = request.data.get("stderr", "")
-        enverr_content = request.data.get("enverr", "")
         submission_result = request.data.get("result", "")
         metadata = request.data.get("metadata", "")
         submission = get_submission_model(submission_pk)
@@ -1618,7 +1617,6 @@ def update_partially_evaluated_submission(request, challenge_pk):
         submission.completed_at = timezone.now()
         submission.stdout_file.save("stdout.txt", ContentFile(stdout_content))
         submission.stderr_file.save("stderr.txt", ContentFile(stderr_content))
-        submission.enverr_file.save("enverr.txt", ContentFile(enverr_content))
         submission.submission_result_file.save(
             "submission_result.json", ContentFile(str(public_results))
         )
@@ -1673,7 +1671,6 @@ def update_partially_evaluated_submission(request, challenge_pk):
             challenge_phase_pk = request.data.get("challenge_phase")
             stdout_content = request.data.get("stdout", "")
             stderr_content = request.data.get("stderr", "")
-            enverr_content = request.data.get("enverr", "")
             submission_result = request.data.get("result", "")
 
             try:
@@ -1811,9 +1808,6 @@ def update_partially_evaluated_submission(request, challenge_pk):
             )
             submission.stderr_file.save(
                 "stderr.txt", ContentFile(stderr_content)
-            )
-            submission.enverr_file.save(
-                "enverr.txt", ContentFile(enverr_content)
             )
             submission.submission_result_file.save(
                 "submission_result.json", ContentFile(str(public_results))
