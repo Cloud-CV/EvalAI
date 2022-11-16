87e6234aabad1b6b4634e043529f14d075a55f6c
Gunjan Chhablani <chhablani.gunjan@gmail.com>
2022-11-16 12:17:49 -0500

    Fix flake8 issue

--- apps/challenges/views.py
+++ apps/challenges/views.py
@@ -2959,7 +2959,7 @@ def get_worker_logs(request, challenge_pk):
     pattern = ""  # Empty string to get all logs including container logs.
 
     # This is to specify the time window for fetching logs: 3 days before from current time.
-    timeframe = 4320 # 15
+    timeframe = 4320  # 15
     limit = 1000  # logs last 1000 lines
     current_time = int(round(time.time() * 1000))
     start_time = current_time - (timeframe * 60000)
