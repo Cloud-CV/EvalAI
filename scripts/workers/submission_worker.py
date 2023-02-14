4d664cedac20bb963d7c9c26293ee0255630a359
Gunjan Chhablani <chhablani.gunjan@gmail.com>
2023-02-14 00:09:50 -0500

    Fix failing test

--- scripts/workers/submission_worker.py
+++ scripts/workers/submission_worker.py
@@ -23,7 +23,10 @@ import requests
 import yaml
 from django.core.files.base import ContentFile
 from django.utils import timezone
-from .statsd_utils import increment_and_push_metrics_to_statsd
+try:
+    from statsd_utils import increment_and_push_metrics_to_statsd
+except:
+    from .statsd_utils import increment_and_push_metrics_to_statsd
 
 # all challenge and submission will be stored in temp directory
 BASE_TEMP_DIR = tempfile.mkdtemp()
