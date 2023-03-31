9bb868574b1bdfcc1b96e033d1e3520fde089901
gchhablani <chhablani.gunjan@gmail.com>
2023-03-31 16:54:33 -0400

    Remove auth token from shell script

--- scripts/monitoring/run_auto_scale_eks_nodes.sh
+++ scripts/monitoring/run_auto_scale_eks_nodes.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 path=$PWD
-auth_token=''
 api_host_url=''
 eks_aws_storage_bucket_name=''
+
 if [ ! -z "$1" ]
   then
     path=$1
@@ -10,51 +10,45 @@ fi
 
 if [ ! -z "$2" ]
   then
-    auth_token=$2
+    api_host_url=$2
 fi
 
 if [ ! -z "$3" ]
   then
-    api_host_url=$3
+    monitoring_api_url=$3
 fi
 
 if [ ! -z "$4" ]
   then
-    monitoring_api_url=$4
+    env=$4
 fi
 
 if [ ! -z "$5" ]
   then
-    env=$5
+    eks_aws_account_id=$5
 fi
 
 if [ ! -z "$6" ]
   then
-    eks_aws_account_id=$6
+    eks_aws_access_key_id=$6
 fi
 
 if [ ! -z "$7" ]
   then
-    eks_aws_access_key_id=$7
+    eks_aws_secret_access_key=$7
 fi
 
 if [ ! -z "$8" ]
   then
-    eks_aws_secret_access_key=$8
+    eks_aws_region=$8
 fi
 
 if [ ! -z "$9" ]
   then
-    eks_aws_region=$9
-fi
-
-if [ ! -z "$10" ]
-  then
-    eks_aws_storage_bucket_name=$10
+    eks_aws_storage_bucket_name=$9
 fi
 
 # crontab doesn't have access to env variable, define explicitly
-export AUTH_TOKEN=${auth_token};
 export API_HOST_URL=${api_host_url};
 export MONITORING_API_URL=${monitoring_api_url};
 export ENV=${env}
