b888070c414936d2ed96634cc1acc4b4abe7f291
gchhablani <chhablani.gunjan@gmail.com>
2023-07-11 16:56:42 -0400

    Add disable logs to phase create serializer

--- apps/challenges/serializers.py
+++ apps/challenges/serializers.py
@@ -377,6 +377,7 @@ class ChallengePhaseCreateSerializer(serializers.ModelSerializer):
             "allowed_submission_file_types",
             "default_submission_meta_attributes",
             "allowed_email_ids",
+            "disable_logs",
         )
 
 
