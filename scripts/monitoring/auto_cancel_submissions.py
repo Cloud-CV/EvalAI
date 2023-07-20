d149d83c54c67e336fa86b56d4c928df790d2f9d
AyushR1 <ayushrathore12501@gmail.com>
2023-07-21 01:07:46 +0530

    Add script to auto cancel submissions older than 14 days
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

---
+++ scripts/monitoring/auto_cancel_submissions.py
@@ -0,0 +1,82 @@
+from datetime import datetime, timedelta
+import pytz  # Use this to handle timezones if needed
+import os
+import logging
+import requests
+from evalai_interface import EvalAI_Interface
+
+def get_submission_time(submission):
+    # Get the submission time based on the presence of "rerun_resumed_at"
+    if "rerun_resumed_at" in submission and submission["rerun_resumed_at"]:
+        return datetime.fromisoformat(
+            submission["rerun_resumed_at"].rstrip("Z")
+        )
+    else:
+        return datetime.fromisoformat(submission["submitted_at"].rstrip("Z"))
+
+
+def auto_cancel_submissions(challenge_pk, days_threshold=14):
+    """
+    Auto-cancels submissions that have statuses "submitted" or "running" for more than `days_threshold` days,
+    considering the "rerun_resumed_at" time if available, otherwise using the "submitted_at" time.
+
+    :param challenge_pk: The challenge primary key for which submissions should be checked.
+    :param days_threshold: The number of days after which submissions should be canceled (default is 14).
+    """
+    try:
+        evalai = EvalAI_Interface(AUTH_TOKEN, EVALAI_API_SERVER)
+
+        submissions = evalai.get_submissions_for_challenge(
+            challenge_pk, "finished"
+        )
+        submissions += evalai.get_submissions_for_challenge(
+            challenge_pk, "running"
+        )
+        submissions += evalai.get_submissions_for_challenge(
+            challenge_pk, "resuming"
+        )
+        print(submissions)
+
+        current_time = datetime.now(pytz.utc)
+        for submission in submissions:
+            status = submission["status"]
+            submission_time = get_submission_time(submission)
+            submission_time = pytz.utc.localize(submission_time)
+
+            time_difference = current_time - submission_time
+            if time_difference > timedelta(days=days_threshold):
+                data = {"status": "cancelled"}
+                evalai.update_submission_status(data, submission["challenge"])
+                print(
+                    f"Cancelled submission with PK {submission['id']}. Previous status: {status}. Time Lapsed: {time_difference}"
+                )
+    except Exception as e:
+        raise (f"Error in auto-cancel script: {str(e)}")
+
+# Example usage:
+if __name__ == "__main__":
+    # Provide your AUTH_TOKEN and EVALAI_API_SERVER
+    AUTH_TOKEN = os.environ.get("AUTH_TOKEN")	
+    EVALAI_API_SERVER = os.environ.get("API_HOST_URL")
+
+    # Initialize the EvalAI_Interface
+    evalai = EvalAI_Interface(AUTH_TOKEN, EVALAI_API_SERVER)
+
+    # Get all challenges
+    challenges = evalai.get_challenges()
+    
+    # Loop through all challenges and run the auto-cancel script for each challenge
+    for challenge in challenges["results"]:
+        challenge_pk = challenge["id"]
+        print(f"Running auto-cancel script for challenge {challenge_pk}")
+        auto_cancel_submissions(challenge_pk)
+    next_page = challenges["next"]
+    while next_page is not None:
+        challenges = evalai.get_challenges(next_page_url=next_page)
+        for challenge in challenges["results"]:
+            challenge_pk = challenge["id"]
+            print(f"Running auto-cancel script for challenge {challenge_pk}")
+            auto_cancel_submissions(challenge_pk)
+        next_page = challenges["next"]
+
+
