012aada32dc2293917aff5984603a0eee279162f
gchhablani <chhablani.gunjan@gmail.com>
2023-07-30 02:16:16 -0400

    Update shell scripts

--- scripts/monitoring/run_auto_scale_eks_nodes.sh
+++ scripts/monitoring/run_auto_scale_eks_nodes.sh
@@ -17,37 +17,32 @@ if [ ! -z "$3" ]; then
 fi
 
 if [ ! -z "$4" ]; then
-  monitoring_api_url=$4
+  env=$4
 fi
 
 if [ ! -z "$5" ]; then
-  env=$5
+  eks_aws_account_id=$5
 fi
 
 if [ ! -z "$6" ]; then
-  eks_aws_account_id=$6
+  eks_aws_access_key_id=$6
 fi
 
 if [ ! -z "$7" ]; then
-  eks_aws_access_key_id=$7
+  eks_aws_secret_access_key=$7
 fi
 
 if [ ! -z "$8" ]; then
-  eks_aws_secret_access_key=$8
+  eks_aws_region=$8
 fi
 
-if [ ! -z "$9" ]; then
-  eks_aws_region=$9
-fi
-
-if [ ! -z "${10}" ]; then
-  eks_aws_storage_bucket_name=${10}
+if [ ! -z "${9}" ]; then
+  eks_aws_storage_bucket_name=${9}
 fi
 
 # crontab doesn't have access to env variable, define explicitly
 export JSON_PATH=${json_path}
 export API_HOST_URL=${api_host_url}
-export MONITORING_API_URL=${monitoring_api_url}
 export ENV=${env}
 export EKS_AWS_ACCOUNT_ID=${eks_aws_account_id}
 export EKS_AWS_ACCESS_KEY_ID=${eks_aws_access_key_id}
