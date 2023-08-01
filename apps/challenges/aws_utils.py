1fa57958dcf0ff5a18a3102857a3fe3814a72dd7
AyushR1 <ayushrathore12501@gmail.com>
2023-08-02 01:02:00 +0530

    Fix global dict change and only modify worker image
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- apps/challenges/aws_utils.py
+++ apps/challenges/aws_utils.py
@@ -175,9 +175,6 @@ def register_task_def_by_challenge_pk(client, queue_name, challenge):
     AWS_SES_REGION_NAME = settings.AWS_SES_REGION_NAME
     AWS_SES_REGION_ENDPOINT = settings.AWS_SES_REGION_ENDPOINT
 
-    if challenge.worker_image_url:
-        COMMON_SETTINGS_DICT["WORKER_IMAGE"] = challenge.worker_image_url
-
     if execution_role_arn:
         from .utils import get_aws_credentials_for_challenge
 
@@ -265,6 +262,7 @@ def register_task_def_by_challenge_pk(client, queue_name, challenge):
                 log_group_name=log_group_name,
                 AWS_SES_REGION_NAME=AWS_SES_REGION_NAME,
                 AWS_SES_REGION_ENDPOINT=AWS_SES_REGION_ENDPOINT,
+                worker_image_url=challenge.worker_image_url,
                 **COMMON_SETTINGS_DICT,
                 **challenge_aws_keys,
             )
@@ -672,9 +670,6 @@ def scale_resources(challenge, worker_cpu_cores, worker_memory):
         logger.exception(e)
         return e.response
 
-    if challenge.worker_image_url:
-        COMMON_SETTINGS_DICT["WORKER_IMAGE"] = challenge.worker_image_url
-
     queue_name = challenge.queue
     container_name = "worker_{}".format(queue_name)
     log_group_name = get_log_group_name(challenge.pk)
@@ -689,6 +684,7 @@ def scale_resources(challenge, worker_cpu_cores, worker_memory):
         log_group_name=log_group_name,
         AWS_SES_REGION_NAME=settings.AWS_SES_REGION_NAME,
         AWS_SES_REGION_ENDPOINT=settings.AWS_SES_REGION_ENDPOINT,
+        worker_image_url=challenge.worker_image_url,
         **COMMON_SETTINGS_DICT,
         **challenge_aws_keys,
     )
