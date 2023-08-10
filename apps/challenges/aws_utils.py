d4f9bddbd7f6bfd13b0be43b2405901107402211
gchhablani <chhablani.gunjan@gmail.com>
2023-08-10 01:42:34 -0400

    [BugFix] Fix missing account IDin EC2 setup

--- apps/challenges/aws_utils.py
+++ apps/challenges/aws_utils.py
@@ -1497,6 +1497,7 @@ def setup_ec2(challenge):
 
     # Define the values for placeholders
     variables = {
+        "AWS_ACCOUNT_ID": aws_keys["AWS_ACCOUNT_ID"],
         "AWS_ACCESS_KEY_ID": aws_keys["AWS_ACCESS_KEY_ID"],
         "AWS_SECRET_ACCESS_KEY": aws_keys["AWS_SECRET_ACCESS_KEY"],
         "AWS_REGION": aws_keys["AWS_REGION"],
