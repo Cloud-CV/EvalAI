6bad2b062b40fac23079a2401129e6a5966977c9
Gunjan Chhablani <chhablani.gunjan@gmail.com>
2023-08-10 04:22:14 -0400

    [BugFix] Fix EC2 deployment script (#4094)

--- scripts/deployment/deploy_ec2_worker.sh
+++ scripts/deployment/deploy_ec2_worker.sh
@@ -3,9 +3,9 @@ sudo apt-get update
 git clone https://github.com/Cloud-CV/EvalAI.git
 sudo mv /EvalAI/ ~
 cd ~/EvalAI
-sudo apt  install awscli  -y
+sudo apt install awscli  -y
 sudo apt install docker-compose -y
-sudo groupadd docker
+sudo apt -V install gnupg2 pass
 sudo usermod -aG docker $USER
 newgrp docker
 aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
@@ -18,5 +18,6 @@ export TRAVIS_BRANCH=${ENVIRONMENT}
 eval $(aws ecr get-login --no-include-email)
 aws s3 cp s3://cloudcv-secrets/evalai/${ENVIRONMENT}/docker_${ENVIRONMENT}.env ./docker/prod/docker_${ENVIRONMENT}.env
 echo "Deploying worker for queue: " ${QUEUE}
+docker-compose -f pull worker
 docker-compose -f docker-compose-${ENVIRONMENT}.yml run --name=worker_${QUEUE} -e CHALLENGE_QUEUE=${QUEUE} -e CHALLENGE_PK=${PK} -d worker
 echo "Deployed worker docker container for queue: " ${QUEUE}
