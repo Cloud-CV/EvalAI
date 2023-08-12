cd957c05d69de80f8a9240412d6938a5cdb3d8bb
Suryansh Pathak <34577232+Suryansh5545@users.noreply.github.com>
2023-08-12 05:58:32 +0530

    [Backend] Add new domain options (#4111)
    * add new domain options
    
    * add tabular as option
    
    * Update models.py
    
    ---------
    
    Co-authored-by: Gunjan Chhablani <chhablani.gunjan@gmail.com>

--- apps/challenges/models.py
+++ apps/challenges/models.py
@@ -63,6 +63,9 @@ class Challenge(TimeStampedModel):
         ("CV", "Computer Vision"),
         ("NLP", "Natural Language Processing"),
         ("RL", "Reinforcement Learning"),
+        ("MM", "Multimodal"),
+        ("AUD", "Audio"),
+        ("TAB", "Tabular"),
     )
     domain = models.CharField(max_length=50, choices=DOMAIN_OPTIONS, null=True, blank=True)
     list_tags = ArrayField(models.TextField(null=True, blank=True), default=list, blank=True)
