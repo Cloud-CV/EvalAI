b2b9f10faff360b9ae8cef322a20126a2baa50e8
Suryansh5545 <Suryanshpathak5545@gmail.com>
2023-08-26 18:41:44 +0530

    Fix build

--- apps/challenges/models.py
+++ apps/challenges/models.py
@@ -272,13 +272,15 @@ def create_eks_cluster_or_ec2_for_challenge(sender, instance, created, **kwargs)
             aws.setup_ec2.delay(serialized_obj)
     aws.challenge_approval_callback(sender, instance, field_name, **kwargs)
 
+
 @receiver(signals.post_save, sender="challenges.Challenge")
 def update_sqs_rendezvous_time(sender, instance, created, **kwargs):
     """
     Function to update the retention time of the SQS queue
     """
     field_name = "approved_by_admin"
-    import boto3, os
+    import boto3
+    import os
 
     if not created and is_model_field_changed(instance, field_name):
         sqs = boto3.resource(
@@ -294,7 +296,6 @@ def update_sqs_rendezvous_time(sender, instance, created, **kwargs):
             queue_name = challenge.queue
             queue = sqs.get_queue_by_name(QueueName=queue_name)
             queue.set_attributes(Attributes={"MessageRetentionPeriod": challenge.sqs_retention_time})
-
 
 
 class DatasetSplit(TimeStampedModel):
