808d5f6101852015eb86f953a7b1509a045fcf37
Ayush Rathore <ayushrathore12501@gmail.com>
2023-08-29 09:28:02 +0530

    [Backend] Add challenge versioning using branches (#4144)
    * Support Multiple challenge version with different branches
    
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>
    
    * Fix migration
    
    * [Backend] Add or delete challenge phases and splits (#4024)
    
    * Ability to remove and add challenge phase and phase split
    
    * Remove changes to data split
    
    * Added check for submission existence
    
    * Added function to Zip function too
    
    * Added check to delete the challenge phase split
    
    * Added fix for challenge phase split removal
    
    * Remove not used error messages and add split deletion to zip method
    
    ---------
    
    Co-authored-by: Gunjan Chhablani <chhablani.gunjan@gmail.com>
    
    * [Frontend] Add search and filter for challenges (#4039)
    
    * [Frontend] Add cache busting to frontend (#4037)
    
    * Added Cache Busting
    
    * .
    
    * add forntend control for search and filter
    
    * Fix test case
    
    * removed junk folder
    
    * improved filter appearance
    
    * remove useless css changes
    
    ---------
    
    Co-authored-by: Gunjan Chhablani <chhablani.gunjan@gmail.com>
    
    * [Frontend] Add sponsors and prizes to challenge (#4030)
    
    * Added Sponsors and Domain
    
    * Changed Prize , sponsor page frontend, editted sponsor model, added test cases
    
    * Fix Test case
    
    * fixed migration
    
    * Fix migrations
    
    * Update challenge_config_utils.py
    
    * Update challenge_config_utils.py
    
    * Fix migration
    
    * Fix flake8 issues
    
    ---------
    
    Co-authored-by: Gunjan Chhablani <chhablani.gunjan@gmail.com>
    
    * Fix migrations
    
    * Fix migrations
    
    ---------
    
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>
    Co-authored-by: Gunjan Chhablani <chhablani.gunjan@gmail.com>
    Co-authored-by: Suryansh Pathak <34577232+Suryansh5545@users.noreply.github.com>

--- apps/challenges/views.py
+++ apps/challenges/views.py
@@ -3723,7 +3723,8 @@ def create_or_update_github_challenge(request, challenge_host_team_pk):
         return Response(response_data, status=status.HTTP_406_NOT_ACCEPTABLE)
 
     challenge_queryset = Challenge.objects.filter(
-        github_repository=request.data["GITHUB_REPOSITORY"]
+        github_repository=request.data["GITHUB_REPOSITORY"],
+        github_branch=request.data["GITHUB_BRANCH"]
     )
 
     if challenge_queryset:
@@ -3783,7 +3784,6 @@ def create_or_update_github_challenge(request, challenge_host_team_pk):
         zip_ref,
         challenge_queryset[0] if challenge_queryset else None
     )
-
     if not len(error_messages):
         if not challenge_queryset:
             error_messages = None
@@ -3800,6 +3800,9 @@ def create_or_update_github_challenge(request, challenge_host_team_pk):
                             ],
                             "github_repository": request.data[
                                 "GITHUB_REPOSITORY"
+                            ],
+                            "github_branch": request.data[
+                                "GITHUB_BRANCH"
                             ],
                         },
                     )
