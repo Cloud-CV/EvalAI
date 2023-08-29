bbad16e7e24dafb4579e0f8483f3be7c6185f789
Gunjan Chhablani <chhablani.gunjan@gmail.com>
2023-08-29 17:00:46 -0400

    Revert "[Frontend] Add sponsors and prizes to challenge (#4030)" (#4152)
    This reverts commit 22253e023d717067933a79197459dd78055577ab.

--- frontend/src/js/route-config/route-config.js
+++ frontend/src/js/route-config/route-config.js
@@ -325,22 +325,6 @@
             title: 'Leaderboard',
         };
 
-        var prizes = {
-            name: "web.challenge-main.challenge-page.prizes",
-            parent: "web.challenge-main.challenge-page",
-            url: "/prizes",
-            templateUrl: baseUrl + "/web/challenge/prizes.html",
-            title: 'Prizes',
-        };
-
-        var sponsors = {
-            name: "web.challenge-main.challenge-page.sponsors",
-            parent: "web.challenge-main.challenge-page",
-            url: "/sponsors",
-            templateUrl: baseUrl + "/web/challenge/sponsors.html",
-            title: 'Sponsors',
-        };
-
         var manage = {
             name: "web.challenge-main.challenge-page.manage",
             parent: "web.challenge-main.challenge-page",
@@ -638,8 +622,6 @@
         $stateProvider.state(my_challenge_all_submission);
         $stateProvider.state(approval_team);
         $stateProvider.state(leaderboard);
-        $stateProvider.state(prizes);
-        $stateProvider.state(sponsors);
         $stateProvider.state(challenge_phase_leaderboard);
         $stateProvider.state(challenge_phase_metric_leaderboard);
 
