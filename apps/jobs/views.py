2b9822d1f9fabbc26007c2381575e70d0678c475
Gunjan Chhablani <chhablani.gunjan@gmail.com>
2023-09-19 12:42:30 -0400

    Revert "[BugFix] Fix duplicate leaderboard data entry creation on resume (#4082)" (#4170)
    This reverts commit ce9462af9ac64dd94808d943ebdadca696fad22c.

--- apps/jobs/views.py
+++ apps/jobs/views.py
@@ -1208,34 +1208,15 @@ def update_submission(request, challenge_pk):
                         response_data, status=status.HTTP_400_BAD_REQUEST
                     )
 
-                try:
-                    leaderboard_data = get_leaderboard_data_model(
-                        submission_pk, challenge_phase_split.pk
-                    )
-                except LeaderboardData.DoesNotExist:
-                    leaderboard_data = None
-
                 data = {"result": accuracies}
-                if leaderboard_data is not None:
-                    serializer = CreateLeaderboardDataSerializer(
-                        leaderboard_data,
-                        data=data,
-                        partial=True,
-                        context={
-                            "challenge_phase_split": challenge_phase_split,
-                            "submission": submission,
-                            "request": request,
-                        },
-                    )
-                else:
-                    serializer = CreateLeaderboardDataSerializer(
-                        data=data,
-                        context={
-                            "challenge_phase_split": challenge_phase_split,
-                            "submission": submission,
-                            "request": request,
-                        },
-                    )
+                serializer = CreateLeaderboardDataSerializer(
+                    data=data,
+                    context={
+                        "challenge_phase_split": challenge_phase_split,
+                        "submission": submission,
+                        "request": request,
+                    },
+                )
                 if serializer.is_valid():
                     leaderboard_data_list.append(serializer)
                 else:
@@ -1604,34 +1585,15 @@ def update_partially_evaluated_submission(request, challenge_pk):
                         response_data, status=status.HTTP_400_BAD_REQUEST
                     )
 
-                try:
-                    leaderboard_data = get_leaderboard_data_model(
-                        submission_pk, challenge_phase_split.pk
-                    )
-                except LeaderboardData.DoesNotExist:
-                    leaderboard_data = None
-
                 data = {"result": accuracies}
-                if leaderboard_data is not None:
-                    serializer = CreateLeaderboardDataSerializer(
-                        leaderboard_data,
-                        data=data,
-                        partial=True,
-                        context={
-                            "challenge_phase_split": challenge_phase_split,
-                            "submission": submission,
-                            "request": request,
-                        },
-                    )
-                else:
-                    serializer = CreateLeaderboardDataSerializer(
-                        data=data,
-                        context={
-                            "challenge_phase_split": challenge_phase_split,
-                            "submission": submission,
-                            "request": request,
-                        },
-                    )
+                serializer = CreateLeaderboardDataSerializer(
+                    data=data,
+                    context={
+                        "challenge_phase_split": challenge_phase_split,
+                        "submission": submission,
+                        "request": request,
+                    },
+                )
                 if serializer.is_valid():
                     leaderboard_data_list.append(serializer)
                 else:
