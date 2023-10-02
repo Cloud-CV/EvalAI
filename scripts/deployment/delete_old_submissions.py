9702ee0fb6f967cdc55084af44b137bbc69a03b1
gchhablani <chhablani.gunjan@gmail.com>
2023-10-02 17:16:47 -0400

    Fix flake8 issues

--- scripts/deployment/delete_old_submissions.py
+++ scripts/deployment/delete_old_submissions.py
@@ -6,14 +6,14 @@ import time
 tmp_directory = '/tmp'
 
 # Calculate the cutoff time (60 seconds ago)
-cutoff_time = time.time() - 60  #  * 60 seconds
+cutoff_time = time.time() - 60  # 60 seconds
 
 # List all directories in /tmp starting with 'tmp'
 tmp_folders = [d for d in os.listdir(tmp_directory) if os.path.isdir(os.path.join(tmp_directory, d)) and d.startswith('tmp')]
 
 for folder in tmp_folders:
     folder_path = os.path.join(tmp_directory, folder)
-    
+
     # Check if the folder was created more than 60 seconds ago
     folder_creation_time = os.path.getctime(folder_path)
     if folder_creation_time < cutoff_time:
