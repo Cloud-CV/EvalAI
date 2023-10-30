0c924de4f29a4ac1cea2478f090e9977960fc416
Gunjan Chhablani <chhablani.gunjan@gmail.com>
2023-10-30 18:01:10 -0400

    Revert "[Backend] Allow disabling leaderboard data (#4180)" (#4200)
    This reverts commit ae017b46fbb406dadfd2bb44da025336022d9e60.

--- apps/challenges/urls.py
+++ apps/challenges/urls.py
@@ -299,11 +299,6 @@ urlpatterns = [
         views.get_sponsors_by_challenge,
         name="get_sponsors_by_challenge",
     ),
-    url(
-        r"^challenge/update_leaderboard_data/$",
-        views.update_leaderboard_data,
-        name="update_leaderboard_data",
-    )
 ]
 
 app_name = "challenges"
