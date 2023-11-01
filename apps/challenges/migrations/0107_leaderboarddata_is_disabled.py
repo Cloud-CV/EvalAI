84032e6e76cacd59f33ca7d406291d382fb46f4c
Gunjan Chhablani <chhablani.gunjan@gmail.com>
2023-10-31 23:48:03 -0400

    Update 0107_leaderboarddata_is_disabled.py

--- apps/challenges/migrations/0107_leaderboarddata_is_disabled.py
+++ apps/challenges/migrations/0107_leaderboarddata_is_disabled.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='leaderboarddata',
             name='is_disabled',
-            field=models.BooleanField(default=False),
+            field=models.BooleanField(null=False, default=False),
         ),
     ]
