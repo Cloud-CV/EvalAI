5acd2e01a6706ee0e788bbc599f00365802a76cb
hj <harshitjain0562@gmail.com>
2024-06-07 21:25:58 +0530

    Test for permission controller

--- frontend/tests/controllers-test/permissionCtrl.test.js
+++ frontend/tests/controllers-test/permissionCtrl.test.js
@@ -30,3 +30,48 @@ describe('Unit tests for permission controller', function () {
         });
     });
 });
+
+describe('Unit tests for requestLink function', function() {
+    var vm, $rootScope, utilities;
+
+    beforeEach(angular.mock.module('evalai'));
+
+    beforeEach(inject(function(_$rootScope_, _utilities_) {
+        $rootScope = _$rootScope_;
+        utilities = _utilities_;
+
+        vm = {
+            requestLink: function() {
+                var userKey = utilities.getData('userKey');
+                var parameters = {};
+                parameters.url = 'accounts/user/resend_email_verification/';
+                parameters.method = 'POST';
+                parameters.token = userKey;
+                parameters.callback = {
+                    onSuccess: function() {
+                        vm.sendMail = true;
+                        $rootScope.notify("success", "The verification link was sent again.");
+                    },
+                    onError: function(response) {
+                        var message = response.data.detail;
+                        var time = Math.floor(message.match(/\d+/g)[0] / 60);
+                        if (response.status == 429) {
+                            $rootScope.notify("error", "Request limit exceeded. Please wait for " + time + " minutes and try again.");
+                        } else {
+                            $rootScope.notify("error", "Something went wrong. Please try again.");
+                        }
+                    }
+                };
+                utilities.sendRequest(parameters);
+            }
+        };
+
+        spyOn(utilities, 'sendRequest');
+        spyOn($rootScope, 'notify');
+    }));
+
+    it('sends a request to resend the email verification link', function() {
+        vm.requestLink();
+        expect(utilities.sendRequest).toHaveBeenCalled();
+    });
+});
