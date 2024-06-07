79d15bf28acb6864d4f4f07112bd641a576e44b2
hj <harshitjain0562@gmail.com>
2024-06-07 21:21:14 +0530

    UpdateProfileTest

--- frontend/tests/controllers-test/updateProfileCtrl.test.js
+++ frontend/tests/controllers-test/updateProfileCtrl.test.js
@@ -178,3 +178,180 @@ describe('Unit tests for update profile controller', function () {
         });
     });
 });
+
+describe('Unit tests for getting user profile', function() {
+    var utilities, $rootScope, vm;
+    var successResponse = {
+        status: 200,
+        data: {
+            name: 'Test User'
+        }
+    };
+    var errorResponse = {
+        status: 400,
+        data: 'error'
+    };
+
+    beforeEach(angular.mock.module('evalai'));
+
+    beforeEach(inject(function(_utilities_, _$rootScope_) {
+        utilities = _utilities_;
+        $rootScope = _$rootScope_;
+        vm = {
+            user: null
+        };
+
+        spyOn(utilities, 'sendRequest').and.callFake(function(parameters) {
+            if (parameters.url === 'auth/user/') {
+                parameters.callback.onSuccess(successResponse);
+            } else {
+                parameters.callback.onError(errorResponse);
+            }
+        });
+
+        spyOn($rootScope, 'notify');
+    }));
+
+    it('gets user profile and sets vm.user', function() {
+        var userKey = 'testUserKey';
+        var parameters = {
+            url: 'auth/user/',
+            method: 'GET',
+            token: userKey,
+            callback: {
+                onSuccess: function(response) {
+                    var status = response.status;
+                    var result = response.data;
+                    if (status == 200) {
+                        vm.user = result;
+                    }
+                },
+                onError: function() {
+                    $rootScope.notify("error", "Error in loading profile, please try again later !");
+                }
+            }
+        };
+
+        utilities.sendRequest(parameters);
+
+        expect(vm.user).toEqual(successResponse.data);
+        expect($rootScope.notify).not.toHaveBeenCalled();
+    });
+
+    it('handles error when getting user profile', function() {
+        var userKey = 'testUserKey';
+        var parameters = {
+            url: 'auth/user_error/',
+            method: 'GET',
+            token: userKey,
+            callback: {
+                onSuccess: function(response) {
+                    var status = response.status;
+                    var result = response.data;
+                    if (status == 200) {
+                        vm.user = result;
+                    }
+                },
+                onError: function() {
+                    $rootScope.notify("error", "Error in loading profile, please try again later !");
+                }
+            }
+        };
+
+        utilities.sendRequest(parameters);
+
+        expect(vm.user).toBeNull();
+        expect($rootScope.notify).toHaveBeenCalledWith('error', 'Error in loading profile, please try again later !');
+    });
+});
+
+describe('Unit tests for updateProfile function', function() {
+    var vm;
+
+    beforeEach(angular.mock.module('evalai'));
+
+    beforeEach(inject(function() {
+        vm = {
+            user: {
+                github_url: null,
+                google_scholar_url: null,
+                linkedin_url: null
+            },
+            isURLValid: function(url) {
+                return url.length <= 200;
+            },
+            updateProfile: function(resetconfirmFormValid) {
+                if (resetconfirmFormValid) {
+                    this.user.github_url = this.user.github_url === null ? "" : this.user.github_url;
+                    this.user.google_scholar_url = this.user.google_scholar_url === null ? "" : this.user.google_scholar_url;
+                    this.user.linkedin_url = this.user.linkedin_url === null ? "" : this.user.linkedin_url;
+
+                    if (!this.isURLValid(this.user.github_url)) {
+                        this.isFormError = true;
+                        this.FormError = "Github URL length should not be greater than 200!";
+                        return;
+                    } else if (!this.isURLValid(this.user.google_scholar_url)) {
+                        this.isFormError = true;
+                        this.FormError = "Google Scholar URL length should not be greater than 200!";
+                        return;
+                    } else if (!this.isURLValid(this.user.linkedin_url)) {
+                        this.isFormError = true;
+                        this.FormError = "LinkedIn URL length should not be greater than 200!";
+                        return;
+                    }
+                }
+            }
+        };
+    }));
+
+    it('updates profile when form is valid', function() {
+        vm.user.github_url = 'https://github.com/test';
+        vm.user.google_scholar_url = 'https://scholar.google.com/test';
+        vm.user.linkedin_url = 'https://linkedin.com/in/test';
+        vm.updateProfile(true);
+        expect(vm.isFormError).toBeUndefined();
+        expect(vm.FormError).toBeUndefined();
+    });
+
+    it('does not update profile when form is invalid', function() {
+        vm.user.github_url = 'https://github.com/test' + 'a'.repeat(200);
+        vm.updateProfile(true);
+        expect(vm.isFormError).toBe(true);
+        expect(vm.FormError).toBe('Github URL length should not be greater than 200!');
+    });
+});
+
+describe('Unit tests for isURLValid function', function() {
+    var vm;
+
+    beforeEach(angular.mock.module('evalai'));
+
+    beforeEach(inject(function() {
+        vm = {
+            isURLValid: function(url) {
+                if (url === undefined || url === null) {
+                    return true;
+                }
+                return (url.length <= 200);
+            }
+        };
+    }));
+
+    it('returns true when URL is undefined', function() {
+        expect(vm.isURLValid(undefined)).toBe(true);
+    });
+
+    it('returns true when URL is null', function() {
+        expect(vm.isURLValid(null)).toBe(true);
+    });
+
+    it('returns true when URL length is less than or equal to 200', function() {
+        var url = 'https://test.com/' + 'a'.repeat(182); 
+        expect(vm.isURLValid(url)).toBe(true);
+    });
+
+    it('returns false when URL length is greater than 200', function() {
+        var url = 'https://test.com/' + 'a'.repeat(201);
+        expect(vm.isURLValid(url)).toBe(false);
+    });
+});
