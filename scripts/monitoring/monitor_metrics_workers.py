6569e44051501f774a8c17aeff5023d616cd1c60
MinhThieu145 <minhthieu135@gmail.com>
2024-07-13 15:03:08 -0600

    feat: Add functions to retrieve CPU, memory, and storage utilization metrics for a challenge

--- scripts/monitoring/monitor_metrics_workers.py
+++ scripts/monitoring/monitor_metrics_workers.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 import pytz
 import requests
@@ -15,64 +16,100 @@ authorization_header = {
 ENV = os.environ.get("ENV", "dev")
 
 
-def get_cpu_metrics_for_challenge(challenge, range_days=1, period_seconds=300):
-    get_cpu_metrics_endpoint = "{}/api/challenges/{}/get_ecs_workers_metrics/cpu/?range={}&period={}".format(
-        evalai_endpoint, challenge.id, range_days, period_seconds
+def fetch_metrics_in_chunks(endpoint, range_days=1, period_seconds=300):
+    max_days_per_request = 1  # Define maximum days per request to 1 day
+    metrics_data = []
+
+    start_date = datetime.datetime.now(pytz.UTC) - datetime.timedelta(
+        days=range_days
     )
-    response = requests.get(
-        get_cpu_metrics_endpoint, headers=authorization_header
+    end_date = datetime.datetime.now(pytz.UTC)
+
+    while start_date < end_date:
+        chunk_end_date = min(
+            start_date + datetime.timedelta(days=max_days_per_request),
+            end_date,
+        )
+        chunk_range_days = (chunk_end_date - start_date).days
+        chunk_endpoint = (
+            f"{endpoint}&range={chunk_range_days}&period={period_seconds}"
+        )
+        response = requests.get(chunk_endpoint, headers=authorization_header)
+
+        if response.status_code == 200:
+            metrics_data.append(response.json())
+        else:
+            print(
+                f"Failed to fetch metrics for chunk: {
+                    start_date} to {chunk_end_date}"
+            )
+
+        start_date = chunk_end_date
+
+    return metrics_data
+
+
+def get_cpu_metrics_for_challenge(challenge, range_days=1, period_seconds=300):
+    get_cpu_metrics_endpoint = f"{evalai_endpoint}/api/challenges/{
+        challenge.id}/get_ecs_workers_metrics/cpu/?period={period_seconds}"
+    metrics_data = fetch_metrics_in_chunks(
+        get_cpu_metrics_endpoint, range_days, period_seconds
     )
     print(
         "CPU Metrics for Challenge ID: {}, Title: {}".format(
             challenge.id, challenge.title
         )
     )
-    print(response)
+    print(metrics_data)
+    return metrics_data
 
 
 def get_memory_metrics_for_challenge(
     challenge, range_days=1, period_seconds=300
 ):
-    get_memory_metrics_endpoint = "{}/api/challenges/{}/get_ecs_workers_metrics/memory/?range={}&period={}".format(
-        evalai_endpoint, challenge.id, range_days, period_seconds
-    )
-    response = requests.get(
-        get_memory_metrics_endpoint, headers=authorization_header
+    get_memory_metrics_endpoint = f"{evalai_endpoint}/api/challenges/{
+        challenge.id}/get_ecs_workers_metrics/memory/?period={period_seconds}"
+    metrics_data = fetch_metrics_in_chunks(
+        get_memory_metrics_endpoint, range_days, period_seconds
     )
     print(
         "Memory Metrics for Challenge ID: {}, Title: {}".format(
             challenge.id, challenge.title
         )
     )
-    print(response)
+    print(metrics_data)
+    return metrics_data
 
 
 def get_storage_metrics_for_challenge(
     challenge, range_days=1, period_seconds=300
 ):
-    get_storage_metrics_endpoint = "{}/api/challenges/{}/get_ecs_workers_metrics/storage/?range={}&period={}".format(
-        evalai_endpoint, challenge.id, range_days, period_seconds
-    )
-    response = requests.get(
-        get_storage_metrics_endpoint, headers=authorization_header
+    get_storage_metrics_endpoint = f"{evalai_endpoint}/api/challenges/{
+        challenge.id}/get_ecs_workers_metrics/storage/?period={period_seconds}"
+    metrics_data = fetch_metrics_in_chunks(
+        get_storage_metrics_endpoint, range_days, period_seconds
     )
     print(
         "Storage Metrics for Challenge ID: {}, Title: {}".format(
             challenge.id, challenge.title
         )
     )
-    print(response)
+    print(metrics_data)
+    return metrics_data
 
 
 def monitor_workers_for_challenge(response, evalai_interface):
     for challenge in response["results"]:
         try:
             cpu_utilization_datapoints = get_cpu_metrics_for_challenge(
-                challenge)
+                challenge
+            )
             memory_utilization_datapoints = get_memory_metrics_for_challenge(
-                challenge)
+                challenge
+            )
             storage_utilization_datapoints = get_storage_metrics_for_challenge(
-                challenge)
+                challenge
+            )
 
             # log the metrics with print
             print(cpu_utilization_datapoints)
