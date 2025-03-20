1767a4a7b5dbc8b020cadf1218ceee41351a6cb2
Akshat Singh <akshatsingh@Akshats-MacBook-Air.local>
2025-03-20 23:42:54 +0530

    Test Cases

--- frontend/tests/controllers-test/profileCtrl.test.js
+++ frontend/tests/controllers-test/profileCtrl.test.js
@@ -162,6 +162,60 @@ describe('Unit tests for profile controller', function () {
             expect($mdDialog.hide).toHaveBeenCalled();
         });
     });
+    describe('Unit tests for Token Expired Dialog', function () {
+        var $mdDialog, vm, $rootScope, $scope, createController;
+    
+        beforeEach(angular.mock.module('evalai'));
+    
+        beforeEach(inject(function (_$controller_, _$rootScope_, _$mdDialog_) {
+            $mdDialog = _$mdDialog_;
+            $rootScope = _$rootScope_;
+            $scope = $rootScope.$new();
+    
+            createController = function () {
+                return _$controller_('profileCtrl', { $scope: $scope });
+            };
+            vm = createController();
+        }));
+    
+        it('should open the token expired dialog', function () {
+            spyOn(console, 'log');
+            spyOn($mdDialog, 'show');
+    
+            vm.showTokenExpiredDialog();
+    
+            expect(console.log).toHaveBeenCalledWith("Token Expired Dialog Triggered!");
+            expect($mdDialog.show).toHaveBeenCalledWith({
+                templateUrl: 'dist/views/web/auth/auth-token-expired-dialog.html',
+                controller: jasmine.any(Array)
+            });
+        });
+    
+        it('should refresh token when refreshToken is called', function () {
+            spyOn(vm, 'refreshToken');
+            spyOn($mdDialog, 'hide');
+    
+            var dialogController = $mdDialog.show.calls.mostRecent().args[0].controller[2];
+            var dialogScope = {};
+            dialogController(dialogScope, $mdDialog);
+            dialogScope.refreshToken();
+    
+            expect($mdDialog.hide).toHaveBeenCalled();
+            expect(vm.refreshToken).toHaveBeenCalled();
+        });
+    
+        it('should dismiss the dialog when dismiss is called', function () {
+            spyOn($mdDialog, 'hide');
+    
+            var dialogController = $mdDialog.show.calls.mostRecent().args[0].controller[2];
+            var dialogScope = {};
+            dialogController(dialogScope, $mdDialog);
+            dialogScope.dismiss();
+    
+            expect($mdDialog.hide).toHaveBeenCalled();
+        });
+    });
+    
 
     describe('Unit tests for `updateProfile` function', function () {
         var success, tryClauseResponse;
