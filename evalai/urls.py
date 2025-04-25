b516123cd73e25fcc30dc65b6ed41c8cab12ad70
Zahed-Riyaz <f20213179@hyderabad.bits-pilani.ac.in>
2025-04-25 20:27:39 +0530

    update for quality checks

--- evalai/urls.py
+++ evalai/urls.py
@@ -19,7 +19,6 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import re_path
 from django.views.generic.base import TemplateView
 from drf_spectacular.views import (
     SpectacularAPIView,
