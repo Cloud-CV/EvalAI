c1fe2a548d692f9490aff937a60e86bf59c5d5f5
Sekhar-Kumar-Dash <119131588+Sekhar-Kumar-Dash@users.noreply.github.com>
2025-04-27 22:00:22 +0530

    fixed  pylint errors in urls.py

--- apps/hosts/urls.py
+++ apps/hosts/urls.py
@@ -19,17 +19,21 @@ urlpatterns = [
         name="create_challenge_host_team",
     ),
     url(
-        r"^challenge_host_team/(?P<challenge_host_team_pk>[0-9]+)/challenge_host$",
+        r"^challenge_host_team/"
+        r"(?P<challenge_host_team_pk>[0-9]+)/challenge_host$",
         views.challenge_host_list,
         name="get_challenge_host_list",
     ),
     url(
-        r"^challenge_host_team/(?P<challenge_host_team_pk>[0-9]+)/challenge_host/(?P<pk>[0-9]+)$",
+        r"^challenge_host_team/"
+        r"(?P<challenge_host_team_pk>[0-9]+)/"
+        r"challenge_host/(?P<pk>[0-9]+)$",
         views.challenge_host_detail,
         name="get_challenge_host_details",
     ),
     url(
-        r"^remove_self_from_challenge_host/(?P<challenge_host_team_pk>[0-9]+)$",
+        r"^remove_self_from_challenge_host/"
+        r"(?P<challenge_host_team_pk>[0-9]+)$",
         views.remove_self_from_challenge_host_team,
         name="remove_self_from_challenge_host_team",
     ),
