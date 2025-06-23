e1d9436257f01ef409a4d1289136d624ce55dcd9
Akshat Singh <akshatsingh@Akshats-MacBook-Air.local>
2025-06-23 15:41:53 +0530

    Build Fixed

--- tests/unit/analytics/test_apps.py
+++ tests/unit/analytics/test_apps.py
@@ -1,6 +1,7 @@
 import analytics
 from analytics.apps import AnalyticsConfig
 
+
 def test_analytics_app_config_properties():
     config = AnalyticsConfig("analytics", analytics)
     assert config.name == "analytics"
