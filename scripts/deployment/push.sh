7e7059b22759c258c96e0965df56ed47dc413bd7
Rishabh Jain <rishabhjain2018@gmail.com>
2026-03-01 22:38:19 -0800

    Update Nginx configuration files to use the correct SSL certificate path and enhance deployment script to pull additional SSL certificates from S3, improving the deployment process for both production and staging environments. (#5043)

--- scripts/deployment/push.sh
+++ scripts/deployment/push.sh
@@ -8,6 +8,7 @@ build_and_push() {
         eval $(aws ecr get-login --no-include-email)
         echo "Pulling ssl certificates and nginx configuration..."
         aws s3 cp s3://cloudcv-secrets/eval.ai/ssl/ ./ssl/ --recursive
+        aws s3 cp s3://cloudcv-secrets/ssl/ ./ssl/ --recursive
         aws s3 cp s3://cloudcv-secrets/evalai/${TRAVIS_BRANCH}/ssl/ ./ssl/ --recursive
         aws s3 cp s3://cloudcv-secrets/evalai/${TRAVIS_BRANCH}/nginx_${TRAVIS_BRANCH}.conf ./docker/prod/nodejs/nginx_${TRAVIS_BRANCH}.conf
         echo "Pulled ssl certificates and nginx configuration successfully"
